2c76cc31a81e9e225708bba91957cd33a88676ca
ttbarnes <tony@tonybarnes.me>
2022-05-19 12:10:57 +0100

    add express session, csrf

--- ui/server/index.js
+++ ui/server/index.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const compression = require('compression');
 const morgan = require('morgan');
+const crypto = require('crypto');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
 const path = require('path');
 
-const security = require('./middleware/headers/security');
+const { csrf: csrfToken, security } = require('./middleware');
 const configureNunjucks = require('./nunjucks-configuration');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const https = Boolean(process.env.HTTPS || 0);
 
 app.use(security);
+app.use(compression());
+
+const cookie = {
+  path: '/',
+  httpOnly: true,
+  secure: https,
+  sameSite: 'strict',
+  maxAge: 604800000, // 7 days
+};
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || crypto.randomBytes(256 / 8).toString('hex'),
+  resave: false,
+  saveUninitialized: true,
+  cookie,
+};
+
+app.use(session(sessionOptions));
+
+app.use(cookieParser());
+app.use(csrf());
+app.use(csrfToken());
 
 configureNunjucks({
   autoescape: true,
@@ -18,7 +45,6 @@ configureNunjucks({
   noCache: true,
 });
 
-app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
---
+++ ui/server/middleware/index.js
@@ -0,0 +1,7 @@
+const csrf = require('./csrf');
+const security = require('./headers/security');
+
+module.exports = {
+  csrf,
+  security,
+};
