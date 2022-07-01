a3ffd35f346246063ece6fbbe79695c79cb9eb33
ttbarnes <tony@tonybarnes.me>
2022-07-01 13:40:52 +0100

    increase cypress wait value for country search

--- e2e-tests/cypress/integration/journeys/your-quote/your-quote-change-answers.spec.js
+++ e2e-tests/cypress/integration/journeys/your-quote/your-quote-change-answers.spec.js
@@ -137,7 +137,7 @@ context('Your quote page - change answers', () => {
       // autocomplete component does not have a focused attribute, instead it has a class.
       // this is added with client side JS.
       // we have to wait to ensure that client side js has been executed.
-      cy.wait(1500); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
 
       buyerBasedPage.searchInput().should('have.class', 'autocomplete__input--focused');
     });
