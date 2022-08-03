f0eddaa8909a567e6fe12f2a985d35c7d9e4c212
ttbarnes <tony@tonybarnes.me>
2022-08-03 12:31:16 +0100

    feat/EMS-241 - months mapping in summary lists rendering month when value is not 1 or 0

--- ui/server/helpers/data-content-mappings/map-period-months.test.js
+++ ui/server/helpers/data-content-mappings/map-period-months.test.js
@@ -1,11 +1,33 @@
 const mapPeriodMonths = require('./map-period-months');
 
 describe('server/helpers/map-period-months', () => {
-  it('should return a formatted string', () => {
-    const result = mapPeriodMonths(20);
+  describe('when value is 0', () => {
+    it('should return a formatted string with `month`', () => {
+      const result = mapPeriodMonths(0);
 
-    const expected = '20 months';
+      const expected = '0 months';
 
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('when value is 1', () => {
+    it('should return a formatted string with `month`', () => {
+      const result = mapPeriodMonths(1);
+
+      const expected = '1 month';
+
+      expect(result).toEqual(expected);
+    });
+  });
+  
+  describe('when value is not 0 or 1', () => {
+    it('should return a formatted string with `months`', () => {
+      const result = mapPeriodMonths(2);
+
+      const expected = '2 months';
+
+      expect(result).toEqual(expected);
+    });
   });
 });
