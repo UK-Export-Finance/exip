258672afdde9a25d5b225e50b5bdcef59eadc5bc
ttbarnes <tony@tonybarnes.me>
2022-08-10 14:24:15 +0100

    feat:EMS-200 - fix typos

--- src/ui/jest.config.js
+++ src/ui/jest.config.js
@@ -1,17 +1,3 @@
-// module.exports = {
-//   collectCoverageFrom: [
-//     'server/controllers/**/*.{js,}',
-//     'server/routes/**/*.{js,}',
-//     'server/helpers/*.{js,}',
-//     'server/generate-quote/*.{js,}',
-//   ],
-//   coverageDirectory: 'generated_reports/coverage/unit',
-//   testMatch: ['**/*.test.js', '**/*.component-test.js'],
-//   moduleNameMapper: {
-//     '^.+\\.(css|less|scss)$': 'babel-jest',
-//   },
-// };
-
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
