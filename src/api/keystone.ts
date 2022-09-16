16dbce757f018e2db4f35f99f071eeaed1d3f8c6
Tony Barnes <tony@tonybarnes.me>
2022-09-16 16:47:27 +0100

    feat/EMS-316 - API setup (#110)
    * feat(EMS-316): initial api
    
    * feat(EMS-316): add airbnb-base eslint config to root deps
    
    * feat(EMS-316): add airbnb config to root deps
    
    * feat(EMS-316): add eslint-plugin-jsx-a11y to api deps
    
    * feat(EMS-316): add eslint-plugin-jsx-a11y to root deps instead of api
    
    * feat(EMS-316): add eslint-plugin-react to root deps
    
    * feat(EMS-316): add/update root deps
    
    * feat(EMS-316): add eslint-import-resolver-typescript to root deps
    
    * feat(EMS-316): add keystone to root deps

---
+++ src/api/keystone.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { config } from '@keystone-6/core';
+import { lists } from './schema';
+import { withAuth, session } from './auth';
+
+export default withAuth(
+  config({
+    db: {
+      provider: 'mysql',
+      url: String(process.env.DB_URI),
+    },
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    lists,
+    session,
+  }),
+);
