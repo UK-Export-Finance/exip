a553f82d4d5f1a7ea8b48e2bf7032ac6ccd974b1
ttbarnes <tony@tonybarnes.me>
2022-09-28 10:25:18 +0100

    feat(EMS-378): only run GA script in production env

--- src/ui/server/nunjucks-configuration/index.ts
+++ src/ui/server/nunjucks-configuration/index.ts
@@ -1,4 +1,7 @@
 import nunjucks from 'nunjucks';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 interface Opts {
   autoescape: boolean;
@@ -9,7 +12,15 @@ interface Opts {
 const configureNunjucks = (opts: Opts) => {
   const appViews = ['node_modules/govuk-frontend', 'templates'];
 
-  nunjucks.configure(appViews, opts);
+  const nunjucksEnvironment = nunjucks.configure(appViews, opts);
+
+  nunjucksEnvironment.addGlobal('runAnalytics', () => {
+    if (process.env.NODE_ENV === 'production') {
+      return true;
+    }
+
+    return false;
+  });
 
   return nunjucks;
 };
