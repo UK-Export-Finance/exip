434b1dc080d891a1065014fe2c5a399950262318
ttbarnes <tony@tonybarnes.me>
2022-10-13 15:44:59 +0100

    create corePageVariables function and extend the type in singleInputPageVariables

--- src/ui/server/helpers/single-input-page-variables.ts
+++ src/ui/server/helpers/single-input-page-variables.ts
@@ -1,47 +1,11 @@
-import { BUTTONS, COOKIES_CONSENT, FIELDS, FOOTER, LINKS, PRODUCT } from '../content-strings';
+import corePageVariables from './core-page-variables';
+import { FIELDS } from '../content-strings';
+import { SingleInputPageVariablesInput, SingleInputPageVariables } from '../../types';
 
-interface PageContentStrings {
-  PAGE_TITLE: string;
-  HEADING: string;
-  FIELDS?: object;
-  HINTS?: object;
-  LINKS?: object;
-}
-
-interface SingleInputPageVariables {
-  FIELD_ID: string;
-  PAGE_CONTENT_STRINGS: PageContentStrings;
-  BACK_LINK?: string;
-}
-
-interface PageVariablesContentStrings {
-  BUTTONS: object;
-  COOKIES_CONSENT: object;
-  FOOTER: object;
-  LINKS: object;
-  PRODUCT: object;
-}
-
-interface PageVariables {
-  CONTENT_STRINGS: PageVariablesContentStrings;
-  FIELD_ID: string;
-  FIELD_LABEL?: string;
-  FIELD_HINT?: string | object;
-  BACK_LINK?: string;
-}
-
-const singleInputPageVariables = ({ FIELD_ID, PAGE_CONTENT_STRINGS, BACK_LINK }: SingleInputPageVariables) => {
-  const pageVariables: PageVariables = {
-    CONTENT_STRINGS: {
-      ...PAGE_CONTENT_STRINGS,
-      BUTTONS,
-      COOKIES_CONSENT,
-      FOOTER,
-      LINKS,
-      PRODUCT,
-    },
+const singleInputPageVariables = ({ FIELD_ID, PAGE_CONTENT_STRINGS, BACK_LINK }: SingleInputPageVariablesInput) => {
+  const pageVariables: SingleInputPageVariables = {
+    ...corePageVariables({ PAGE_CONTENT_STRINGS, BACK_LINK }),
     FIELD_ID,
-    BACK_LINK,
   };
 
   const fieldStrings = FIELDS[FIELD_ID];
