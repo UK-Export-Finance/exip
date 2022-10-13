20d3a1aef441bf5bc1e3a5cb83f35a5615224ba1
ttbarnes <tony@tonybarnes.me>
2022-10-13 15:47:01 +0100

    linting

---
+++ src/ui/types/page-variables.d.ts
@@ -0,0 +1,34 @@
+interface PageContentStrings {
+  PAGE_TITLE: string;
+  HEADING: string;
+}
+
+interface PageVariablesContentStrings {
+  BUTTONS: object;
+  COOKIES_CONSENT: object;
+  FOOTER: object;
+  LINKS: object;
+  PRODUCT: object;
+}
+
+interface CorePageVariablesInput {
+  PAGE_CONTENT_STRINGS: PageContentStrings;
+  BACK_LINK?: string;
+}
+
+interface CorePageVariables {
+  CONTENT_STRINGS: PageVariablesContentStrings;
+  BACK_LINK?: string;
+}
+
+interface SingleInputPageVariablesInput extends CorePageVariablesInput {
+  FIELD_ID: string;
+}
+
+interface SingleInputPageVariables extends CorePageVariables {
+  FIELD_ID: string;
+  FIELD_LABEL?: string;
+  FIELD_HINT?: string | object;
+}
+
+export { CorePageVariablesInput, CorePageVariables, PageContentStrings, PageVariablesContentStrings, SingleInputPageVariablesInput, SingleInputPageVariables };
