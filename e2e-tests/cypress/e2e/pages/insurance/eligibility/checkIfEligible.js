77e720c1ff7d871adb24a8b490065aa3698f2e67
ttbarnes <tony@tonybarnes.me>
2022-10-24 14:16:06 +0100

    fix typo

--- e2e-tests/cypress/e2e/pages/insurance/eligibility/checkIfEligible.js
+++ e2e-tests/cypress/e2e/pages/insurance/eligibility/checkIfEligible.js
@@ -1,5 +1,6 @@
 const checkIfEligiblePage = {
   heading: () => cy.get('[data-cy="heading"]'),
+  body: () => cy.get('[data-cy="body"]'),
   submitButton: () => cy.get('[data-cy="submit-button"]'),
 };
 
