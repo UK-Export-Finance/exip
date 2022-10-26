bc42c11cb526fdd77406402e0c23ba0b7c1db7a7
Tony Barnes <tony@tonybarnes.me>
2022-10-26 11:24:48 +0100

    feat(EMS-353): initial data model for full application (#137)
    * feat(EMS-353): initial data model for full application
    
    * feat(EMS-353): initial data model for full application
    
    * feat(EMS-353): dockerise API
    
    * feat(EMS-353): root linting for UI

--- src/api/schema.ts
+++ src/api/schema.ts
@@ -1,25 +1,55 @@
 import { list } from '@keystone-6/core';
-import { text, password } from '@keystone-6/core/fields';
+import { integer, relationship, select, text, timestamp, password } from '@keystone-6/core/fields';
 import { document } from '@keystone-6/fields-document';
-import { Lists } from '.keystone/types'; // eslint-disable-line
+import { addMonths } from 'date-fns';
+import { Lists } from '.keystone/types'; // eslint-disable-line import/no-extraneous-dependencies
+import { APPLICATION } from './constants';
 
-export const lists: Lists = {
-  User: list({
-    fields: {
-      name: text({ validation: { isRequired: true } }),
-      email: text({
-        validation: { isRequired: true },
-        isIndexed: 'unique',
-        isFilterable: true,
-      }),
-      password: password({ validation: { isRequired: true } }),
+export const lists = {
+  ReferenceNumber: {
+    db: {
+      idField: { kind: 'autoincrement' },
     },
-    ui: {
-      listView: {
-        initialColumns: ['name', 'email'],
+    fields: {
+      application: relationship({ ref: 'Application' }),
+    },
+  },
+  Application: {
+    fields: {
+      createdAt: timestamp(),
+      updatedAt: timestamp(),
+      referenceNumber: integer({
+        isIndexed: true,
+      }),
+      submissionDeadline: timestamp(),
+      submissionType: select({
+        options: [{ label: APPLICATION.SUBMISSION_TYPE.MIA, value: APPLICATION.SUBMISSION_TYPE.MIA }],
+        defaultValue: APPLICATION.SUBMISSION_TYPE.MIA,
+      }),
+    },
+    hooks: {
+      resolveInput: async ({ resolvedData, context }) => {
+        const modifiedData = resolvedData;
+
+        // generate and add a new unique reference number
+        const { id: newReferenceNumber } = await context.db.ReferenceNumber.createOne({
+          data: {},
+        });
+        modifiedData.referenceNumber = newReferenceNumber;
+
+        // add dates
+        const now = new Date();
+        modifiedData.createdAt = now;
+        modifiedData.updatedAt = now;
+        modifiedData.submissionDeadline = addMonths(new Date(now), APPLICATION.SUBMISSION_DEADLINE_IN_MONTHS);
+
+        // add default submission type
+        modifiedData.submissionType = APPLICATION.SUBMISSION_TYPE.MIA;
+
+        return modifiedData;
       },
     },
-  }),
+  },
   Page: list({
     fields: {
       heading: text({
@@ -40,4 +70,19 @@ export const lists: Lists = {
       },
     },
   }),
-};
+  User: list({
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      email: text({
+        validation: { isRequired: true },
+        isIndexed: 'unique',
+      }),
+      password: password({ validation: { isRequired: true } }),
+    },
+    ui: {
+      listView: {
+        initialColumns: ['name', 'email'],
+      },
+    },
+  }),
+} as Lists;
