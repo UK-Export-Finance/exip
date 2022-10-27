30b5652df4e5d1718d0f27067eeeaf0540cfa8c5
ttbarnes <tony@tonybarnes.me>
2022-10-27 13:39:08 +0100

    fix(EMS-379): update e2e tests to check autocomplete value instead of hidden input value

--- e2e-tests/cypress/e2e/pages/quote/buyerCountry.js
+++ e2e-tests/cypress/e2e/pages/quote/buyerCountry.js
@@ -4,7 +4,6 @@ const buyerCountryPage = {
   heading: () => cy.get('[data-cy="heading"]'),
   hint: () => cy.get(`[data-cy="${FIELD_IDS.BUYER_COUNTRY}-hint"]`),
   searchInput: () => cy.get(`#${FIELD_IDS.BUYER_COUNTRY}`),
-  hiddenInput: () => cy.get(`#${FIELD_IDS.BUYER_COUNTRY}`),
   results: () => cy.get(`#${FIELD_IDS.BUYER_COUNTRY} + ul li`),
   noResults: () => cy.get('.autocomplete__option--no-results'),
   errorMessage: () => cy.get(`[data-cy="${FIELD_IDS.BUYER_COUNTRY}-error-message"]`),
