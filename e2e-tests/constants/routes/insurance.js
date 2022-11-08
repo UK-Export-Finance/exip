8fb44fa6379ea8ecaf2c135ca831c1f777713ad0
ttbarnes <tony@tonybarnes.me>
2022-11-08 15:23:06 +0000

    feat(EMS-499): add tests for redirection to 'start again' page when skipping insurance eligibility forms

--- e2e-tests/constants/routes/insurance.js
+++ e2e-tests/constants/routes/insurance.js
@@ -19,9 +19,9 @@ const INSURANCE_ROUTES = {
     LETTER_OF_CREDIT: `${INSURANCE}${ELIGIBILITY}/letter-of-credit`,
     PRE_CREDIT_PERIOD: `${INSURANCE}${ELIGIBILITY}/pre-credit-period`,
     COMPANIES_HOUSE_NUMBER: `${INSURANCE}${ELIGIBILITY}/companies-house-number`,
-    COMPANIES_HOUSE_NUMBER: `${INSURANCE}${ELIGIBILITY}/companies-house-number`,
     ELIGIBLE_TO_APPLY_ONLINE: `${INSURANCE}${ELIGIBILITY}/eligible-to-apply-online`,
     ALREADY_HAVE_ACCOUNT: `${INSURANCE}${ELIGIBILITY}/already-have-account`,
+    NEED_TO_START_AGAIN: `${INSURANCE}${ELIGIBILITY}/need-to-start-again`,
   },
 };
 
