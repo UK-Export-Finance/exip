1d15167a239eaf90e9d792fd074af844ad7b3ced
ttbarnes <tony@tonybarnes.me>
2022-11-29 10:08:55 +0000

    linting

--- src/api/custom-schema.ts
+++ src/api/custom-schema.ts
@@ -1,10 +1,10 @@
 import type { GraphQLSchema } from 'graphql';
 import { mergeSchemas } from '@graphql-tools/schema';
-// @ts-ignore
-// import { NotifyClient } from 'notifications-node-client';
 
-// const notifyKey: any = process.env.GOV_NOTIFY_API_KEY;
-// const notifyClient = new NotifyClient(notifyKey);
+import { NotifyClient } from 'notifications-node-client';
+
+const notifyKey: any = process.env.GOV_NOTIFY_API_KEY;
+const notifyClient = new NotifyClient(notifyKey);
 
 export const extendGraphqlSchema = (schema: GraphQLSchema) =>
   mergeSchemas({
@@ -20,22 +20,22 @@ export const extendGraphqlSchema = (schema: GraphQLSchema) =>
       `,
     resolvers: {
       Mutation: {
-        // sendEmail: (root, variables) => {
-        //   try {
-        //     console.info('Calling Notify API. templateId: ', variables.templateId);
-        //     const { templateId, sendToEmailAddress } = variables;
+        sendEmail: (root, variables) => {
+          try {
+            console.info('Calling Notify API. templateId: ', variables.templateId);
+            const { templateId, sendToEmailAddress } = variables;
 
-        //     notifyClient.sendEmail(templateId, sendToEmailAddress, {
-        //       personalisation: {},
-        //       reference: null,
-        //     });
+            notifyClient.sendEmail(templateId, sendToEmailAddress, {
+              personalisation: {},
+              reference: null,
+            });
 
-        //     return { success: true };
-        //   } catch (err) {
-        //     console.error('Unable to send email', { err });
-        //     return { success: false };
-        //   }
-        // },
+            return { success: true };
+          } catch (err) {
+            console.error('Unable to send email', { err });
+            return { success: false };
+          }
+        },
       },
     },
   });
