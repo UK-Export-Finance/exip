8fc57ba5a3f2b2927938160c13c5802cd64898ca
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2022-12-06 11:22:19 +0000

    EMS-617 linting fixes

--- src/api/custom-schema.ts
+++ src/api/custom-schema.ts
@@ -1,5 +1,6 @@
 import type { GraphQLSchema } from 'graphql';
 import { mergeSchemas } from '@graphql-tools/schema';
+// @ts-ignore
 import { NotifyClient } from 'notifications-node-client';
 import axios from 'axios';
 import dotenv from 'dotenv';
@@ -76,49 +77,49 @@ export const extendGraphqlSchema = (schema: GraphQLSchema) =>
           }
         },
       },
-    Query: {
-      /**
-       * Call for companies house API
-       * @param variables - companies house number is received as a string within variables
-       * @returns either mapped response or success false flag with or without apiError
-       */
-      getCompaniesHouseInformation: async (root, variables) => {
-        try {
-          const { companiesHouseNumber } = variables;
-          console.info('Calling Companies House API for ', companiesHouseNumber);
-          const sanitisedRegNo: number = companiesHouseNumber.padStart(8, '0');
+      Query: {
+        /**
+         * Call for companies house API
+         * @param variables - companies house number is received as a string within variables
+         * @returns either mapped response or success false flag with or without apiError
+         */
+        getCompaniesHouseInformation: async (root, variables) => {
+          try {
+            const { companiesHouseNumber } = variables;
+            console.info('Calling Companies House API for ', companiesHouseNumber);
+            const sanitisedRegNo: number = companiesHouseNumber.padStart(8, '0');
 
-          const response = await axios({
-            method: 'get',
-            url: `${companiesHouseURL}/company/${sanitisedRegNo}`,
-            auth: { username, password: '' },
-            validateStatus(status) {
-              return status === 200 || status === 404;
-            },
-          });
+            const response = await axios({
+              method: 'get',
+              url: `${companiesHouseURL}/company/${sanitisedRegNo}`,
+              auth: { username, password: '' },
+              validateStatus(status) {
+                return status === 200 || status === 404;
+              },
+            });
 
-          // if no data in response or status is not 200 then return blank object
-          if (!response.data || response.status === 404) {
+            // if no data in response or status is not 200 then return blank object
+            if (!response.data || response.status === 404) {
+              return {
+                success: false,
+              };
+            }
+
+            // maps response to camelCase fields
+            const mappedResponse = mapCompaniesHouseFields(response.data);
+
             return {
+              ...mappedResponse,
+              success: true,
+            };
+          } catch (error) {
+            console.error('Error calling Companies House API', { error });
+            return {
+              apiError: true,
               success: false,
             };
           }
-
-          // maps response to camelCase fields
-          const mappedResponse = mapCompaniesHouseFields(response.data);
-
-          return {
-            ...mappedResponse,
-            success: true,
-          };
-        } catch (error) {
-          console.error('Error calling Companies House API', { error });
-          return {
-            apiError: true,
-            success: false,
-          };
-        }
+        },
       },
     },
-  },
-});
+  });
