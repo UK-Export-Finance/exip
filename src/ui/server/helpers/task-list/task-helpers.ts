5da82e1649511e79bde493e7f6ffd3d77afe5be9
ttbarnes <tony@tonybarnes.me>
2022-12-07 13:29:27 +0000

    improve a task list function's documentation

--- src/ui/server/helpers/task-list/task-helpers.ts
+++ src/ui/server/helpers/task-list/task-helpers.ts
@@ -22,7 +22,7 @@ export const getTaskById = (groupTasks: Array<TaskListDataTask>, taskId: string)
 /**
  * getAllTasksFieldsInAGroup
  * @param {Object} group Task list group
- * @returns {Array} All field IDs from all tasks in the provided group
+ * @returns {Array} All field IDs from all tasks in the provided group. Flattens the task fields into a 1 level array.
  */
 export const getAllTasksFieldsInAGroup = (group: TaskListDataGroup): Array<string> => {
   if (group && group.tasks && group.tasks.length) {
