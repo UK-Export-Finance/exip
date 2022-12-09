afdb65c131b961e34b85386dc15779fb51641a68
ttbarnes <tony@tonybarnes.me>
2022-12-09 10:57:27 +0000

    feat(EMS-503): filter out empty fields when calling hasFormData

--- src/ui/server/helpers/has-form-data/index.ts
+++ src/ui/server/helpers/has-form-data/index.ts
@@ -1,16 +1,50 @@
 import { RequestBody } from '../../../types';
 
 /**
+ * stripEmptyFormFields
+ * Filters out form fields with empty values
+ * @param {Express.Request.body} Form data
+ * @returns {Object} Form data without empty values
+ */
+export const stripEmptyFormFields = (formBody: RequestBody) => {
+  if (formBody) {
+    const fieldsWithValues = {};
+
+    const keys = Object.keys(formBody);
+
+    keys.forEach((key) => {
+      const value = formBody[key];
+
+      if (value !== '' && value !== null && value !== undefined) {
+        fieldsWithValues[key] = value;
+      }
+    });
+
+    return fieldsWithValues;
+  }
+
+  return {};
+};
+
+/**
  * hasFormData
  * Checks if form data has values
  * @param {Express.Request.body} Form data
  * @returns {Boolean}
  */
 const hasFormData = (formBody: RequestBody) => {
+  if (!formBody) {
+    return false;
+  }
+
   const { _csrf, ...formData } = formBody;
 
-  if (Object.keys(formData).length) {
-    return true;
+  if (formData && Object.keys(formData).length) {
+    const fieldsWithValues = stripEmptyFormFields(formData);
+
+    if (Object.keys(fieldsWithValues).length) {
+      return true;
+    }
   }
 
   return false;
