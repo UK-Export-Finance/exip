00a1af477f25454ce4ff7be1f89e79530b24b012
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2022-12-13 09:55:14 +0000

    feat(EMS-718): Root unit test

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'server/controllers/**/*.{ts,}',
+    'server/middleware/**/*.{ts,}',
+    'server/routes/**/*.{ts,}',
+    'server/helpers/**/*.{ts,}',
+    'server/generate-quote/*.{ts,}',
+  ],
+  coverageDirectory: 'generated_reports/coverage/tests',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
