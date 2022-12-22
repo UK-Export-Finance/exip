98624768ecae3aee4692de21a51e7b96eb1f678d
ttbarnes <tony@tonybarnes.me>
2022-12-22 16:04:29 +0000

    fix typo

--- src/ui/server/helpers/combine-validation-rules/index.ts
+++ src/ui/server/helpers/combine-validation-rules/index.ts
@@ -2,10 +2,10 @@ import { ValidationErrors } from '../../../types';
 
 /**
  * combineValidationRules
- * validates company details page response
+ * combine multiple validation rule results into a single object
  * throws validation errors if any fields are not completed or incorrectly completed
  * @param {Express.Request.body} responseBody containing an object with the company details response body
- * @returns {object} object containing errors or blank object
+ * @returns {object} object containing errors from multiple fields/rules, or a blank object
  */
 /* eslint-disable no-unused-vars, prettier/prettier */
 const combineValidationRules = (rules: Array<(formBody: object, errors: ValidationErrors) => ValidationErrors>, data: object) => {
