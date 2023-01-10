726dc4f9cf75726c9f89f4760e1c79a852d905d9
ttbarnes <tony@tonybarnes.me>
2023-01-10 17:58:19 +0000

    feat(EMS-609): remove mapCountriesSelect and use mapCountries instead. Update your buyer controller to call keystone countries API instead of CIS API

--- src/ui/server/controllers/insurance/your-buyer/index.ts
+++ src/ui/server/controllers/insurance/your-buyer/index.ts
@@ -3,16 +3,17 @@ import { Request, Response } from '../../../../types';
 import api from '../../../api';
 import { ROUTES, TEMPLATES } from '../../../constants';
 import insuranceCorePageVariables from '../../../helpers/page-variables/core/insurance';
-import { mapCountriesSelect } from '../../../helpers/mappings/map-countries-select';
+import mapCountries from '../../../helpers/mappings/map-countries';
 import yourBuyerDetailsValidation from './validation';
 import { FIELDS } from '../../../content-strings/fields/insurance/your-buyer';
 
 export const get = async (req: Request, res: Response) => {
-  const countries = await api.external.getCountries();
+  const countries = await api.keystone.countries.getAll();
+
   if (!countries || !countries.length) {
     return res.redirect(ROUTES.PROBLEM_WITH_SERVICE);
   }
-  const mappedCountries = mapCountriesSelect(countries);
+  const mappedCountries = mapCountries(countries);
   return res.render(TEMPLATES.INSURANCE.YOUR_BUYER.BUYER_BUYER_DETAILS, {
     ...insuranceCorePageVariables({
       PAGE_CONTENT_STRINGS: PAGES.INSURANCE.YOUR_BUYER_DETAILS,
@@ -28,7 +29,7 @@ export const post = async (req: Request, res: Response) => {
   if (!countries || !countries.length) {
     return res.redirect(ROUTES.PROBLEM_WITH_SERVICE);
   }
-  const mappedCountries = mapCountriesSelect(countries);
+  const mappedCountries = mapCountries(countries);
   const validationErrors = yourBuyerDetailsValidation(req.body);
 
   if (validationErrors && Object.keys(validationErrors).length) {
