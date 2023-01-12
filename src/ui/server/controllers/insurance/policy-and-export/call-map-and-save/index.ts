d66b33ec22f24e855a845037381ad378640042a1
ttbarnes <tony@tonybarnes.me>
2023-01-12 11:49:51 +0000

    feat(EMS-607): add documentation to callMapAndSave function

--- src/ui/server/controllers/insurance/policy-and-export/call-map-and-save/index.ts
+++ src/ui/server/controllers/insurance/policy-and-export/call-map-and-save/index.ts
@@ -1,6 +1,14 @@
 import mapAndSave from '../map-and-save';
 import { Application, RequestBody, ValidationErrors } from '../../../../../types';
 
+/**
+ * callMapAndSave
+ * Call the "map and save" function with or without validation errors
+ * @param {RequestBody} Form body
+ * @param {Object} Application
+ * @param {Object} Form Validation errors
+ * @returns {Boolean}
+ */
 const callMapAndSave = async (formData: RequestBody, application: Application, validationErrors?: ValidationErrors) => {
   try {
     let saveResponse;
