2e5a701fbbfcad0f9e4b0c77dc192f86b1aa57f4
venki <vensurag@gmail.com>
2023-01-16 15:11:57 +0000

    EMS-695: Filed Ids and Added Fileds and max length

--- src/ui/server/content-strings/fields/insurance/your-buyer/index.ts
+++ src/ui/server/content-strings/fields/insurance/your-buyer/index.ts
@@ -4,6 +4,11 @@ export const FIELDS = {
       ID: 'organisation',
       LABEL: "Buyer's company or organisation name",
     },
+    BUYER_ADDRESS: {
+      ID: 'address',
+      LABEL: 'Address',
+      MAX_LENGTH: 330,
+    },
     BUYER_COUNTRY: {
       ID: 'country',
       LABEL: 'Country',
