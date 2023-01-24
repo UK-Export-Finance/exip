b3307bd5e230068253cedc9ced4cd44752e4d829
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2023-01-24 17:07:50 +0000

    EMS-827 fixed failing test

--- e2e-tests/cypress/support/navigate-to-url.js
+++ e2e-tests/cypress/support/navigate-to-url.js
@@ -1,3 +1,8 @@
 export default (url) => {
-  cy.visit(url);
+  cy.visit(url, {
+    auth: {
+      username: Cypress.config('basicAuthKey'),
+      password: Cypress.config('basicAuthSecret'),
+    },
+  });
 };
