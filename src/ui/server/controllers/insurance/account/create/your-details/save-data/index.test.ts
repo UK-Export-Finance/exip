47acfe9002be9475aff5a6ba0e9dfa10972a44bf
ttbarnes <tony@tonybarnes.me>
2023-02-03 16:06:39 +0000

    feat(EMS-794): account creation - unit test for save data function

---
+++ src/ui/server/controllers/insurance/account/create/your-details/save-data/index.test.ts
@@ -0,0 +1,49 @@
+import save from '.';
+import api from '../../../../../../api';
+import { sanitiseData } from '../../../../../../helpers/sanitise-data';
+import { mockAccount } from '../../../../../../test-mocks';
+
+describe('controllers/account/create/your-details/save-data', () => {
+  const mockCreateAccountResponse = mockAccount;
+
+  let accountCreateSpy = jest.fn(() => Promise.resolve(mockCreateAccountResponse));
+
+  const mockFormBody = mockAccount;
+
+  beforeEach(() => {
+    api.keystone.account.create = accountCreateSpy;
+  });
+
+  it('should call api.keystone.account.create form body and sanitised data', async () => {
+    await save.account(mockFormBody);
+
+    expect(accountCreateSpy).toHaveBeenCalledTimes(1);
+
+    const expectedSanitisedData = sanitiseData(mockFormBody);
+    expect(accountCreateSpy).toHaveBeenCalledWith(expectedSanitisedData);
+  });
+
+  it('should return the API response', async () => {
+    const result = await save.account(mockFormBody);
+
+    expect(result).toEqual(mockCreateAccountResponse);
+  });
+
+  describe('api error handling', () => {
+    describe('when there is an error', () => {
+      beforeEach(() => {
+        accountCreateSpy = jest.fn(() => Promise.reject());
+        api.keystone.account.create = accountCreateSpy;
+      });
+
+      it('should throw an error', async () => {
+        try {
+          await save.account(mockFormBody);
+        } catch (err) {
+          const expected = new Error('Creating exporter account');
+          expect(err).toEqual(expected);
+        }
+      });
+    });
+  });
+});
