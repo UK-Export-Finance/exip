ebc0c999d8f7032e6d430190580a0afd19fed3c0
ttbarnes <tony@tonybarnes.me>
2023-02-20 14:00:07 +0000

    feat(EMS-1046): accessibility statement content

--- e2e-tests/cypress/e2e/pages/index.js
+++ e2e-tests/cypress/e2e/pages/index.js
@@ -1,8 +1,10 @@
+const accessibilityStatementPage = require('./accessibility-statement');
 const cookiesPage = require('./cookies');
 const pageNotFoundPage = require('./pageNotFound');
 const quotePages = require('./quote');
 
 module.exports = {
+  accessibilityStatementPage,
   cookiesPage,
   pageNotFoundPage,
   quote: quotePages,
