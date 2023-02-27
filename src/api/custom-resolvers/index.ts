39c4062deb340ce6ff4eb73318ab4b615e8cc62f
ttbarnes <tony@tonybarnes.me>
2023-02-27 11:54:12 +0000

    feat(EMS-900): fix typo

--- src/api/custom-resolvers/index.ts
+++ src/api/custom-resolvers/index.ts
@@ -3,6 +3,6 @@ import verifyAccountEmailAddress from './verify-account-email-address';
 import sendEmailConfirmEmailAddress from './send-email-confirm-email-address';
 import accountSignIn from './account-sign-in';
 import verifyAccountSignInCode from './verify-account-sign-in-code';
-import addAndGetOTP from './add-and-get-otp';
+import addAndGetOTP from './add-and-get-OTP';
 
 export { createAccount, verifyAccountEmailAddress, sendEmailConfirmEmailAddress, accountSignIn, verifyAccountSignInCode, addAndGetOTP };
