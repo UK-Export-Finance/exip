2ad5ecdbe45949d97df18c57713b1fc205310953
ttbarnes <tony@tonybarnes.me>
2023-02-27 10:09:43 +0000

    feat(EMS-900): bump keystone deps

--- src/api/.keystone/config.js
+++ src/api/.keystone/config.js
@@ -18,6 +18,10 @@ var __copyProps = (to, from, except, desc) => {
   return to;
 };
 var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
   isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
   mod
 ));
@@ -65,7 +69,7 @@ var APPLICATION = {
 var ACCOUNT = {
   EMAIL: {
     VERIFICATION_EXPIRY: () => {
-      const now = new Date();
+      const now = /* @__PURE__ */ new Date();
       const day = now.getDate();
       const tomorrow = new Date(now.setDate(day + 1));
       return tomorrow;
@@ -89,15 +93,17 @@ var ACCOUNT = {
       }
     }
   },
+  // One time password
   OTP: {
     DIGITS: 6,
     VERIFICATION_EXPIRY: () => {
-      const now = new Date();
+      const now = /* @__PURE__ */ new Date();
       const milliseconds = 3e5;
       const future = new Date(now.setMilliseconds(milliseconds));
       return future;
     }
   },
+  // JSON web token
   JWT: {
     KEY: {
       SIGNATURE: String(process.env.JWT_SIGNING_KEY),
@@ -108,7 +114,7 @@ var ACCOUNT = {
       EXPIRY: "8h"
     },
     SESSION_EXPIRY: () => {
-      const now = new Date();
+      const now = /* @__PURE__ */ new Date();
       const hours = 8;
       const future = new Date(now.getTime() + hours * 60 * 60 * 1e3);
       return future;
@@ -281,7 +287,7 @@ var lists = {
                 id: exporterBrokerId
               }
             };
-            const now = new Date();
+            const now = /* @__PURE__ */ new Date();
             modifiedData.createdAt = now;
             modifiedData.updatedAt = now;
             modifiedData.submissionDeadline = (0, import_date_fns.addMonths)(new Date(now), APPLICATION.SUBMISSION_DEADLINE_IN_MONTHS);
@@ -409,6 +415,7 @@ var lists = {
       email: (0, import_fields.text)({ validation: { isRequired: true } }),
       salt: (0, import_fields.text)({ validation: { isRequired: true } }),
       hash: (0, import_fields.text)({ validation: { isRequired: true } }),
+      // isVerified flag will only be true if the exporter has verified their email address.
       isVerified: (0, import_fields.checkbox)({ defaultValue: false }),
       verificationHash: (0, import_fields.text)(),
       verificationExpiry: (0, import_fields.timestamp)(),
@@ -425,7 +432,7 @@ var lists = {
         const accountInputData = resolvedData;
         if (operation === "create") {
           console.info("Creating new exporter account");
-          const now = new Date();
+          const now = /* @__PURE__ */ new Date();
           accountInputData.createdAt = now;
           accountInputData.updatedAt = now;
           try {
@@ -441,7 +448,7 @@ var lists = {
         }
         if (operation === "update") {
           console.info("Updating exporter account");
-          accountInputData.updatedAt = new Date();
+          accountInputData.updatedAt = /* @__PURE__ */ new Date();
         }
         return accountInputData;
       }
@@ -615,6 +622,8 @@ var { withAuth } = (0, import_auth.createAuth)({
   sessionData: "name",
   secretField: "password",
   initFirstItem: {
+    // If there are no items in the database, keystone will ask you to create
+    // a new user, filling in these fields.
     fields: ["name", "email", "password"]
   }
 });
@@ -699,7 +708,7 @@ var verifyAccountEmailAddress = async (root, variables, context) => {
     console.info("Verifying exporter email address");
     const exporter = await get_account_by_field_default(context, "verificationHash", variables.token);
     if (exporter) {
-      const now = new Date();
+      const now = /* @__PURE__ */ new Date();
       const canActivateExporter = (0, import_date_fns2.isBefore)(now, exporter.verificationExpiry);
       if (!canActivateExporter) {
         console.info("Unable to verify exporter email - verification period has expired");
@@ -969,7 +978,7 @@ var verifyAccountSignInCode = async (root, variables, context) => {
       };
     }
     const { otpSalt, otpHash, otpExpiry } = exporter;
-    const now = new Date();
+    const now = /* @__PURE__ */ new Date();
     const hasExpired = (0, import_date_fns3.isAfter)(now, otpExpiry);
     if (hasExpired) {
       console.info("Unable to verify exporter account sign in code - verification period has expired");
@@ -1036,7 +1045,7 @@ var add_and_get_otp_default = addAndGetOTP;
 // helpers/create-full-timestamp-from-day-month.ts
 var createFullTimestampFromDayAndMonth = (day, month) => {
   if (day && month) {
-    return new Date(`${new Date().getFullYear()}-${month}-${day}`);
+    return /* @__PURE__ */ new Date(`${(/* @__PURE__ */ new Date()).getFullYear()}-${month}-${day}`);
   }
   return null;
 };
@@ -1059,6 +1068,7 @@ var mapCompaniesHouseFields = (companiesHouseResponse) => {
     companyNumber: companiesHouseResponse.company_number,
     dateOfCreation: companiesHouseResponse.date_of_creation,
     sicCodes: companiesHouseResponse.sic_codes,
+    // creates timestamp for financialYearEndDate from day and month if exist
     financialYearEndDate: create_full_timestamp_from_day_month_default(
       companiesHouseResponse.accounts?.accounting_reference_date?.day,
       companiesHouseResponse.accounts?.accounting_reference_date?.month
@@ -1302,6 +1312,11 @@ var extendGraphqlSchema = (schema) => (0, import_schema.mergeSchemas)({
       }
     },
     Query: {
+      /**
+       * Call for companies house API
+       * @param variables - companies house number is received as a string within variables
+       * @returns either mapped response or success false flag with or without apiError
+       */
       getCompaniesHouseInformation: async (root, variables) => {
         try {
           const { companiesHouseNumber } = variables;
