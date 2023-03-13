24eedc18ae1c92fbf5598c69fee613ddc23ecd6c
Tony Barnes <tony@tonybarnes.me>
2023-03-13 14:05:26 +0000

    feat(EMS-1137): Account sign in/creation - redirect if already logged in  (#359)
    * feat(EMS-876): application creation - associate an application with an account and only create in enter-code POST controller
    
    * feat(EMS-876): insurance - dashboard - get all applications and render in the page with a temporary list
    
    * feat(EMS-876): insurance - dashboard - get all applications and render in the page with a temporary list
    
    * feat(EMS-876): application creation - update E2E tests for eligibility flow
    
    * feat(EMS-876): linting
    
    * feat(EMS-876): address todo comment
    
    * feat(EMS-876): account creation - solution for E2E tests signing into an account before running the test
    
    * feat(EMS-876): account sign in - update all policy and export E2E tests to sign in before running
    
    * feat(EMS-876): account sign in - update all your business and your buyer E2E tests to sign in before running
    
    * feat(EMS-876): account sign in - update all sections E2E test to sign in before running
    
    * feat(EMS-876): account sign in - run delete account command in every account E2E test after tests have been run
    
    * feat(EMS-876): remove todo comment
    
    * feat(EMS-876): fix/update API unit test
    
    * feat(EMS-876): update SQL dump
    
    * feat(EMS-1137): account sign in - redirect to dashboard when visiting the sign in page and already signed in
    
    * feat(EMS-1137): account sign in - redirect to dashboard when visiting the create account page and already signed in
    
    * feat(EMS-876): fix/update E2E tests
    
    * feat(EMS-876): linting
    
    * feat(EMS-876): fix typo
    
    * feat(EMS-876): add missing documentation in UI controllers
    
    * feat(EMS-1137): account sign in - update already-signed-in E2E test url definition
    
    * feat(EMS-1137): update E2E tests
    
    * feat(EMS-1137): simplify flaky E2E test

--- e2e-tests/cypress/e2e/journeys/insurance/no-access-to-application.spec.js
+++ e2e-tests/cypress/e2e/journeys/insurance/no-access-to-application.spec.js
@@ -1,27 +1,14 @@
 import noAccessToApplicationPage from '../../pages/insurance/noAccessToApplication';
-import { enterCodePage } from '../../pages/insurance/account/sign-in';
-import { submitButton } from '../../pages/shared';
 import { PAGES } from '../../../../content-strings';
-import { FIELD_IDS } from '../../../../constants';
 import { INSURANCE_ROUTES } from '../../../../constants/routes/insurance';
-import account from '../../../fixtures/account';
 
 const CONTENT_STRINGS = PAGES.INSURANCE.NO_ACCESS_TO_APPLICATION_PAGE;
 
 const {
   ROOT,
-  DASHBOARD,
   ALL_SECTIONS,
   NO_ACCESS_TO_APPLICATION,
 } = INSURANCE_ROUTES;
-
-const {
-  INSURANCE: {
-    ACCOUNT: { SECURITY_CODE },
-  },
-} = FIELD_IDS;
-
-const secondAccountEmail = Cypress.env('GOV_NOTIFY_EMAIL_RECIPIENT_2');
 
 context('Insurance - no access to application page', () => {
   let referenceNumber;
@@ -37,50 +24,18 @@ context('Insurance - no access to application page', () => {
       url = `${Cypress.config('baseUrl')}${ROOT}/${referenceNumber}${ALL_SECTIONS}`;
 
       cy.url().should('eq', url);
-
-      // create and sign into a different account and try to access to the application
-      const {
-        firstName,
-        lastName,
-        password,
-      } = account;
-
-      return cy.createAccount(firstName, lastName, secondAccountEmail, password).then((verifyAccountUrl) => {
-        // verify the account by navigating to the "verify account" page
-        cy.navigateToUrl(verifyAccountUrl);
-
-        // sign in to the account. Behind the scenes, an application is created at this point.
-        cy.completeAndSubmitSignInAccountForm(secondAccountEmail, password);
-
-        // get the OTP security code
-        return cy.accountAddAndGetOTP(secondAccountEmail).then((securityCode) => {
-          cy.keyboardInput(enterCodePage[SECURITY_CODE].input(), securityCode);
-
-          // submit the OTP security code
-          submitButton().click();
-
-          // assert we are on the dashboard
-          const expectedUrl = `${Cypress.config('baseUrl')}${DASHBOARD}`;
-          cy.url().should('eq', expectedUrl);
-        });
-      });
     });
   });
 
-  beforeEach(() => {
-    cy.saveSession();
-  });
-
   after(() => {
-    // delete first account (uses default GOV_NOTIFY_EMAIL_RECIPIENT email)
     cy.deleteAccount();
-
-    // delete second account
-    cy.deleteAccount(secondAccountEmail);
   });
 
-  describe('when trying to access an application created by the previous user', () => {
+  describe('when trying to access an application created by another user', () => {
     beforeEach(() => {
+      // clear the session - means we are not a logged in user.
+      cy.clearCookie('exip-session');
+
       cy.navigateToUrl(url);
     });
 
