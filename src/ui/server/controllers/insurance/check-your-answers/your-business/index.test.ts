c4d9030be00ae2cf8d94226ea6c48fb91ddbd965
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2023-03-17 15:51:17 +0000

    EMS-1139-1140 added missing test

---
+++ src/ui/server/controllers/insurance/check-your-answers/your-business/index.test.ts
@@ -0,0 +1,95 @@
+import { get, post, TEMPLATE } from '.';
+import { PAGES } from '../../../../content-strings';
+import { ROUTES, TEMPLATES } from '../../../../constants';
+import { Request, Response } from '../../../../../types';
+import insuranceCorePageVariables from '../../../../helpers/page-variables/core/insurance';
+import { yourBusinessSummaryList } from '../../../../helpers/summary-lists/your-business';
+import { mockReq, mockRes, mockApplication } from '../../../../test-mocks';
+
+const CHECK_YOUR_ANSWERS_TEMPLATE = TEMPLATES.INSURANCE.CHECK_YOUR_ANSWERS;
+
+const {
+  PROBLEM_WITH_SERVICE,
+  INSURANCE: {
+    INSURANCE_ROOT,
+    CHECK_YOUR_ANSWERS: { YOUR_BUYER, YOUR_BUSINESS_SAVE_AND_BACK },
+  },
+} = ROUTES;
+
+describe('controllers/insurance/check-your-answers/your-business', () => {
+  let req: Request;
+  let res: Response;
+
+  beforeEach(() => {
+    req = mockReq();
+    res = mockRes();
+
+    res.locals.application = mockApplication;
+    req.params.referenceNumber = String(mockApplication.referenceNumber);
+  });
+
+  describe('TEMPLATE', () => {
+    it('should have the correct template defined', () => {
+      expect(TEMPLATE).toEqual(CHECK_YOUR_ANSWERS_TEMPLATE);
+    });
+  });
+
+  describe('get', () => {
+    it('should render template', async () => {
+      await get(req, res);
+      const checkAndChange = true;
+      const summaryList = yourBusinessSummaryList(
+        mockApplication.exporterCompany,
+        mockApplication.exporterBusiness,
+        mockApplication.exporterBroker,
+        mockApplication.referenceNumber,
+        checkAndChange,
+      );
+
+      const expectedVariables = {
+        ...insuranceCorePageVariables({
+          PAGE_CONTENT_STRINGS: PAGES.INSURANCE.CHECK_YOUR_ANSWERS.YOUR_BUSINESS,
+          BACK_LINK: req.headers.referer,
+        }),
+        SUMMARY_LIST: summaryList,
+        SAVE_AND_BACK_URL: `${INSURANCE_ROOT}/${mockApplication.referenceNumber}${YOUR_BUSINESS_SAVE_AND_BACK}`,
+      };
+
+      expect(res.render).toHaveBeenCalledWith(TEMPLATE, expectedVariables);
+    });
+
+    describe('when there is no application', () => {
+      beforeEach(() => {
+        res.locals = { csrfToken: '1234' };
+      });
+
+      it(`should redirect to ${PROBLEM_WITH_SERVICE}`, async () => {
+        await get(req, res);
+
+        expect(res.redirect).toHaveBeenCalledWith(PROBLEM_WITH_SERVICE);
+      });
+    });
+  });
+
+  describe('post', () => {
+    it(`should redirect to ${YOUR_BUYER}`, () => {
+      post(req, res);
+
+      const expected = `${INSURANCE_ROOT}/${req.params.referenceNumber}${YOUR_BUYER}`;
+
+      expect(res.redirect).toHaveBeenCalledWith(expected);
+    });
+
+    describe('when there is no application', () => {
+      beforeEach(() => {
+        res.locals = { csrfToken: '1234' };
+      });
+
+      it(`should redirect to ${PROBLEM_WITH_SERVICE}`, async () => {
+        await post(req, res);
+
+        expect(res.redirect).toHaveBeenCalledWith(PROBLEM_WITH_SERVICE);
+      });
+    });
+  });
+});
