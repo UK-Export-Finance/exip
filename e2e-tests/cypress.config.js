66ca7bbdb8786b7fcc9cc96105e2154e0ca1bd2c
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-03-20 10:07:23 +0000

    chore(private runners): E2E execution on private runners and added various Cypress configurations

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -6,10 +6,23 @@ dotenv.config();
 const cypressConfig = defineConfig({
   projectId: 'sefntb',
   numTestsKeptInMemory: 1,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   basicAuthKey: 'dev',
   basicAuthSecret: 'test',
   chromeWebSecurity: false,
+  retries: {
+    runMode: 3,
+    openMode: 0,
+  },
   e2e: {
+    pageLoadTimeout: 120000,
+    responseTimeout: 120000,
+    baseUrl: 'http://localhost:5000',
+    apiUrl: 'http://localhost:5001/api/graphql',
+    specPattern: 'cypress/e2e/**/*.spec.js',
+    experimentalSessionAndOrigin: true,
+    testIsolation: 'off',
     env: {
       GOV_NOTIFY_EMAIL_RECIPIENT_1: process.env.GOV_NOTIFY_EMAIL_RECIPIENT_1,
       GOV_NOTIFY_EMAIL_RECIPIENT_2: process.env.GOV_NOTIFY_EMAIL_RECIPIENT_2,
@@ -18,10 +31,6 @@ const cypressConfig = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins')(on, config); // eslint-disable-line global-require
     },
-    baseUrl: 'http://localhost:5000',
-    apiUrl: 'http://localhost:5001/api/graphql',
-    specPattern: 'cypress/e2e/**/*.spec.js',
-    experimentalSessionAndOrigin: true,
   },
 });
 
