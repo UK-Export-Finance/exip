aca4f3f309cbaf5a06b5abe64f1a7dcc84947255
ttbarnes <tony@tonybarnes.me>
2023-03-24 12:51:47 +0000

    feat(EMS-1137-1138): fix/update required fields for exporter business section

--- src/ui/server/helpers/required-fields/exporter-business/index.test.ts
+++ src/ui/server/helpers/required-fields/exporter-business/index.test.ts
@@ -1,6 +1,10 @@
-import { FIELD_IDS } from '../../../constants';
+import FIELD_IDS from '../../../constants/field-ids/insurance';
 import { mockApplication } from '../../../test-mocks';
 import requiredFields, { getBrokerTasks } from '.';
+
+const {
+  EXPORTER_BUSINESS: { COMPANY_HOUSE, YOUR_COMPANY, NATURE_OF_YOUR_BUSINESS, TURNOVER, BROKER },
+} = FIELD_IDS;
 
 const {
   COMPANY_ADDRESS,
@@ -11,41 +15,37 @@ const {
   COMPANY_INCORPORATED,
   FINANCIAL_YEAR_END_DATE: FINANCIAL_YEAR_END_DATE_COMPANY_HOUSE,
   ...COMPANIES_HOUSE_FIELDS
-} = FIELD_IDS.INSURANCE.EXPORTER_BUSINESS.COMPANY_HOUSE;
-const { PHONE_NUMBER, WEBSITE, YOUR_BUSINESS, ...YOUR_COMPANY_FIELDS } = FIELD_IDS.INSURANCE.EXPORTER_BUSINESS.YOUR_COMPANY;
-const { FINANCIAL_YEAR_END_DATE, ...TURNOVER_FIELDS } = FIELD_IDS.INSURANCE.EXPORTER_BUSINESS.TURNOVER;
+} = COMPANY_HOUSE;
+
+const { PHONE_NUMBER, WEBSITE, YOUR_BUSINESS, ...YOUR_COMPANY_FIELDS } = YOUR_COMPANY;
+const { FINANCIAL_YEAR_END_DATE, ...TURNOVER_FIELDS } = TURNOVER;
+const { USING_BROKER, NAME, ADDRESS_LINE_1, TOWN, POSTCODE, EMAIL } = BROKER;
 
 describe('server/helpers/required-fields/exporter-business', () => {
   describe('getBrokerTasks', () => {
-    const { USING_BROKER, NAME, ADDRESS_LINE_1, TOWN, POSTCODE, EMAIL } = FIELD_IDS.INSURANCE.EXPORTER_BUSINESS.BROKER;
-
     describe('when isUsingBroker is "Yes"', () => {
       it('should return multiple field ids in an array', () => {
         const result = getBrokerTasks('Yes');
 
-        const expected = [USING_BROKER, NAME, ADDRESS_LINE_1, TOWN, POSTCODE, EMAIL];
-
-        expect(result).toEqual(expected);
-      });
-    });
-
-    describe('when isUsingBroker is "No"', () => {
-      it('should return a single field id in an array', () => {
-        const result = getBrokerTasks('No');
-
-        const expected = [USING_BROKER];
+        const expected = [NAME, ADDRESS_LINE_1, TOWN, POSTCODE, EMAIL];
 
         expect(result).toEqual(expected);
       });
     });
 
     describe('when isUsingBroker is undefined', () => {
-      it('should return a single field id in an array', () => {
+      it('should return an empty array', () => {
         const result = getBrokerTasks();
 
-        const expected = [USING_BROKER];
+        expect(result).toEqual([]);
+      });
+    });
 
-        expect(result).toEqual(expected);
+    describe('when isUsingBroker is "No"', () => {
+      it('should return an empty array', () => {
+        const result = getBrokerTasks('No');
+
+        expect(result).toEqual([]);
       });
     });
   });
@@ -57,8 +57,9 @@ describe('server/helpers/required-fields/exporter-business', () => {
       const expected = Object.values({
         ...YOUR_COMPANY_FIELDS,
         ...COMPANIES_HOUSE_FIELDS,
-        ...FIELD_IDS.INSURANCE.EXPORTER_BUSINESS.NATURE_OF_YOUR_BUSINESS,
+        ...NATURE_OF_YOUR_BUSINESS,
         ...TURNOVER_FIELDS,
+        USING_BROKER,
         ...getBrokerTasks(mockApplication.exporterBroker.isUsingBroker),
       });
 
