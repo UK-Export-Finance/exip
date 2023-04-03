eef38d9a3ba8bc29eb6cfeb83a229116faba0e36
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-04-03 18:02:47 +0100

    fix(e2e): used native JS month calculation

--- e2e-tests/cypress/fixtures/application.js
+++ e2e-tests/cypress/fixtures/application.js
@@ -90,8 +90,15 @@ const {
  * Application data we use and assert in E2E tests.
  */
 const date = new Date();
-export const startDate = add(date, { months: 3 });
-export const endDate = add(startDate, { months: 6 });
+
+/**
+ * Add months to the above date.
+ * If out of bound (< 12), then date will
+ * move on the following year.
+ * Note: JS months range (0 - 11)
+ */
+export const startDate = new Date(date.setMonth((date.getMonth() + 3))); // Add 3 months
+export const endDate = new Date(date.setMonth((date.getMonth() + 6))); // Add 6 months
 
 const application = {
   ELIGIBILITY: {
