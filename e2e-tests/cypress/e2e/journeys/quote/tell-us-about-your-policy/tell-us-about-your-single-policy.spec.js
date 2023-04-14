5a3281c57bf924003147e6d7135733df1d6cfecb
ttbarnes <tony@tonybarnes.me>
2023-04-14 14:05:27 +0100

    chore(EMS-1318): simplify E2E test after merge

--- e2e-tests/cypress/e2e/journeys/quote/tell-us-about-your-policy/tell-us-about-your-single-policy.spec.js
+++ e2e-tests/cypress/e2e/journeys/quote/tell-us-about-your-policy/tell-us-about-your-single-policy.spec.js
@@ -5,15 +5,9 @@ import {
   completeAndSubmitUkContentForm,
   completeAndSubmitPolicyTypeSingleForm,
 } from '../../../../support/quote/forms';
-import partials from '../../../partials';
 import { submitButton } from '../../../pages/shared';
 import { tellUsAboutYourPolicyPage } from '../../../pages/quote';
-import {
-  FIELDS,
-  LINKS,
-  ORGANISATION,
-  PAGES,
-} from '../../../../../content-strings';
+import { FIELDS, PAGES } from '../../../../../content-strings';
 import { ROUTES, FIELD_IDS, SUPPORTED_CURRENCIES } from '../../../../../constants';
 import { GBP_CURRENCY_CODE } from '../../../../fixtures/currencies';
 
@@ -50,50 +44,6 @@ context('Tell us about your single policy page - as an exporter, I want to provi
   describe('page tests', () => {
     beforeEach(() => {
       cy.navigateToUrl(url);
-    });
-
-    it('passes the audits', () => {
-      cy.lighthouse({
-        // accessibility threshold is reduced here because
-        // the radio component from design system has an invalid aria attribute.
-        // this is out of our control
-        accessibility: 92,
-        performance: 75,
-        'best-practices': 100,
-        seo: 60,
-      });
-    });
-
-    it('renders a back link with correct url', () => {
-      partials.backLink().should('exist');
-      partials.backLink().invoke('text').then((text) => {
-        expect(text.trim()).equal(LINKS.BACK);
-      });
-
-      const expected = `${Cypress.config('baseUrl')}${ROUTES.QUOTE.POLICY_TYPE}`;
-
-      partials.backLink().should('have.attr', 'href', expected);
-    });
-
-    it('renders an analytics cookies consent banner that can be accepted', () => {
-      cy.checkAnalyticsCookiesConsentAndAccept();
-    });
-
-    it('renders an analytics cookies consent banner that can be rejected', () => {
-      cy.rejectAnalyticsCookies();
-    });
-
-    it('renders a phase banner', () => {
-      cy.checkPhaseBanner();
-    });
-
-    it('renders a page title and heading', () => {
-      const expectedPageTitle = `${CONTENT_STRINGS.SINGLE_POLICY_PAGE_TITLE} - ${ORGANISATION}`;
-      cy.title().should('eq', expectedPageTitle);
-
-      tellUsAboutYourPolicyPage.heading().invoke('text').then((text) => {
-        expect(text.trim()).equal(CONTENT_STRINGS.SINGLE_POLICY_HEADING);
-      });
     });
 
     it('renders `currency and amount` legend', () => {
