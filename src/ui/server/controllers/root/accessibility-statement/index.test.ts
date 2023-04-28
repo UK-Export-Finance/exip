3abbc525a20494efc18b91e42e23b8cedcc5f301
ttbarnes <tony@tonybarnes.me>
2023-04-28 10:33:52 +0100

    fix(EMS-1536): accessibility statement - fix servier header name/link

--- src/ui/server/controllers/root/accessibility-statement/index.test.ts
+++ src/ui/server/controllers/root/accessibility-statement/index.test.ts
@@ -1,6 +1,7 @@
 import { get } from '.';
-import { BUTTONS, COOKIES_CONSENT, FOOTER, LINKS, PRODUCT, PAGES, PHASE_BANNER } from '../../../content-strings';
+import { PAGES } from '../../../content-strings';
 import { TEMPLATES } from '../../../constants';
+import corePageVariables from '../../../helpers/page-variables/core/quote';
 import { mockReq, mockRes } from '../../../test-mocks';
 import { Request, Response } from '../../../../types';
 
@@ -17,19 +18,13 @@ describe('controllers/root/accessibility-statement', () => {
     it('should render template', () => {
       get(req, res);
 
-      expect(res.render).toHaveBeenCalledWith(TEMPLATES.ACCESSIBILITY_STATEMENT, {
-        CONTENT_STRINGS: {
-          COOKIES_CONSENT,
-          BUTTONS,
-          FOOTER,
-          LINKS,
-          PRODUCT,
-          PHASE_BANNER,
-          ...PAGES.ACCESSIBILITY_STATEMENT_PAGE,
-        },
-        BACK_LINK: req.headers.referer,
-        FEEDBACK_ROUTE: LINKS.EXTERNAL.FEEDBACK,
-      });
+      expect(res.render).toHaveBeenCalledWith(
+        TEMPLATES.ACCESSIBILITY_STATEMENT,
+        corePageVariables({
+          PAGE_CONTENT_STRINGS: PAGES.ACCESSIBILITY_STATEMENT_PAGE,
+          BACK_LINK: req.headers.referer,
+        }),
+      );
     });
   });
 });
