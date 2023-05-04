9c08768fef0ba21469e9bf7a5ec9e344151959b7
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2023-05-04 15:48:40 +0100

    EMS-1522-1555 used an existing command to submit application eligibility from start new application button

--- e2e-tests/cypress/support/insurance/eligibility/submit-answers-happy-path.js
+++ e2e-tests/cypress/support/insurance/eligibility/submit-answers-happy-path.js
@@ -2,15 +2,8 @@ import submitInsuranceEligibilityAnswersFromBuyerCountryHappyPath from './submit
 
 import { completeStartForm, completeCheckIfEligibleForm } from './forms';
 
-/**
- * startNewApplication defaults to false
- * if true, means on the start new application page from check your answers which redirects to buyer country page
- * if true, then do not need to complete start form or check if eligible
- */
-export default (startNewApplication = false) => {
-  if (!startNewApplication) {
-    completeStartForm();
-    completeCheckIfEligibleForm();
-  }
+export default () => {
+  completeStartForm();
+  completeCheckIfEligibleForm();
   submitInsuranceEligibilityAnswersFromBuyerCountryHappyPath();
 };
