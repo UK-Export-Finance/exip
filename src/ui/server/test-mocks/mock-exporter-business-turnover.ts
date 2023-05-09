3984378e67b0a2495cace37d8468046d8ad176a0
ttbarnes <tony@tonybarnes.me>
2023-05-09 15:16:45 +0100

    chore(tech): fix typo/merge conflict issue

--- src/ui/server/test-mocks/mock-exporter-business-turnover.ts
+++
@@ -1,8 +0,0 @@
-import { Business } from '../../types';
-
-const mockBusinessTurnover = {
-  estimatedAnnualTurnover: '155220',
-  exportsTurnoverPercentage: '2,0',
-} as Business;
-
-export default mockBusinessTurnover;
