a8d2d9920163765115472fc4ea8d79f555c1831f
ttbarnes <tony@tonybarnes.me>
2023-05-10 13:11:37 +0100

    chore(EMS-1526): add temporary logs to debug submission issue in dev environment

--- src/api/helpers/get-application-submitted-email-template-ids/index.ts
+++ src/api/helpers/get-application-submitted-email-template-ids/index.ts
@@ -40,6 +40,13 @@ const getApplicationSubmittedEmailTemplateIds = (application: Application) => {
     templateIds.underwritingTeam = UNDERWRITING_TEAM.NOTIFICATION_ANTI_BRIBERY_AND_TRADING_HISTORY;
   }
 
+
+  console.log('------- temporary debugging dev environment - buyer data \n', buyer);
+  console.log('------- temporary debugging dev environment - declaration data \n', declaration);
+  console.log('------- temporary debugging dev environment - hasAntiBriberyCodeOfConduct? ', hasAntiBriberyCodeOfConduct);
+  console.log('------- temporary debugging dev environment - isConnectedWithBuyer? ', isConnectedWithBuyer);
+  console.log('------- temporary debugging dev environment - templateIds \n', templateIds);
+
   return templateIds;
 };
 
