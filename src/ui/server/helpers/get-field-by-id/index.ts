833ee382cb5d5777dc3f2ef9e934206e322a8376
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2023-05-17 16:31:37 +0100

    EMS-1527 added documentation

--- src/ui/server/helpers/get-field-by-id/index.ts
+++ src/ui/server/helpers/get-field-by-id/index.ts
@@ -4,13 +4,15 @@ import { objectHasProperty } from '../object';
  * getFieldById
  * Get a field from an object by the field ID
  * if field has optional prefix, then add to the id
- * @param {Object} Fields
- * @param {String} Field ID
+ * @param {Object} fields
+ * @param {String} fieldId ID
+ * @param {String} fieldIdPrefix optional field if prefix
  * @returns {Object} Field or an object with the provided field ID.
  */
 const getFieldById = (fields: object, fieldId: string, fieldIdPrefix?: string) => {
   if (objectHasProperty(fields, fieldId)) {
     return {
+      // if prefix, then add prefix before fieldId, else set id as fieldId
       id: fieldIdPrefix ? `${fieldIdPrefix}-${fieldId}` : fieldId,
       ...fields[fieldId],
     };
