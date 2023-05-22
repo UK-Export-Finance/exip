01e400a6baa808d8d24721437e1c002b1628e5eb
ttbarnes <tony@tonybarnes.me>
2023-05-22 11:24:45 +0100

    feat(EMS-1537): improve API unit test coverage

--- src/api/helpers/create-authentication-entry/index.ts
+++ src/api/helpers/create-authentication-entry/index.ts
@@ -1,11 +1,21 @@
 import { Context } from '.keystone/types'; // eslint-disable-line
 
+/**
+ * createAuthenticationEntry
+ * Create an entry in the Authentication table
+ * @param {Object} KeystoneJS context API
+ * @param {String} Account ID
+ * @returns {Boolean}
+ */
 const createAuthenticationEntry = async (context: Context, entry: object) => {
   console.info('Creating authentication entry');
 
   try {
     const result = await context.db.Authentication.createOne({
-      data: entry,
+      data: {
+        ...entry,
+        createdAt: new Date(),
+      },
     });
 
     return result;
