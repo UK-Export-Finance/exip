bee168ad85ca76ef814cd0a26683a614a84405a3
Tony Barnes <tony@tonybarnes.me>
2023-05-25 14:56:21 +0100

    feat(EMS-1662): Account password reset - reset auth retries (#499)
    * feat(EMS-1662): account password reset - wipe retries after successful password reset
    
    * feat(EMS-1662): improve API unit test coverage
    
    * feat(EMS-1662): fix/update API unit test

--- src/api/.keystone/config.js
+++ src/api/.keystone/config.js
@@ -1794,7 +1794,7 @@ var getAuthenticationRetriesByAccountId = async (context, accountId) => {
     return retries;
   } catch (err) {
     console.error(err);
-    throw new Error(`Getting authentication retries ${err}`);
+    throw new Error(`Getting authentication retries by account ID ${err}`);
   }
 };
 var get_authentication_retries_by_account_id_default = getAuthenticationRetriesByAccountId;
@@ -2551,6 +2551,7 @@ var accountPasswordReset = async (root, variables, context) => {
         hasBeenUsedBefore: true
       };
     }
+    await delete_authentication_retries_default(context, accountId);
     const authEntry = {
       account: {
         connect: {
