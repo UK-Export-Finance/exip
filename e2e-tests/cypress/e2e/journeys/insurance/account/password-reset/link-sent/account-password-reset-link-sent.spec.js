1459f7e021ba9ff45f507fb65fc0a6cf87068aef
ttbarnes <tony@tonybarnes.me>
2023-05-31 12:04:58 +0100

    chore(tech): add missing deleteAccount cypress command to password reset E2E test

--- e2e-tests/cypress/e2e/journeys/insurance/account/password-reset/link-sent/account-password-reset-link-sent.spec.js
+++ e2e-tests/cypress/e2e/journeys/insurance/account/password-reset/link-sent/account-password-reset-link-sent.spec.js
@@ -37,6 +37,8 @@ const goToPasswordResetLinkSentPage = () => {
 
 context('Insurance - Account - Password reset - link sent page - As an Exporter, I want to reset the password on my UKEF digital service account, So that I can securely access my UKEF digital service account', () => {
   before(() => {
+    cy.deleteAccount();
+
     cy.navigateToUrl(START);
     cy.submitEligibilityAndStartAccountCreation();
     cy.completeAndSubmitCreateAccountForm();
@@ -44,10 +46,6 @@ context('Insurance - Account - Password reset - link sent page - As an Exporter,
 
   beforeEach(() => {
     cy.saveSession();
-  });
-
-  after(() => {
-    cy.deleteAccount();
   });
 
   describe('when visiting the page', () => {
