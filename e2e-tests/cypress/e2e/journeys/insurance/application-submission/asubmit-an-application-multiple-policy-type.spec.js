6f4f1ef2aa0dddd697f1e1497f30e3b9797713a9
ttbarnes <tony@tonybarnes.me>
2023-06-02 11:30:58 +0100

    chore(tech): submit application E2E tests - create single/multiple versions

---
+++ e2e-tests/cypress/e2e/journeys/insurance/application-submission/asubmit-an-application-multiple-policy-type.spec.js
@@ -0,0 +1,45 @@
+import dashboardPage from '../../../pages/insurance/dashboard';
+import header from '../../../partials/header';
+import { APPLICATION } from '../../../../../constants';
+import { INSURANCE_ROUTES } from '../../../../../constants/routes/insurance';
+
+const { table } = dashboardPage;
+
+const {
+  ROOT: INSURANCE_ROOT,
+  DASHBOARD,
+  APPLICATION_SUBMITTED,
+} = INSURANCE_ROUTES;
+
+context('Insurance - submit an application multiple policy type - As an Exporter, I want to submit my completed export insurance application, So that UKEF can process and make a decision on my application', () => {
+  let referenceNumber;
+  let url;
+
+  const dashboardUrl = `${Cypress.config('baseUrl')}${DASHBOARD}`;
+
+  before(() => {
+    cy.completeSignInAndSubmitAnApplication({
+      policyType: APPLICATION.POLICY_TYPE.MULTIPLE
+    }).then((refNumber) => {
+      referenceNumber = refNumber;
+
+      url = `${Cypress.config('baseUrl')}${INSURANCE_ROOT}/${referenceNumber}${APPLICATION_SUBMITTED}`;
+
+      cy.url().should('eq', url);
+    });
+  });
+
+  beforeEach(() => {
+    cy.saveSession();
+  });
+
+  after(() => {
+    cy.deleteApplication(referenceNumber);
+  });
+
+  it(`should redirect to ${APPLICATION_SUBMITTED}`, () => {
+    const expectedUrl = `${Cypress.config('baseUrl')}${INSURANCE_ROOT}/${referenceNumber}${APPLICATION_SUBMITTED}`;
+
+    cy.url().should('eq', expectedUrl);
+  });
+});
