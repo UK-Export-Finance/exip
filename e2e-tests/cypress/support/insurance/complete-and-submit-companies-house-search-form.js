b83746b0728217c1c0aebd476fd8d7c3f26472d0
Zain Kassam <zain.kassam@ukexportfinance.gov.uk>
2023-06-06 10:51:52 +0100

    EMS-892 added documentation

--- e2e-tests/cypress/support/insurance/complete-and-submit-companies-house-search-form.js
+++ e2e-tests/cypress/support/insurance/complete-and-submit-companies-house-search-form.js
@@ -1,5 +1,10 @@
 import { companyDetails } from '../../e2e/pages/your-business';
 
+/**
+ * completeAndSubmitCompaniesHouseSearchForm
+ * completes the companies house search form and presses the search button
+ * @param {Object} companyHouseNumberVariables
+ */
 const completeAndSubmitCompaniesHouseSearchForm = ({ companiesHouseNumber }) => {
   cy.keyboardInput(companyDetails.companiesHouseSearch(), companiesHouseNumber);
   companyDetails.companiesHouseSearchButton().click();
