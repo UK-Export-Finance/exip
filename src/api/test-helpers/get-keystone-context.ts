c347fd4fe86b4b22b7a6d6902fe5ee788b0f088a
ttbarnes <tony@tonybarnes.me>
2023-06-13 13:27:57 +0100

    chore(EMS-1721): remove unnecessary getKeystoneContext context type

--- src/api/test-helpers/get-keystone-context.ts
+++ src/api/test-helpers/get-keystone-context.ts
@@ -2,7 +2,6 @@ import { getContext } from '@keystone-6/core/context';
 import dotenv from 'dotenv';
 import * as PrismaModule from '.prisma/client'; // eslint-disable-line import/no-extraneous-dependencies
 import baseConfig from '../keystone';
-import { Context } from '.keystone/types'; // eslint-disable-line import/no-extraneous-dependencies
 
 dotenv.config();
 
@@ -17,7 +16,7 @@ const DB_URL = String(process.env.DATABASE_URL);
 const getKeystoneContext = () => {
   const config = { ...baseConfig, db: { ...baseConfig.db, url: DB_URL } };
 
-  const context = getContext(config, PrismaModule) as Context;
+  const context = getContext(config, PrismaModule);
 
   return context;
 };
