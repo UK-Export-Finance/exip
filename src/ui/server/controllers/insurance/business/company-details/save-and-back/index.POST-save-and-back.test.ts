054bf2ded276213fdbc4657ffc73a895b7e23986
ttbarnes <tony@tonybarnes.me>
2023-07-24 10:40:09 +0100

    chore(EMS-1803): simplify changes from data saving PR

--- src/ui/server/controllers/insurance/business/company-details/save-and-back/index.POST-save-and-back.test.ts
+++ src/ui/server/controllers/insurance/business/company-details/save-and-back/index.POST-save-and-back.test.ts
@@ -79,26 +79,6 @@ describe('controllers/insurance/business/companies-details', () => {
 
         expect(res.redirect).toHaveBeenCalledWith(`${INSURANCE_ROOT}/${req.params.referenceNumber}${ALL_SECTIONS}`);
       });
-
-      it('should call mapAndSave.companyDetails once with the data from constructPayload function and company', async () => {
-        req.body = {
-          ...body,
-          injection: 1,
-        };
-
-        await post(req, res);
-
-        const payload = constructPayload(req.body, FIELD_IDS);
-
-        expect(updateMapAndSave).toHaveBeenCalledTimes(1);
-
-        const updateBody = {
-          ...payload,
-          ...mockCompany,
-        };
-
-        expect(updateMapAndSave).toHaveBeenCalledWith(updateBody, mockApplication, {});
-      });
     });
 
     describe('when there is no application', () => {
