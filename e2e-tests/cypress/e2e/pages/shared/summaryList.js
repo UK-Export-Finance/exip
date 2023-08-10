63d204b72b0e2e152df75d5c3a59244509700865
ttbarnes <tony@tonybarnes.me>
2023-08-10 14:40:36 +0100

    chore(EMS-1298): fix typo

--- e2e-tests/cypress/e2e/pages/shared/summaryList.js
+++ e2e-tests/cypress/e2e/pages/shared/summaryList.js
@@ -1,7 +1,9 @@
-export const summaryList = {
+const summaryList = {
   field: (fieldId) => ({
     key: () => cy.get(`.${fieldId}-key`),
     value: () => cy.get(`.${fieldId}-value`),
     changeLink: () => cy.get(`[data-cy="${fieldId}-change-link"]`),
   }),
 };
+
+export default summaryList;
