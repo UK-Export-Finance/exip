4721d8fee1a5c47a200965d24ced5b9d679f8d9b
ttbarnes <tony@tonybarnes.me>
2023-08-16 14:03:49 +0100

    feat(EMS-1918): quote - policy type - copy updates

--- e2e-tests/content-strings/fields/index.js
+++ e2e-tests/content-strings/fields/index.js
@@ -105,13 +105,22 @@ export const FIELDS = {
         ID: FIELD_IDS.SINGLE_POLICY_TYPE,
         VALUE: FIELD_VALUES.POLICY_TYPE.SINGLE,
         TEXT: 'Single contract policy',
-        HINT: 'This covers a single export contract with a single buyer for one or more shipments. You need to pay before the policy starts.',
+        HINT: [
+          'Covers a single contract with a buyer, for one or more shipments',
+          'Cover for up to 2 years',
+          'Best for a one off- project, when you know the exact value of your export contract now',
+          'You pay for the insurance before the policy starts',
+        ],
       },
       MULTIPLE: {
         ID: FIELD_IDS.MULTIPLE_POLICY_TYPE,
         VALUE: FIELD_VALUES.POLICY_TYPE.MULTIPLE,
-        TEXT: 'Multiple contract policy (also known as a revolving policy)',
-        HINT: "This covers multiple export contracts with the same buyer for a policy period of 12 months. You do not need to pay before the policy starts. You'll pay each time you declare a new export sale.",
+        TEXT: 'Multiple contract policy',
+        HINT: [
+          'Covers multiple contracts with the same buyer, usually for 12 months',
+          "Best if you'll have an ongoing relationship with the buyer but you're not sure yet how many contracts or sales you'll have",
+          'You only pay for your insurance each time you declare a new contract or sale - no need to pay before the policy starts',
+        ],
         INSET: [
           [
             {
--- e2e-tests/pages/quote/policyType.js
+++ e2e-tests/pages/quote/policyType.js
@@ -3,23 +3,23 @@ import {
   FIELD_VALUES,
 } from '../../constants';
 
-const { POLICY_TYPE, SINGLE_POLICY_LENGTH } = FIELD_IDS;
+const { POLICY_TYPE, SINGLE_POLICY_TYPE, MULTIPLE_POLICY_TYPE, SINGLE_POLICY_LENGTH } = FIELD_IDS;
+
+const {
+  POLICY_TYPE: { SINGLE, MULTIPLE },
+} = FIELD_VALUES;
 
 const policyTypePage = {
   [POLICY_TYPE]: {
     single: {
-      label: () => cy.get(`[data-cy="${POLICY_TYPE}-${FIELD_VALUES.POLICY_TYPE.SINGLE}-label"]`),
-      hint: () => cy.get(`[data-cy="${POLICY_TYPE}-${FIELD_VALUES.POLICY_TYPE.SINGLE}-hint"]`),
-      input: () => cy.get(`[data-cy="${POLICY_TYPE}-${FIELD_VALUES.POLICY_TYPE.SINGLE}-input"]`),
+      label: () => cy.get(`[data-cy="${SINGLE_POLICY_TYPE}-label"]`),
+      hintListItem: (index) => cy.get(`[data-cy="${SINGLE_POLICY_TYPE}-hint-list-item-${index}"]`),
+      input: () => cy.get(`[data-cy="${SINGLE_POLICY_TYPE}-input"]`),
     },
     multiple: {
-      label: () => cy.get(`[data-cy="${POLICY_TYPE}-${FIELD_VALUES.POLICY_TYPE.MULTIPLE}-label"]`),
-      hint: () => cy.get(`[data-cy="${POLICY_TYPE}-${FIELD_VALUES.POLICY_TYPE.MULTIPLE}-hint"]`),
-      input: () => cy.get(`[data-cy="${POLICY_TYPE}-${FIELD_VALUES.POLICY_TYPE.MULTIPLE}-input"]`),
-      inset: {
-        text: () => cy.get('[data-cy="multiple-policy-inset"] [data-cy="details-1"]'),
-        link: () => cy.get('[data-cy="multiple-policy-inset"] [data-cy="details-1"] a').eq(0),
-      },
+      label: () => cy.get(`[data-cy="${MULTIPLE_POLICY_TYPE}-label"]`),
+      hintListItem: (index) => cy.get(`[data-cy="${MULTIPLE_POLICY_TYPE}-hint-list-item-${index}"]`),
+      input: () => cy.get(`[data-cy="${MULTIPLE_POLICY_TYPE}-input"]`),
     },
     errorMessage: () => cy.get(`[data-cy="${POLICY_TYPE}-error-message"]`),
   },
