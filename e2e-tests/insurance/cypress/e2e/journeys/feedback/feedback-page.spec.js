752e0bd41645517a47fe3b955846cf0dd70aadaf
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2023-09-06 11:52:55 +0100

    chore(temp-cypress-fix): test with failing e2e test

--- e2e-tests/insurance/cypress/e2e/journeys/feedback/feedback-page.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/feedback/feedback-page.spec.js
@@ -76,7 +76,7 @@ context('Insurance - Feedback - As an exporter I want to give feedback on the UK
 
       cy.checkText(field.label(), FIELDS[IMPROVEMENT].LABEL);
       cy.checkText(field.hint(), FIELDS[IMPROVEMENT].HINT);
-      field.input().should('exist');
+      field.input().should('not.exist');
     });
 
     it(`should render the ${OTHER_COMMENTS} section`, () => {
