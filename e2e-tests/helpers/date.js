d1a78eea718518b2f285bfc9026629bc8419598f
ttbarnes <tony@tonybarnes.me>
2023-09-14 11:03:36 +0100

    chore(EMS-1978): add missing documentation

--- e2e-tests/helpers/date.js
+++ e2e-tests/helpers/date.js
@@ -4,7 +4,7 @@ import { DATE_FORMAT } from '../constants';
 /**
  * createTimestampFromNumbers
  * Create a timestamp from day, month and year numbers
- * @param {Number} Day 
+ * @param {Number} Day
  * @param {Number} Month
  * @param {Number} Year
  * @returns {String} Timestamp
@@ -14,8 +14,8 @@ export const createTimestampFromNumbers = (day, month, year) => new Date(`${mont
 /**
  * formatDate
  * Format a date with a provided date format, or default format.
- * @param {String} Timestamp 
- * @param {String} Date format 
+ * @param {String} Timestamp
+ * @param {String} Date format
  * @returns {String} Formatted date
  */
 export const formatDate = (timestamp, dateFormat = DATE_FORMAT.DEFAULT) => format(new Date(timestamp), dateFormat);
