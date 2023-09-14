69f6d93d49a2ef8ecbafb97c9eb30ae5a6496422
ttbarnes <tony@tonybarnes.me>
2023-09-14 15:14:37 +0100

    chore(EMS-1978): add company with no financial year end date to E2E fixtures

--- e2e-tests/fixtures/companies.js
+++ e2e-tests/fixtures/companies.js
@@ -1,29 +1,37 @@
 import {
-  COMPANIES_HOUSE_NUMBER, COMPANIES_HOUSE_NUMBER_NO_SIC_CODE, COMPANIES_HOUSE_NUMBER_MULTIPLE_SIC_CODES,
+  COMPANIES_HOUSE_NUMBER,
+  COMPANIES_HOUSE_NUMBER_NO_SIC_CODE,
+  COMPANIES_HOUSE_NUMBER_MULTIPLE_SIC_CODES,
+  COMPANIES_HOUSE_NUMBER_NO_FINANCIAL_YEAR_END_DATE,
 } from '../constants/examples';
 import { EXPORTER_BUSINESS as EXPORTER_BUSINESS_FIELD_IDS } from '../constants/field-ids/insurance/business';
 import mockApplication from './application';
 import mockSicCodes from './sic-codes';
 
 const {
-  COMPANY_HOUSE: { INDUSTRY_SECTOR_NAME },
+  COMPANY_HOUSE: { INDUSTRY_SECTOR_NAME, FINANCIAL_YEAR_END_DATE },
 } = EXPORTER_BUSINESS_FIELD_IDS;
 
 const baseCompany = mockApplication.EXPORTER_COMPANY;
 
+const { financialYearEndDate, ...baseCompanyFields } = baseCompany;
+
 const mockCompanies = {
   [COMPANIES_HOUSE_NUMBER]: {
-    ...baseCompany,
+    ...baseCompanyFields,
     companyNumber: COMPANIES_HOUSE_NUMBER,
+    [FINANCIAL_YEAR_END_DATE]: baseCompany[FINANCIAL_YEAR_END_DATE],
   },
   [COMPANIES_HOUSE_NUMBER_NO_SIC_CODE]: {
-    ...baseCompany,
+    ...baseCompanyFields,
     companyNumber: COMPANIES_HOUSE_NUMBER_NO_SIC_CODE,
+    [FINANCIAL_YEAR_END_DATE]: baseCompany[FINANCIAL_YEAR_END_DATE],
     sicCodes: [],
   },
   [COMPANIES_HOUSE_NUMBER_MULTIPLE_SIC_CODES]: {
-    ...baseCompany,
+    ...baseCompanyFields,
     companyNumber: COMPANIES_HOUSE_NUMBER_MULTIPLE_SIC_CODES,
+    [FINANCIAL_YEAR_END_DATE]: baseCompany[FINANCIAL_YEAR_END_DATE],
     sicCodes: [
       mockSicCodes[1].code,
       mockSicCodes[2].code,
@@ -36,6 +44,10 @@ const mockCompanies = {
       mockSicCodes[3][INDUSTRY_SECTOR_NAME],
       mockSicCodes[4][INDUSTRY_SECTOR_NAME],
     ],
+  },
+  [COMPANIES_HOUSE_NUMBER_NO_FINANCIAL_YEAR_END_DATE]: {
+    ...baseCompanyFields,
+    companyNumber: COMPANIES_HOUSE_NUMBER_NO_FINANCIAL_YEAR_END_DATE,
   },
 };
 
