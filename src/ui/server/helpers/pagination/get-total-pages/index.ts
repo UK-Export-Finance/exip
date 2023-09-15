6445bf0a50a5eb1d5a8008e0f61bc0cdbe60c519
Tony Barnes <tony@tonybarnes.me>
2023-09-15 12:01:19 +0100

    fix(EMS-1159): dashboard pagination - fix issue where pagination would not display for 16 applications (#984)

--- src/ui/server/helpers/pagination/get-total-pages/index.ts
+++ src/ui/server/helpers/pagination/get-total-pages/index.ts
@@ -4,7 +4,7 @@ import { MAX_APPLICATIONS_PER_PAGE } from '../../../constants';
  * getTotalPages
  * Get the total amount of pages for pagination.
  * 1 page should have a maximum of MAX_APPLICATIONS_PER_PAGE
- * Therefore, divide and round the total of applications/pages
+ * Therefore, divide and round the total of applications/pages up to the nearest number.
  * @param {Number} Total amount of applications
  * @returns {Number}
  */
@@ -12,7 +12,7 @@ export const getTotalPages = (totalApplications: number) => {
   let totalPages = 1;
 
   if (totalApplications > MAX_APPLICATIONS_PER_PAGE) {
-    totalPages = Math.round(totalApplications / MAX_APPLICATIONS_PER_PAGE);
+    totalPages = Math.ceil(totalApplications / MAX_APPLICATIONS_PER_PAGE);
   }
 
   return totalPages;
