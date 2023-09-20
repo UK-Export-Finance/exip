1a61a80291d13626ba39adc5685081d807b50765
ttbarnes <tony@tonybarnes.me>
2023-09-20 11:17:31 +0100

    chore(EMS-1963): fix typo

--- src/ui/server/api/keystone/APIM/index.ts
+++ src/ui/server/api/keystone/APIM/index.ts
@@ -8,22 +8,22 @@ const APIM = {
       const response = (await apollo('GET', getApimCisCountries, {})) as ApolloResponse;
 
       if (response.errors) {
-        console.error('GraphQL network error querying NEW API CALL %O', response.errors);
+        console.error('GraphQL network error querying APIM - CIS countries API %O', response.errors);
       }
 
       if (response?.networkError?.result?.errors) {
-        console.error('GraphQL network error querying NEW API CALL %O', response.networkError.result.errors);
+        console.error('GraphQL network error querying APIM - CIS countries API %O', response.networkError.result.errors);
       }
 
       if (response?.data?.getApimCisCountries) {
         return response.data.getApimCisCountries;
       }
 
-      console.error('Error with GraphQL NEW API CALL %O', response);
-      throw new Error('Getting NEW API CALL');
+      console.error('Error with GraphQL APIM - CIS countries API %O', response);
+      throw new Error('Getting APIM - CIS countries API');
     } catch (err) {
-      console.error('Error getting NEW API CALL %O', err);
-      throw new Error('Getting NEW API CALL');
+      console.error('Error getting APIM - CIS countries API %O', err);
+      throw new Error('Getting APIM - CIS countries API');
     }
   },
 };
