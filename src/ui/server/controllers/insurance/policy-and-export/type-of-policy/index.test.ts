380b5c2e52de297168f1bc52d5c36a4216f6d0bc
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2023-09-20 14:08:15 +0100

    feat(EMS-1993) linting fix

--- src/ui/server/controllers/insurance/policy-and-export/type-of-policy/index.test.ts
+++ src/ui/server/controllers/insurance/policy-and-export/type-of-policy/index.test.ts
@@ -103,9 +103,10 @@ describe('controllers/insurance/policy-and-export/type-of-policy', () => {
   describe('post', () => {
     describe('when there are no validation errors', () => {
       beforeEach(() => {
+        const FIELD = FIELDS[FIELD_ID];
+
         req.body = {
-          // @ts-ignore
-          [FIELD_ID]: FIELDS[FIELD_ID]?.OPTIONS?.SINGLE.VALUE,
+          [FIELD_ID]: FIELD?.OPTIONS?.SINGLE?.VALUE,
         };
       });
 
@@ -131,8 +132,9 @@ describe('controllers/insurance/policy-and-export/type-of-policy', () => {
 
       describe('when the answer is `multi`', () => {
         it(`should redirect to ${ROUTES.INSURANCE.POLICY_AND_EXPORTS.MULTIPLE_CONTRACT_POLICY}`, async () => {
-          // @ts-ignore
-          req.body[FIELD_ID] = FIELDS[FIELD_ID]?.OPTIONS?.MULTIPLE.VALUE;
+          const FIELD = FIELDS[FIELD_ID];
+
+          req.body[FIELD_ID] = FIELD[FIELD_ID]?.OPTIONS?.MULTIPLE.VALUE;
 
           await post(req, res);
 
@@ -205,9 +207,10 @@ describe('controllers/insurance/policy-and-export/type-of-policy', () => {
 
     describe('api error handling', () => {
       beforeEach(() => {
+        const FIELD = FIELDS[FIELD_ID];
+
         req.body = {
-          // @ts-ignore
-          [FIELD_ID]: FIELDS[FIELD_ID]?.OPTIONS?.SINGLE.VALUE,
+          [FIELD_ID]: FIELD[FIELD_ID]?.OPTIONS?.SINGLE.VALUE,
         };
       });
 
