b539fde352ed38e8acffd383ba0334128d572ce5
ttbarnes <tony@tonybarnes.me>
2023-09-21 14:11:28 +0100

    chore(EMS-2010): application creation - default finalDestinationKnown value

---
+++ src/api/constants/application/versions/index.test.ts
@@ -0,0 +1,17 @@
+import VERSIONS from '.';
+
+describe('api/constants/application/versions', () => {
+  it('should return an array of application versions', () => {
+    const expected = [
+      {
+        VERSION_NUMBER: '1',
+        OVER_500K_SUPPORT: false,
+        MAXIMUM_BUYER_CAN_OWE: 500000,
+        TOTAL_VALUE_OF_CONTRACT: 500000,
+        DEFAULT_FINAL_DESTINATION_KNOWN: true,
+      },
+    ];
+
+    expect(VERSIONS).toEqual(expected);
+  });
+});
