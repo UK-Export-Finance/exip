8bbd76d1cc95031973b982493836b9f31a256eb4
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-10-04 11:05:42 +0100

    fix(unit): fixed unit test

--- src/ui/server/helpers/sanitise-environment/index.test.ts
+++ src/ui/server/helpers/sanitise-environment/index.test.ts
@@ -8,9 +8,9 @@ describe('helper/sanitise-environment/sanitise', () => {
     expect(result).toBe(input);
   });
 
-  // Replaces all occurrences of `\\\\n` with `\n` in the input string
-  it('should replace all occurrences of `\\\\n` with `\n` in the input string', () => {
-    const input = 'Hello\\\\nWorld\\\\n';
+  // Replaces all occurrences of `\\\n` with `\n` in the input string
+  it('should replace all occurrences of `\\\n` with `\n` in the input string', () => {
+    const input = 'Hello\\\nWorld\\\n';
     const result = sanitise(input);
     expect(result).toBe('Hello\nWorld\n');
   });
@@ -22,23 +22,23 @@ describe('helper/sanitise-environment/sanitise', () => {
     expect(result).toBe('');
   });
 
-  // Returns the input string if it does not contain `\\\\n`
-  it('should return the input string if it does not contain `\\\\n`', () => {
+  // Returns the input string if it does not contain `\\\n`
+  it('should return the input string if it does not contain `\\\n`', () => {
     const input = 'Hello World';
     const result = sanitise(input);
     expect(result).toBe(input);
   });
 
-  // Returns the input string with `\\\\n` replaced by `\n` even if there are multiple occurrences in the string
-  it('should return the input string with `\\\\n` replaced by `\n` even if there are multiple occurrences in the string', () => {
-    const input = 'Hello\\\\nWorld\\\\n!';
+  // Returns the input string with `\\\n` replaced by `\n` even if there are multiple occurrences in the string
+  it('should return the input string with `\\\n` replaced by `\n` even if there are multiple occurrences in the string', () => {
+    const input = 'Hello\\\nWorld\\\n!';
     const result = sanitise(input);
     expect(result).toBe('Hello\nWorld\n!');
   });
 
   // Handles input strings with leading and trailing spaces correctly
   it('should handle input strings with leading and trailing spaces correctly', () => {
-    const input = '   Hello\\\\nWorld   ';
+    const input = '   Hello\\\nWorld   ';
     const result = sanitise(input);
     expect(result).toBe('   Hello\nWorld   ');
   });
