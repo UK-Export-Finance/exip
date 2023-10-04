e9728b319b9918b5127d709bd957f4951a093581
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-10-04 10:25:23 +0100

    fix(escape): escaping environment variables with double quotes

--- src/ui/server/helpers/sanitise-environment/index.ts
+++ src/ui/server/helpers/sanitise-environment/index.ts
@@ -1,10 +1,11 @@
 /**
  * Sanitises an environment variable string by replacing occurrences of all the sequence
- * '\\n' with a newline character ('\n'). Note this function has also replace escaped backslash (\).
+ * '\\n' with a newline character ('\n').
+ * Note the triple back-slash are for escaping the escaped back-slash.
  * @param string - The input string that needs to be sanitised.
  * @returns The sanitised version of the input string.
  * @example `ABC\\nCBA` will be sanitised to
  * `ABC
  * CBA`
  */
-export const sanitise = (string: string | undefined): string | undefined => (string ? string.replace(/\\\\n/g, '\n') : string);
+export const sanitise = (string?: string): string | undefined => (string ? string.replace(/\\\n/g, '\n') : string);
