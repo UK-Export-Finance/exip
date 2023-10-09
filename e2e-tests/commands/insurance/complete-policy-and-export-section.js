5bf32899f2aacc63b54ebd4c5b614b409b5f714e
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2023-10-09 15:49:00 +0100

    feat(EMS-2043): added documentation

--- e2e-tests/commands/insurance/complete-policy-and-export-section.js
+++ e2e-tests/commands/insurance/complete-policy-and-export-section.js
@@ -2,12 +2,19 @@ import { FIELD_VALUES } from '../../constants';
 
 const { SINGLE, MULTIPLE } = FIELD_VALUES.POLICY_TYPE;
 
+/**
+ * completePolicyAndExportSection
+ * completes policy and export section
+ * handles policyType and if sameName on policy
+ * @param {String} policyType - if single or multiple policy - defaults to single
+ * @param {Boolean} sameName - if sameName on policy - defaults to true
+ */
 const completePolicyAndExportSection = ({ policyType = SINGLE, sameName = true }) => {
+  cy.completeAndSubmitPolicyTypeForm(policyType);
+
   if (policyType === SINGLE) {
-    cy.completeAndSubmitPolicyTypeForm(policyType);
     cy.completeAndSubmitSingleContractPolicyForm({});
   } else {
-    cy.completeAndSubmitPolicyTypeForm(MULTIPLE);
     cy.completeAndSubmitMultipleContractPolicyForm({});
   }
 
