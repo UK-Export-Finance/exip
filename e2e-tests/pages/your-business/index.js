5ee993102bf69cab892f23f6a774cc5396a483a6
ttbarnes <tony@tonybarnes.me>
2023-10-23 13:50:10 +0100

    chore(EMS-2110-2115): remove companies house search from 'your business' flow

--- e2e-tests/pages/your-business/index.js
+++ e2e-tests/pages/your-business/index.js
@@ -1,13 +1,9 @@
-import companiesHouseNumber from './companies-house-number/companiesHouseNumber';
 import companyDetails from './company-details/companyDetails';
-import companiesHouseUnavailablePage from './companies-house-unavailable/companiesHouseUnavailablePage';
 import brokerPage from './broker/broker';
 import turnoverPage from './turnover';
 
 export {
-  companiesHouseNumber,
   companyDetails,
-  companiesHouseUnavailablePage,
   brokerPage,
   turnoverPage,
 };
--- src/ui/server/constants/templates/insurance/business/index.ts
+++ src/ui/server/constants/templates/insurance/business/index.ts
@@ -1,7 +1,5 @@
 export const BUSINESS_TEMPLATES = {
-  COMPANIES_HOUSE_NUMBER: 'insurance/your-business/companies_house_number.njk',
   COMPANY_DETAILS: 'insurance/your-business/company-details.njk',
-  COMPANIES_HOUSE_UNAVAILABLE: 'insurance/your-business/companies-house-error.njk',
   NATURE_OF_YOUR_BUSINESS: 'insurance/your-business/nature-of-your-business.njk',
   TURNOVER: 'insurance/your-business/turnover.njk',
   BROKER: 'insurance/your-business/broker.njk',
