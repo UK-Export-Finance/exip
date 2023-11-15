a98fcd348861bcbf10cf41dc914a69b4b91fe9df
ttbarnes <tony@tonybarnes.me>
2023-11-15 14:13:02 +0000

    feat(EMS-2227): no pdf - end buyer - e2e test coverage

--- e2e-tests/insurance/cypress/e2e/journeys/eligibility/end-buyer/end-buyer.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/eligibility/end-buyer/end-buyer.spec.js
@@ -16,11 +16,12 @@ const {
 
 const {
   START,
-  ELIGIBILITY: { END_BUYER, UK_GOODS_OR_SERVICES, CHECK_YOUR_ANSWERS, CANNOT_APPLY_MULTIPLE_RISKS },
+  ELIGIBILITY: {
+    END_BUYER, UK_GOODS_OR_SERVICES, CHECK_YOUR_ANSWERS, CANNOT_APPLY_MULTIPLE_RISKS,
+  },
 } = INSURANCE_ROUTES;
 
 const baseUrl = Cypress.config('baseUrl');
-
 
 context('Insurance - End buyer page - as an exporter, I want to confirm if payment by the buyer of my export depends on payment from an end buyer, So that UKEF can have clarity of my export transaction', () => {
   const url = `${baseUrl}${END_BUYER}`;
