c1af92b82d04695f588614fddeae810e81d97b9f
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2023-11-21 15:20:33 +0000

    fix(EMS-2297-98-99): fixed various nature of business bugs and misformatted variable

--- e2e-tests/commands/insurance/complete-company-details-form.js
+++ e2e-tests/commands/insurance/complete-company-details-form.js
@@ -21,14 +21,14 @@ const {
  * @param {Boolean} differentTradingAddress
  * @param {String} phoneNumber
  * @param {String} companyWebsite
- * @param {Boolean} completedifferentTradingName
+ * @param {Boolean} completedDifferentTradingName
  */
 const completeCompaniesDetailsForm = ({
   differentTradingName = false,
   differentTradingAddress = false,
   phoneNumber,
   companyWebsite,
-  completedifferentTradingName = true,
+  completedDifferentTradingName = true,
 }) => {
   if (differentTradingName) {
     yesRadioInput().first().click();
@@ -36,7 +36,7 @@ const completeCompaniesDetailsForm = ({
     noRadioInput().first().click();
   }
 
-  if (differentTradingName && completedifferentTradingName) {
+  if (differentTradingName && completedDifferentTradingName) {
     field(DIFFERENT_TRADING_NAME).input().type(YOUR_COMPANY[DIFFERENT_TRADING_NAME]);
   }
 
