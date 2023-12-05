be39754fb04520c4e7aea3a643fad2a4cf8c0ac9
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2023-12-05 15:19:40 +0000

    feat(EMS-2183): fixed failing api test

--- src/api/test-helpers/applications.ts
+++ src/api/test-helpers/applications.ts
@@ -1,7 +1,7 @@
 import { Application, TestHelperApplicationCreate, TestHelperApplicationGet, TestHelperApplicationUpdate } from '../types';
 
 const applicationQuery =
-  'id createdAt updatedAt referenceNumber dealType submissionCount submissionDeadline submissionType status previousStatus version eligibility { id } exportContract { id } owner { id } company { id } business { id } broker { id } buyer { id } sectionReview { id } declaration { id } policyContact { id }';
+  'id createdAt updatedAt referenceNumber dealType submissionCount submissionDeadline submissionType status previousStatus version eligibility { id } exportContract { id } owner { id } company { id } business { id } broker { id } buyer { id } sectionReview { id } declaration { id } policyContact { id } differentTradingAddress { id }';
 
 /**
  * create application test helper
