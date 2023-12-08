3bfcb610d2f414e4306e685bddaa52b6e927dea8
ttbarnes <tony@tonybarnes.me>
2023-12-08 12:06:32 +0000

    chore(tests): fix/update flaky date tests

--- src/ui/server/controllers/insurance/policy/multiple-contract-policy/index.test.ts
+++ src/ui/server/controllers/insurance/policy/multiple-contract-policy/index.test.ts
@@ -1,4 +1,3 @@
-import { add, getMonth, getYear } from 'date-fns';
 import { pageVariables, TEMPLATE, FIELD_IDS, totalMonthsOfCoverOptions, get, post } from '.';
 import { GBP_CURRENCY_CODE, ROUTES, TEMPLATES } from '../../../../constants';
 import POLICY_FIELD_IDS from '../../../../constants/field-ids/insurance/policy';
@@ -288,9 +287,9 @@ describe('controllers/insurance/policy/multiple-contract-policy', () => {
     const date = new Date();
 
     const validBody = {
-      [`${REQUESTED_START_DATE}-day`]: '1',
-      [`${REQUESTED_START_DATE}-month`]: getMonth(add(date, { months: 1 })),
-      [`${REQUESTED_START_DATE}-year`]: getYear(add(date, { years: 1 })),
+      [`${REQUESTED_START_DATE}-day`]: date.getDate(),
+      [`${REQUESTED_START_DATE}-month`]: date.getMonth() + 1,
+      [`${REQUESTED_START_DATE}-year`]: date.getFullYear() + 1,
       [TOTAL_MONTHS_OF_COVER]: '1',
       [TOTAL_SALES_TO_BUYER]: '1000',
       [MAXIMUM_BUYER_WILL_OWE]: '500',
