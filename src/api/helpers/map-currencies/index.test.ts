8be09f72f21b4efa05f6190f3446f66bfe642413
ttbarnes <tony@tonybarnes.me>
2023-12-13 15:55:47 +0000

    chore(EMS-1964): fix/update unit test

--- src/api/helpers/map-currencies/index.test.ts
+++ src/api/helpers/map-currencies/index.test.ts
@@ -1,42 +1,28 @@
 import mapCurrencies, { getSupportedCurrencies } from '.';
+import { SUPPORTED_CURRENCIES } from '../../constants';
+import sortArrayAlphabetically from '../sort-array-alphabetically';
 import mockCurrencies from '../../test-mocks/mock-currencies';
 
 describe('helpers/map-currencies', () => {
   describe('getSupportedCurrencies', () => {
-    it('should only return supported currencies (GBP, EUR, USD)', () => {
+    it('should only return supported currencies via SUPPORTED_CURRENCIES constants', () => {
       const result = getSupportedCurrencies(mockCurrencies);
 
-      const expected = [mockCurrencies[0], mockCurrencies[2], mockCurrencies[3]];
+      const expected = mockCurrencies.filter((currency) => SUPPORTED_CURRENCIES.find((currencyCode: string) => currency.isoCode === currencyCode));
 
       expect(result).toEqual(expected);
     });
   });
 
   describe('mapCurrencies', () => {
-    const supportedCurrencies = getSupportedCurrencies(mockCurrencies);
+    it('should return an array of currencies sorted alphabetically', () => {
+      const result = mapCurrencies(mockCurrencies);
 
-    it('should return an array of mapped objects', () => {
-      const result = mapCurrencies(supportedCurrencies);
+      const supportedCurrencies = getSupportedCurrencies(mockCurrencies);
 
-      const expected = supportedCurrencies.map((currency) => ({
-        isoCode: currency.isoCode,
-        text: currency.name,
-      }));
+      const expected = sortArrayAlphabetically(supportedCurrencies, 'name');
 
       expect(result).toEqual(expected);
-    });
-
-    describe('when renderIsoCodeInText is passed', () => {
-      it('should return an array of mapped objects with ISO codes in the text', () => {
-        const result = mapCurrencies(supportedCurrencies);
-
-        const expected = supportedCurrencies.map((currency) => ({
-          isoCode: currency.isoCode,
-          text: `${currency.isoCode} - ${currency.name}`,
-        }));
-
-        expect(result).toEqual(expected);
-      });
     });
   });
 });
