7732d6dd22e5d628824901d8ba13877023b46a0c
ttbarnes <tony@tonybarnes.me>
2023-12-14 16:51:49 +0000

    feat(EMS-2300): no pdf - new cypress object for section start page assertions

---
+++ e2e-tests/commands/shared-commands/assertions/assert-section-start-content.js
@@ -0,0 +1,63 @@
+import {
+  intro,
+  listIntro,
+  listItem,
+  listOutro,
+  outro,
+  startNowLink,
+  allSectionsLink,
+} from '../../../pages/shared';
+import { BUTTONS } from '../../../content-strings';
+
+/**
+ * assertSectionStartContent
+ * Assert various pieces of content in a "section start" page.
+ */
+const assertSectionStartContent = {
+  intro: (expectedText) => cy.checkText(intro(), expectedText),
+  list: {
+    intro: (expectedText) => cy.checkText(listIntro(), expectedText),
+    items: (expectedItems) => {
+      expectedItems.forEach((expectedText, index) => {
+        cy.checkText(listItem(index + 1), expectedText);
+      });
+    },
+    outro: (expectedText) => cy.checkText(listOutro(), expectedText),
+  },
+  outro: (expectedText) => cy.checkText(outro(), expectedText),
+
+  startNow: {
+    link: ({ expectedUrl }) => {
+      cy.checkLink(
+        startNowLink(),
+        expectedUrl,
+        BUTTONS.START_NOW,
+      );
+    },
+    linkRedirection: ({ currentUrl, expectedUrl }) => {
+      cy.navigateToUrl(currentUrl);
+
+      startNowLink().click();
+
+      cy.assertUrl(expectedUrl);
+    },
+  },
+  allSections: {
+    link: ({ expectedUrl }) => {
+      cy.checkLink(
+        allSectionsLink(),
+        expectedUrl,
+        BUTTONS.START_DIFFERENT_SECTION,
+      );
+    },
+    linkRedirection: ({ currentUrl, expectedUrl }) => {
+      cy.navigateToUrl(currentUrl);
+
+      allSectionsLink().click();
+
+      cy.assertUrl(expectedUrl);
+    },
+  },
+};
+
+export default assertSectionStartContent;
