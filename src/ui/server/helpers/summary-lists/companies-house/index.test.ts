61ee3b129c0754c475c8978115cf4007c0b8c64d
ttbarnes <tony@tonybarnes.me>
2023-12-18 19:07:25 +0000

    chore(EMS-722): split up companies house summary list assertions

--- src/ui/server/helpers/summary-lists/companies-house/index.test.ts
+++ src/ui/server/helpers/summary-lists/companies-house/index.test.ts
@@ -15,43 +15,61 @@ const {
 
 describe('server/helpers/summary-lists/companies-house', () => {
   describe('generateFields', () => {
-    it('should populate field groups when provided with companyDetails', () => {
-      const isApplicationData = true;
-      const result = generateFields(mockCompany, isApplicationData);
+    const isApplicationData = true;
+    const result = generateFields(mockCompany, isApplicationData);
 
-      const expected = [
-        fieldGroupItem({
-          field: getFieldById(FIELDS, COMPANY_NUMBER),
-          data: mockCompany,
-        }),
-        fieldGroupItem({
-          field: getFieldById(FIELDS, COMPANY_NAME),
-          data: mockCompany,
-        }),
-        fieldGroupItem(
-          {
-            field: getFieldById(FIELDS, COMPANY_ADDRESS),
-            data: mockCompany,
-          },
-          generateMultipleFieldHtml(mockCompany[COMPANY_ADDRESS]),
-        ),
-        fieldGroupItem(
-          {
-            field: getFieldById(FIELDS, COMPANY_INCORPORATED),
-            data: mockCompany,
-          },
-          formatDate(mockCompany[COMPANY_INCORPORATED]),
-        ),
-        fieldGroupItem(
-          {
-            field: getFieldById(FIELDS, COMPANY_SIC),
-            data: mockCompany,
-          },
-          generateSicCodesValue(mockCompany, isApplicationData),
-        ),
-      ];
+    it(`it should populate a ${COMPANY_NUMBER} field`, () => {
+      const expected = fieldGroupItem({
+        field: getFieldById(FIELDS, COMPANY_NUMBER),
+        data: mockCompany,
+      });
 
-      expect(result).toEqual(expected);
+      expect(result[0]).toEqual(expected);
+    });
+
+    it(`it should populate a ${COMPANY_NAME} field`, () => {
+      const expected = fieldGroupItem({
+        field: getFieldById(FIELDS, COMPANY_NAME),
+        data: mockCompany,
+      });
+
+      expect(result[1]).toEqual(expected);
+    });
+
+    it(`it should populate a ${COMPANY_ADDRESS} field`, () => {
+      const expected = fieldGroupItem(
+        {
+          field: getFieldById(FIELDS, COMPANY_ADDRESS),
+          data: mockCompany,
+        },
+        generateMultipleFieldHtml(mockCompany[COMPANY_ADDRESS]),
+      );
+
+      expect(result[2]).toEqual(expected);
+    });
+
+    it(`it should populate a ${COMPANY_INCORPORATED} field`, () => {
+      const expected = fieldGroupItem(
+        {
+          field: getFieldById(FIELDS, COMPANY_INCORPORATED),
+          data: mockCompany,
+        },
+        formatDate(mockCompany[COMPANY_INCORPORATED]),
+      );
+
+      expect(result[3]).toEqual(expected);
+    });
+
+    it(`it should populate a ${COMPANY_SIC} field`, () => {
+      const expected = fieldGroupItem(
+        {
+          field: getFieldById(FIELDS, COMPANY_SIC),
+          data: mockCompany,
+        },
+        generateSicCodesValue(mockCompany, isApplicationData),
+      );
+
+      expect(result[4]).toEqual(expected);
     });
   });
 
