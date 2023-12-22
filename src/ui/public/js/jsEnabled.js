797588fab99936544dfc295e1e40e639836f1710
Tony Barnes <tony@tonybarnes.me>
2023-12-22 12:27:53 +0000

    chore(EMS-2418): No PDF - Upgrade GOV design package (#1569)
    * chore(EMS-2418): bump gov design library
    
    * chore(EMS-2418): no pdf - update client side scripts to use module attribute value
    
    * chore(EMS-2418): fix typo
    
    * chore(EMS-2418): update integrity hashes
    
    * chore(EMS-2418): remove autogenerated/added javascript license text

--- src/ui/public/js/jsEnabled.js
+++ src/ui/public/js/jsEnabled.js
@@ -1,2 +1,2 @@
-var EXIP;!function(){document.body.className=document.body.className?`${document.body.className} js-enabled`:"js-enabled";const e=document.getElementById("tasks-filters");e&&(e.className="js-enabled"),(EXIP=void 0===EXIP?{}:EXIP).jsEnabled={}}();
+var EXIP;document.body.className+=" js-enabled"+("noModule"in HTMLScriptElement.prototype?" govuk-frontend-supported":""),(EXIP=void 0===EXIP?{}:EXIP).jsEnabled={};
 //# sourceMappingURL=jsEnabled.js.map
