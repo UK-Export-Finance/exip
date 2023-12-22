ef050f7a51249db8aae7ef5c43c6523fd1d73bcf
ttbarnes <tony@tonybarnes.me>
2023-12-22 14:57:38 +0000

    chore(docs): update documentation

--- e2e-tests/commands/shared-commands/assertions/assert-first-and-last-radios.js
+++ e2e-tests/commands/shared-commands/assertions/assert-first-and-last-radios.js
@@ -3,6 +3,11 @@ import { radioInputs } from '../../../pages/shared';
 /**
  * assertYesNoRadiosOrder
  * Check that "yes" and "no" radio inputs are in the correct order.
+ * Note: other cypress approaches for this test assertion have been explored.
+ * All explored options involve adding 2/3 more commands, purely for this command.
+ * Such commands would make this assertion harder to read.
+ * Mostly due to being unable to assign cy.wrap to a variable.
+ * It is simpler and easier to follow to have as below.
  * @param {Boolean} noRadioFirst: No radio should be the first radio. Defaults to false.
  */
 const assertYesNoRadiosOrder = ({ noRadioFirst = false }) => {
