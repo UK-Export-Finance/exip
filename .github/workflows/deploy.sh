8d94befe666f5643368078a4f3a891569899b0ee
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2024-01-04 17:45:06 +0000

    chore(merge): Merge `main` into `main-application-no-pdf` (#1641)

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -16,51 +16,58 @@ read selection
 if [ -n "$selection" ]; then
 
     ############### INFRASTRUCTURE ###############
-    if [ "$selection" = "0" ]
-    then
-    destination=infrastructure
-    branch=main
+    if [ "$selection" = "0" ]; then
+        destination=infrastructure
+        branch=main
     ############### DEPLOYMENT ###############
-    elif [ "$selection" = "1" ]
-    then
-    read destination
-    branch=main-application-no-pdf
+    elif [ "$selection" = "1" ]; then
+        read destination
+        ############### DEVELOPMENT ###############
+        if [ "$destination" = "dev" ]; then
+            branch=main
+        ############### FEATURE ###############
+        elif [ "$destination" = "feature" ]; then
+            branch=main-application-no-pdf
+        ############### STAGING ###############
+        elif [ "$destination" = "staging" ]; then
+            branch=dev
+        ############### PRODUCTION ###############
+        elif [ "$destination" = "production" ]; then
+            branch=staging
+        fi
     ############### ACR PURGE ###############
-    elif [ "$selection" = "2" ]
-    then
-    destination=""
-    branch=""
-    az acr run --cmd "acr purge --filter 'get-a-quote:.*' --ago 15d" --registry "${ACR}" /dev/null
+    elif [ "$selection" = "2" ]; then
+        destination=""
+        branch=""
+        az acr run --cmd "acr purge --filter 'get-a-quote:.*' --ago 15d" --registry "${ACR}" /dev/null
     ############### ACR PURGE ###############
     fi
 
-    if [ -n "$destination" -a -n "$branch" ]
-    then
-    # Display latest push commit
-    git checkout "${branch}"
-    git pull
-    printf "\n\n${NC}⬆️ ${branch} branch latest push : ${NC}"
-    printf "${GREEN}"
-    git log -n 1 --pretty | sort | grep commit
-    printf "${NC}\n\n"
+    if [ -n "$destination" -a -n "$branch" ]; then
+        # Display latest push commit
+        git checkout "${branch}"
+        git pull
+        printf "\n\n${NC}⬆️ ${branch} branch latest push : ${NC}"
+        printf "${GREEN}"
+        git log -n 1 --pretty | sort | grep commit
+        printf "${NC}\n\n"
 
-    # Deploy
-    git checkout -b "${destination}"
-    git push -f --set-upstream origin "${destination}"
+        # Deploy
+        git checkout -b "${destination}"
+        git push -f --set-upstream origin "${destination}"
 
-    # Clean up
-    git checkout "${branch}"
-    git branch -d "${destination}"
-    printf "\n\n✅ ${destination} deployment initiated, switched to ${branch}.\n\n"
+        # Clean up
+        git checkout "${branch}"
+        git branch -d "${destination}"
+        printf "\n\n✅ ${destination} deployment initiated, switched to ${branch}.\n\n"
     fi
 
 else
-    printf "${RED} ❌ Invalid input, terminating.${NC}\n\n";
-    exit 0;
+    printf "${RED} ❌ Invalid input, terminating.${NC}\n\n"
+    exit 0
 fi
 
 #######################################
 # UKEF deployment shell script v0.0.7
-# 31/05/2023
-# Abhi Markan
+# 03/01/2024
 #######################################
