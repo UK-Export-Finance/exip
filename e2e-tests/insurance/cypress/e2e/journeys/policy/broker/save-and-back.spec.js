1faa0b78400574685434e9ec253b2ae37a6db9a0
Tony Barnes <tony@tonybarnes.me>
2024-01-12 14:59:53 +0000

    feat(EMS-2558): No PDF - Policy - Pre-credit period - Save and back (#1699)
    * feat(EMS-2558): no pdf - pre-credit period - save and back
    
    * chore(tech): simplify broker save and back e2e test

--- e2e-tests/insurance/cypress/e2e/journeys/policy/broker/save-and-back.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/policy/broker/save-and-back.spec.js
@@ -101,11 +101,8 @@ context('Insurance - Policy - Broker page - Save and back', () => {
 
       cy.startInsurancePolicySection({});
 
-      // go through 4 policy forms.
-      cy.clickSubmitButtonMultipleTimes({ count: 4 });
-
-      // TODO: EMS-2558 - this will not be required once pre-credit period has data saving.
-      cy.completeAndSubmitPreCreditPeriodForm({});
+      // go through 5 policy forms.
+      cy.clickSubmitButtonMultipleTimes({ count: 5 });
 
       brokerPage[USING_BROKER].yesRadioInput().should('be.checked');
       cy.checkValue(field(NAME), application.EXPORTER_BROKER[NAME]);
@@ -145,11 +142,8 @@ context('Insurance - Policy - Broker page - Save and back', () => {
 
         cy.startInsurancePolicySection({});
 
-        // go through 4 policy forms.
-        cy.clickSubmitButtonMultipleTimes({ count: 4 });
-
-        // TODO: EMS-2558 - this will not be required once pre-credit period has data saving.
-        cy.completeAndSubmitPreCreditPeriodForm({});
+        // go through 5 policy forms.
+        cy.clickSubmitButtonMultipleTimes({ count: 5 });
 
         brokerPage[USING_BROKER].yesRadioInput().should('be.checked');
         cy.checkValue(field(NAME), application.EXPORTER_BROKER[NAME]);
@@ -180,11 +174,8 @@ context('Insurance - Policy - Broker page - Save and back', () => {
 
         cy.startInsurancePolicySection({});
 
-        // go through 4 policy forms.
-        cy.clickSubmitButtonMultipleTimes({ count: 4 });
-
-        // TODO: EMS-2558 - this will not be required once pre-credit period has data saving.
-        cy.completeAndSubmitPreCreditPeriodForm({});
+        // go through 5 policy forms.
+        cy.clickSubmitButtonMultipleTimes({ count: 5 });
 
         brokerPage[USING_BROKER].noRadioInput().should('be.checked');
       });
