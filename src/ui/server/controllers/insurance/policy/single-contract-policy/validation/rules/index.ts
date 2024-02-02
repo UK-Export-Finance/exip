7e578a5110f5c2a258c456f60e716ec0ef1b55ef
ttbarnes <tony@tonybarnes.me>
2024-02-02 14:56:47 +0000

    feat(EMS-2722): no pdf - add alternative currency code rules to single policy

--- src/ui/server/controllers/insurance/policy/single-contract-policy/validation/rules/index.ts
+++ src/ui/server/controllers/insurance/policy/single-contract-policy/validation/rules/index.ts
@@ -1,9 +1,10 @@
 import requestedStartDateRules from '../../../../../../shared-validation/requested-start-date';
 import policyCurrencyCodeRules from '../../../../../../shared-validation/policy-currency-code';
+import policyAlternativeCurrencyCodeRule from '../../../../../../shared-validation/policy-alternative-currency-code';
 import contractCompletionDateRules from './contract-completion-date';
 import { ValidationErrors } from '../../../../../../../types';
 
-const rules = [requestedStartDateRules, contractCompletionDateRules, policyCurrencyCodeRules];
+const rules = [requestedStartDateRules, contractCompletionDateRules, policyCurrencyCodeRules, policyAlternativeCurrencyCodeRule];
 
 const validationRules = rules as Array<() => ValidationErrors>;
 
