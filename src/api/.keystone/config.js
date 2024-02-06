49dabd74c40d58e8429ed698cdc02780d759899b
ttbarnes <tony@tonybarnes.me>
2024-02-06 12:30:09 +0000

    feat(EMS-2640-2586): fix/update unit test

--- src/api/.keystone/config.js
+++ src/api/.keystone/config.js
@@ -209,8 +209,8 @@ var POLICY = {
   },
   NEED_PRE_CREDIT_PERIOD: "needPreCreditPeriodCover",
   CREDIT_PERIOD_WITH_BUYER: "creditPeriodWithBuyer",
-  NEED_ANOTHER_COMPANY_TO_BE_INSURED: "needAnotherCompanyToBeInsured",
-  OTHER_COMPANY_TO_INSURE_NAME_TBC: {
+  REQUEST_JOINTLY_INSURED_PARTY: "requestJointlyInsuredParty",
+  REQUESTED_JOINTLY_INSURED_PARTY: {
     COMPANY_NAME: "companyName",
     COMPANY_NUMBER: "companyNumber",
     COUNTRY: "country"
