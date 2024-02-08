af066d78821f753054cff9a068b647dd0985c799
ttbarnes <tony@tonybarnes.me>
2024-02-08 19:06:44 +0000

    chore(EMS-2825): improve cypress assertion commands structure

---
+++ e2e-tests/commands/shared-commands/assertions/pagination.js
@@ -0,0 +1,7 @@
+Cypress.Commands.add('assertActivePaginationLink', require('../../pagination/assert-active-pagination-link'));
+Cypress.Commands.add('assertPaginationDoesNotExist', require('../../pagination/assert-pagination-does-not-exist'));
+Cypress.Commands.add('assertPaginationItemLink', require('../../pagination/assert-pagination-item-link'));
+Cypress.Commands.add('assertPaginationItemEllipsis', require('../../pagination/assert-pagination-item-ellipsis'));
+Cypress.Commands.add('assertPaginationNextLink', require('../../pagination/assert-pagination-next-link'));
+Cypress.Commands.add('assertPaginationPreviousLink', require('../../pagination/assert-pagination-previous-link'));
+Cypress.Commands.add('assertPaginationState', require('../../pagination/assert-pagination-state'));
---
+++ e2e-tests/commands/shared-commands/assertions/tasks.js
@@ -0,0 +1,34 @@
+Cypress.Commands.add('checkTaskStatus', require('./tasks/check-task-status'));
+
+Cypress.Commands.add('checkTaskStatusCannotStart', require('./tasks/check-task-status-cannot-start'));
+Cypress.Commands.add('checkTaskStatusNotStartedYet', require('./tasks/check-task-status-not-started-yet'));
+Cypress.Commands.add('checkTaskStatusInProgress', require('./tasks/check-task-status-in-progress'));
+Cypress.Commands.add('checkTaskStatusCompleted', require('./tasks/check-task-status-complete'));
+
+Cypress.Commands.add('checkTaskEligibilityStatusIsComplete', require('./tasks/eligibility/is-complete'));
+
+Cypress.Commands.add('checkTaskBusinessStatusIsNotStartedYet', require('./tasks/business/is-not-started-yet'));
+Cypress.Commands.add('checkTaskBusinessStatusIsInProgress', require('./tasks/business/is-in-progress'));
+Cypress.Commands.add('checkTaskBusinessStatusIsComplete', require('./tasks/business/is-complete'));
+
+Cypress.Commands.add('checkTaskBuyerStatusIsNotStartedYet', require('./tasks/buyer/is-not-started-yet'));
+Cypress.Commands.add('checkTaskBuyerStatusIsInProgress', require('./tasks/buyer/is-in-progress'));
+Cypress.Commands.add('checkTaskBuyerStatusIsComplete', require('./tasks/buyer/is-complete'));
+
+Cypress.Commands.add('checkTaskPolicyStatusIsCannotStart', require('./tasks/policy/is-cannot-start'));
+Cypress.Commands.add('checkTaskPolicyStatusIsNotStartedYet', require('./tasks/policy/is-not-started-yet'));
+Cypress.Commands.add('checkTaskPolicyStatusIsInProgress', require('./tasks/policy/is-in-progress'));
+Cypress.Commands.add('checkTaskPolicyStatusIsComplete', require('./tasks/policy/is-complete'));
+
+Cypress.Commands.add('checkTaskExportContractStatusIsCannotStart', require('./tasks/export-contract/is-cannot-start'));
+Cypress.Commands.add('checkTaskExportContractStatusIsNotStartedYet', require('./tasks/export-contract/is-not-started-yet'));
+Cypress.Commands.add('checkTaskExportContractStatusIsInProgress', require('./tasks/export-contract/is-in-progress'));
+Cypress.Commands.add('checkTaskExportContractStatusIsComplete', require('./tasks/export-contract/is-complete'));
+
+Cypress.Commands.add('checkTaskCheckAnswersStatusIsNotStartedYet', require('./tasks/check-answers/is-not-started-yet'));
+Cypress.Commands.add('checkTaskCheckAnswersStatusIsInProgress', require('./tasks/check-answers/is-in-progress'));
+Cypress.Commands.add('checkTaskCheckAnswersStatusIsComplete', require('./tasks/check-answers/is-complete'));
+
+Cypress.Commands.add('checkTaskDeclarationsAndSubmitStatusIsNotStartedYet', require('./tasks/declarations-and-submit/is-not-started-yet'));
+Cypress.Commands.add('checkTaskDeclarationsAndSubmitStatusIsInProgress', require('./tasks/declarations-and-submit/is-in-progress'));
+Cypress.Commands.add('checkTaskDeclarationsAndSubmitStatusIsComplete', require('./tasks/declarations-and-submit/is-complete'));
