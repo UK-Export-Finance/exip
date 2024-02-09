53e21a9b2d4f18368c85e5351dc0b0d58ae2e348
ttbarnes <tony@tonybarnes.me>
2024-02-09 14:42:24 +0000

    fix(EMS-2786): no pdf - different name on policy - error messages

--- e2e-tests/content-strings/error-messages.js
+++ e2e-tests/content-strings/error-messages.js
@@ -246,7 +246,7 @@ export const ERROR_MESSAGES = {
           INCORRECT_FORMAT: "The policy holder's last name must not include any numbers or symbols",
         },
         [FIELD_IDS.INSURANCE.ACCOUNT.EMAIL]: {
-          IS_EMPTY: "Enter the email address of the person you want named on the policy",
+          IS_EMPTY: 'Enter the email address of the person you want named on the policy',
           INCORRECT_FORMAT: "Enter the policy holder's email address in the correct format, like name@example.com",
           ABOVE_MAXIMUM: "The policy holder's email cannot be more than 300 characters",
         },
