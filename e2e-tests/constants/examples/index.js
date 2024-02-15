5da130fa76f6483f594393481f0254c3e9153010
ttbarnes <tony@tonybarnes.me>
2024-02-15 17:00:10 +0000

    feat(EMS-2854): no pdf - policy - broker address - multi line support

--- e2e-tests/constants/examples/index.js
+++ e2e-tests/constants/examples/index.js
@@ -1,6 +1,7 @@
 export * from './company-details-examples';
 export * from './companies-house-number-examples';
 export * from './email-examples';
+export * from './full-address-examples';
 export * from './password-examples';
 export * from './phone-number-examples';
 export * from './postcode-examples';
