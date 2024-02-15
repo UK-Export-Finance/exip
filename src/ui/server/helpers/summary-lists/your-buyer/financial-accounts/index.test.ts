d10dbe230ebacf2f805a1a0332716d791095fbb3
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-02-15 09:30:54 +0000

    feat(EMS-2630): code fix

--- src/ui/server/helpers/summary-lists/your-buyer/financial-accounts/index.test.ts
+++ src/ui/server/helpers/summary-lists/your-buyer/financial-accounts/index.test.ts
@@ -2,7 +2,7 @@ import financialAccountsFields from '.';
 import { FORM_TITLES } from '../../../../content-strings/form-titles';
 import { YOUR_BUYER_FIELDS as FIELDS } from '../../../../content-strings/fields/insurance';
 import YOUR_BUYER_FIELD_IDS from '../../../../constants/field-ids/insurance/your-buyer';
-import { ROUTES } from '../../../../constants';
+import { INSURANCE_ROUTES } from '../../../../constants/routes/insurance';
 import fieldGroupItem from '../../generate-field-group-item';
 import getFieldById from '../../../get-field-by-id';
 import mapYesNoField from '../../../mappings/map-yes-no-field';
@@ -14,10 +14,8 @@ const {
 } = FORM_TITLES;
 
 const {
-  INSURANCE: {
-    YOUR_BUYER: { BUYER_FINANCIAL_INFORMATION_CHANGE, BUYER_FINANCIAL_INFORMATION_CHECK_AND_CHANGE },
-  },
-} = ROUTES;
+  YOUR_BUYER: { BUYER_FINANCIAL_INFORMATION_CHANGE, BUYER_FINANCIAL_INFORMATION_CHECK_AND_CHANGE },
+} = INSURANCE_ROUTES;
 
 const { HAS_BUYER_FINANCIAL_ACCOUNTS } = YOUR_BUYER_FIELD_IDS;
 
