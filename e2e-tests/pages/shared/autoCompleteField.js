fc5bc9dd5c1b7f3386e36cd457c6e8779a6e3b4d
ttbarnes <tony@tonybarnes.me>
2024-02-19 11:48:34 +0000

    chore(docs): improve documentation

--- e2e-tests/pages/shared/autoCompleteField.js
+++ e2e-tests/pages/shared/autoCompleteField.js
@@ -1,5 +1,12 @@
 import fieldSelector from './field';
 
+/**
+ * autoCompleteField
+ * "Autocomplete" cypress selector.
+ * This field is used for any autocomplete fields, e.g country and currency fields.
+ * @param {String} fieldId: Field ID
+ * @returns 
+ */
 const autoCompleteField = (fieldId) => ({
   ...fieldSelector(fieldId),
   input: () => cy.get(`#${fieldId}`),
