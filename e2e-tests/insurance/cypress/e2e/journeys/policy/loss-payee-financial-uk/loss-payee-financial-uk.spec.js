c9dd5fabd2ed1779b4a9a6229276eedc1961490f
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-03-13 12:52:36 +0000

    feat(EMS-2767): documentation fix

--- e2e-tests/insurance/cypress/e2e/journeys/policy/loss-payee-financial-uk/loss-payee-financial-uk.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/policy/loss-payee-financial-uk/loss-payee-financial-uk.spec.js
@@ -24,7 +24,7 @@ const {
 
 const baseUrl = Cypress.config('baseUrl');
 
-context("Insurance - Policy - Loss payee financial UK page - As an exporter, I want to provide UKEF with my loss payee's bank details So that they can be paid in the event of a claim on the policy", () => {
+context("Insurance - Policy - Loss payee financial UK page - As an exporter, I want to provide UKEF with my loss payee's financial UK details So that they can be paid in the event of a claim on the policy", () => {
   let referenceNumber;
   let url;
   let checkYourAnswersUrl;
