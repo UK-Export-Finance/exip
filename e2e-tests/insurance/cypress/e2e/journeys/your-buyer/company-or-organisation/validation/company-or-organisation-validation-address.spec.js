02e752cd11c49eb20da409dbeedb7b8763c7bd2e
ttbarnes <tony@tonybarnes.me>
2024-03-14 11:15:42 +0000

    fix(EMS-2903): update e2e test

--- e2e-tests/insurance/cypress/e2e/journeys/your-buyer/company-or-organisation/validation/company-or-organisation-validation-address.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/your-buyer/company-or-organisation/validation/company-or-organisation-validation-address.spec.js
@@ -53,7 +53,7 @@ context('Insurance - Your Buyer - Company or organisation page - form validation
 
   it(`should render a validation error and retain the submitted value when ${ADDRESS} is over ${MAXIMUM_CHARACTERS.FULL_ADDRESS} characters`, () => {
     const field = fieldSelector(ADDRESS);
-    const submittedValue = 'a'.repeat(1001);
+    const submittedValue = 'a'.repeat(MAXIMUM_CHARACTERS.FULL_ADDRESS + 1);
 
     const expectedErrorsCount = 2;
     const errorIndex = 1;
