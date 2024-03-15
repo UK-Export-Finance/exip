62fbf62c45f5006472fa2133f25c1cb54b9be564
ttbarnes <tony@tonybarnes.me>
2024-03-15 11:22:31 +0000

    chore(EMS-3077): no pdf - modular policy routes

--- e2e-tests/constants/routes/insurance/policy/another-company.js
+++ e2e-tests/constants/routes/insurance/policy/another-company.js
@@ -2,11 +2,13 @@ import POLICY_ROOT from './root';
 
 const ROOT = `${POLICY_ROOT}/another-company`;
 
+const OTHER_COMPANY_DETAILS_ROOT = `${ROOT}/other-company-details`;
+
 const ANOTHER_COMPANY_ROUTES = {
   ANOTHER_COMPANY: ROOT,
   ANOTHER_COMPANY_SAVE_AND_BACK: `${ROOT}/save-and-go-back`,
-  OTHER_COMPANY_DETAILS: `${POLICY_ROOT}/other-company-details`,
-  OTHER_COMPANY_DETAILS_SAVE_AND_BACK: `${POLICY_ROOT}/other-company-details/save-and-go-back`,
+  OTHER_COMPANY_DETAILS: OTHER_COMPANY_DETAILS_ROOT,
+  OTHER_COMPANY_DETAILS_SAVE_AND_BACK: `${OTHER_COMPANY_DETAILS_ROOT}/save-and-go-back`,
 };
 
 export default ANOTHER_COMPANY_ROUTES;
