4dda178f2f581f348f190297d4ba568c9f7a0157
ttbarnes <tony@tonybarnes.me>
2024-03-26 15:49:09 +0000

    feat(EMS-2565): fix commands issue after merge

--- e2e-tests/insurance/cypress/support/application/export-contract/index.js
+++ e2e-tests/insurance/cypress/support/application/export-contract/index.js
@@ -1,4 +1,5 @@
 Cypress.Commands.add('completeAboutGoodsOrServicesForm', require('../../../../../commands/insurance/complete-about-goods-or-services-form'));
 Cypress.Commands.add('completeAndSubmitAboutGoodsOrServicesForm', require('../../../../../commands/insurance/complete-and-submit-about-goods-or-services-form'));
+Cypress.Commands.add('completeHowYouWillGetPaidForm', require('../../../../../commands/insurance/complete-how-you-will-get-paid-form'));
 Cypress.Commands.add('completeAndSubmitHowYouWillGetPaidForm', require('../../../../../commands/insurance/complete-and-submit-how-you-will-get-paid-form'));
 Cypress.Commands.add('completeAndSubmitPrivateMarketForm', require('../../../../../commands/insurance/complete-and-submit-private-market-form'));
