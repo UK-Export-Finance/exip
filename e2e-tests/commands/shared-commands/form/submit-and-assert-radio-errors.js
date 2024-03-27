7017d57fd098665924f1b49faa8d46dbacd9eb90
ttbarnes <tony@tonybarnes.me>
2024-03-27 12:25:09 +0000

    chore(EMS-3109): improve documentation

--- e2e-tests/commands/shared-commands/form/submit-and-assert-radio-errors.js
+++ e2e-tests/commands/shared-commands/form/submit-and-assert-radio-errors.js
@@ -1,18 +1,16 @@
 import partials from '../../../partials';
 
 /**
- * @param {Object} field: Cypress selector
- * @param {Number} errorIndex: index of error in errorSummary
- * @param {Number} expectedErrorsCount: the number of expected errors in errorSummary
- * @param {Number} errorMessage: error message to assert
- * @param {Number} inlineErrorIndex: the index of radios to find error
+ * @param {Object} field: Cypress selector.
+ * @param {Number} errorIndex: Index of error. Defaults to 0.
+ * @param {Number} expectedErrorsCount: Expected total amount of errors in the errors summary.
+ * @param {Number} expectedErrorMessage: Expected error message.
  */
 const submitAndAssertRadioErrors = ({
   field,
   errorIndex = 0,
   expectedErrorsCount,
   expectedErrorMessage,
-  inlineErrorIndex = 0,
 }) => {
   cy.clickSubmitButton();
 
@@ -29,7 +27,7 @@ const submitAndAssertRadioErrors = ({
 
   field.input().should('have.focus');
 
-  cy.checkText(field.errorMessage().eq(inlineErrorIndex), `Error: ${expectedErrorMessage}`);
+  cy.checkText(field.errorMessage().eq(errorIndex), `Error: ${expectedErrorMessage}`);
 };
 
 export default submitAndAssertRadioErrors;
