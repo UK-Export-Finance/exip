755d488c9a3736fa9d12b472715f672e38415ae8
Tony Barnes <tony@tonybarnes.me>
2024-03-27 12:42:13 +0000

    feat(EMS-2940): no pdf - export contract - declined by private market - page setup (#2163)

---
+++ src/ui/server/controllers/insurance/export-contract/declined-by-private-market/index.test.ts
@@ -0,0 +1,103 @@
+import { pageVariables, FIELD_ID, PAGE_CONTENT_STRINGS, TEMPLATE, get } from '.';
+import { TEMPLATES } from '../../../../constants';
+import { INSURANCE_ROUTES } from '../../../../constants/routes/insurance';
+import EXPORT_CONTRACT_FIELD_IDS from '../../../../constants/field-ids/insurance/export-contract';
+import { PAGES } from '../../../../content-strings';
+import { EXPORT_CONTRACT_FIELDS as FIELDS } from '../../../../content-strings/fields/insurance/export-contract';
+import singleInputPageVariables from '../../../../helpers/page-variables/single-input/insurance';
+import getUserNameFromSession from '../../../../helpers/get-user-name-from-session';
+import mapApplicationToFormFields from '../../../../helpers/mappings/map-application-to-form-fields';
+import { Request, Response } from '../../../../../types';
+import { mockReq, mockRes, mockApplication } from '../../../../test-mocks';
+
+const { INSURANCE_ROOT, PROBLEM_WITH_SERVICE } = INSURANCE_ROUTES;
+
+const {
+  PRIVATE_MARKET: { DECLINED_DESCRIPTION },
+} = EXPORT_CONTRACT_FIELD_IDS;
+
+const {
+  INSURANCE: {
+    EXPORT_CONTRACT: { DECLINED_BY_PRIVATE_MARKET },
+  },
+} = TEMPLATES;
+
+describe('controllers/insurance/export-contract/declined-by-private-market', () => {
+  let req: Request;
+  let res: Response;
+  let refNumber: number;
+
+  beforeEach(() => {
+    req = mockReq();
+    res = mockRes();
+
+    res.locals.application = mockApplication;
+    req.params.referenceNumber = String(mockApplication.referenceNumber);
+    refNumber = Number(mockApplication.referenceNumber);
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('PAGE_CONTENT_STRINGS', () => {
+    it('should have the correct strings', () => {
+      expect(PAGE_CONTENT_STRINGS).toEqual(PAGES.INSURANCE.EXPORT_CONTRACT.DECLINED_BY_PRIVATE_MARKET);
+    });
+  });
+
+  describe('pageVariables', () => {
+    it('should have correct properties', () => {
+      const result = pageVariables(refNumber);
+
+      const expected = {
+        FIELD: {
+          ID: FIELD_ID,
+          ...FIELDS.PRIVATE_MARKET[FIELD_ID],
+        },
+        SAVE_AND_BACK_URL: `${INSURANCE_ROOT}/${req.params.referenceNumber}#`,
+      };
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('TEMPLATE', () => {
+    it('should have the correct template defined', () => {
+      expect(TEMPLATE).toEqual(DECLINED_BY_PRIVATE_MARKET);
+    });
+  });
+
+  describe('FIELD_ID', () => {
+    it('should have the correct FIELD_ID', () => {
+      expect(FIELD_ID).toEqual(DECLINED_DESCRIPTION);
+    });
+  });
+
+  describe('get', () => {
+    it('should render template', () => {
+      get(req, res);
+
+      const expectedVariables = {
+        ...singleInputPageVariables({ FIELD_ID, PAGE_CONTENT_STRINGS, BACK_LINK: req.headers.referer }),
+        ...pageVariables(refNumber),
+        userName: getUserNameFromSession(req.session.user),
+        application: mapApplicationToFormFields(mockApplication),
+      };
+
+      expect(res.render).toHaveBeenCalledWith(TEMPLATE, expectedVariables);
+    });
+
+    describe('when there is no application', () => {
+      beforeEach(() => {
+        delete res.locals.application;
+      });
+
+      it(`should redirect to ${PROBLEM_WITH_SERVICE}`, () => {
+        get(req, res);
+
+        expect(res.redirect).toHaveBeenCalledWith(PROBLEM_WITH_SERVICE);
+      });
+    });
+  });
+});
