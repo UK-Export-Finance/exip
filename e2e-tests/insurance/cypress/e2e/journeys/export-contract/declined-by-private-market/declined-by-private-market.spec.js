8e16d30f0026da3ced3ca3c3a2fabcad93c34e10
ttbarnes <tony@tonybarnes.me>
2024-03-28 10:30:43 +0000

    chore(EMS-3109): fix/update e2e tests

--- e2e-tests/insurance/cypress/e2e/journeys/export-contract/declined-by-private-market/declined-by-private-market.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/export-contract/declined-by-private-market/declined-by-private-market.spec.js
@@ -29,8 +29,6 @@ const textareaField = {
   input: fieldSelector(FIELD_ID).textarea,
 };
 
-const expectedErrorsCount = 1;
-
 const baseUrl = Cypress.config('baseUrl');
 
 context('Insurance - Export contract - Declined by private market page - As an exporter, I want to explain why I could not get insurance through the private market previously, So that UKEF can accurately assess the viability of my insurance application', () => {
@@ -99,31 +97,21 @@ context('Insurance - Export contract - Declined by private market page - As an e
     });
 
     it(`should display validation errors if ${FIELD_ID} is left empty`, () => {
-      const errorMessage = ERRORS[FIELD_ID].IS_EMPTY;
-
-      cy.submitAndAssertFieldErrors(
-        textareaField,
-        null,
-        0,
-        expectedErrorsCount,
-        errorMessage,
-        true,
-      );
+      cy.submitAndAssertFieldErrors({
+        field: textareaField,
+        expectedErrorMessage: ERRORS[FIELD_ID].IS_EMPTY,
+      });
     });
 
     describe(`when ${FIELD_ID} is over ${MAXIMUM_CHARACTERS.DECLINED_BY_PRIVATE_MARKET_DESCRIPTION} characters`, () => {
       it('should display validation errors and retain the submitted value', () => {
-        const errorMessage = ERRORS[FIELD_ID].ABOVE_MAXIMUM;
         const submittedValue = 'a'.repeat(MAXIMUM_CHARACTERS.DECLINED_BY_PRIVATE_MARKET_DESCRIPTION + 1);
 
-        cy.submitAndAssertFieldErrors(
-          textareaField,
-          submittedValue,
-          0,
-          expectedErrorsCount,
-          errorMessage,
-          true,
-        );
+        cy.submitAndAssertFieldErrors({
+          field: textareaField,
+          value: submittedValue,
+          expectedErrorMessage: ERRORS[FIELD_ID].ABOVE_MAXIMUM,
+        });
 
         cy.checkTextareaValue({
           fieldId: FIELD_ID,
