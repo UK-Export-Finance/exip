cfa8f332a539d2cc3fc4b93341e142f0d3e1b3f1
ttbarnes <tony@tonybarnes.me>
2024-04-12 10:30:02 +0100

    feat(EMS-3117): no pdf - export contract - agent service - ui flow

---
+++ e2e-tests/commands/insurance/complete-and-submit-agent-service-form.js
@@ -0,0 +1,29 @@
+import { field } from '../../pages/shared';
+import FIELD_IDS from '../../constants/field-ids/insurance/export-contract';
+import application from '../../fixtures/application';
+
+const {
+  AGENT_SERVICE: { SERVICE_DESCRIPTION },
+} = FIELD_IDS;
+
+/**
+ * completeAndSubmitAgentServiceForm
+ * Complete and submit the "Agent service" form
+ */
+const completeAndSubmitAgentServiceForm = ({
+  serviceDescription = application.EXPORT_CONTRACT.AGENT_SERVICE[SERVICE_DESCRIPTION],
+  agentIsCharging = false,
+}) => {
+
+  cy.keyboardInput(field(SERVICE_DESCRIPTION).textarea(), serviceDescription);
+
+  if (agentIsCharging) {
+    cy.clickYesRadioInput();
+  } else {
+    cy.clickNoRadioInput();
+  }
+
+  cy.clickSubmitButton();
+};
+
+export default completeAndSubmitAgentServiceForm;
