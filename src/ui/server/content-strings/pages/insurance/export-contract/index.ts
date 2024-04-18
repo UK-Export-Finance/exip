5af489f8fd81cdd1152cb31db4ca1fe777f0ee1f
ttbarnes <tony@tonybarnes.me>
2024-04-18 16:20:13 +0100

    style(EMS-3170): no pdf - export contract - content update

--- src/ui/server/content-strings/pages/insurance/export-contract/index.ts
+++ src/ui/server/content-strings/pages/insurance/export-contract/index.ts
@@ -7,12 +7,7 @@ const ROOT = {
   PAGE_TITLE: 'Tell us about your export contract',
   LIST: {
     INTRO: "We'll ask you questions about:",
-    ITEMS: [
-      'what goods and services you want to export',
-      "how you'll get paid",
-      'your agent (if you have one)',
-      'your end buyer (if you have one)',
-    ],
+    ITEMS: ['what goods and services you want to export', "how you'll get paid", 'your agent (if you have one)', 'your end buyer (if you have one)'],
   },
   OUTRO:
     "We may also ask you about your approach to the private insurance market. This is because we provide credit insurance for exporters who haven't been able to get a policy through the private market.",
