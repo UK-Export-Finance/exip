6a15356ffcb8fd445e30efe3a430295dd96cc3fe
ttbarnes <tony@tonybarnes.me>
2024-04-20 15:44:26 +0100

    feat(EMS-3122): fix/update data mapping/saving issue

--- src/ui/server/controllers/insurance/export-contract/map-submitted-data/agent-service-charge/index.test.ts
+++ src/ui/server/controllers/insurance/export-contract/map-submitted-data/agent-service-charge/index.test.ts
@@ -2,26 +2,10 @@ import mapSubmittedData from '.';
 import FIELD_IDS from '../../../../../constants/field-ids/insurance/export-contract';
 
 const {
-  AGENT_CHARGES: { FIXED_SUM_AMOUNT, CHARGE_PERCENTAGE },
+  AGENT_CHARGES: { CHARGE_PERCENTAGE, FIXED_SUM_AMOUNT, METHOD },
 } = FIELD_IDS;
 
 describe('controllers/insurance/export-contract/map-submitted-data/agent-service-charge', () => {
-  describe(`when ${FIXED_SUM_AMOUNT} is provided`, () => {
-    it(`should return the form body with mapped ${FIXED_SUM_AMOUNT} as a number`, () => {
-      const mockFormBody = {
-        [FIXED_SUM_AMOUNT]: '1',
-      };
-
-      const result = mapSubmittedData(mockFormBody);
-
-      const expected = {
-        [FIXED_SUM_AMOUNT]: Number(mockFormBody[FIXED_SUM_AMOUNT]),
-      };
-
-      expect(result).toEqual(expected);
-    });
-  });
-
   describe(`when ${CHARGE_PERCENTAGE} is provided`, () => {
     it(`should return the form body with mapped ${CHARGE_PERCENTAGE} as a number`, () => {
       const mockFormBody = {
@@ -38,25 +22,57 @@ describe('controllers/insurance/export-contract/map-submitted-data/agent-service
     });
   });
 
-  describe(`when ${FIXED_SUM_AMOUNT} and ${CHARGE_PERCENTAGE} are provided`, () => {
-    it('should return the form body with both fields mapped as a number', () => {
+  describe(`when ${FIXED_SUM_AMOUNT} is provided`, () => {
+    it(`should return the form body with mapped ${FIXED_SUM_AMOUNT} as a number`, () => {
       const mockFormBody = {
-        [FIXED_SUM_AMOUNT]: '0',
-        [CHARGE_PERCENTAGE]: '1',
+        [FIXED_SUM_AMOUNT]: '1',
       };
 
       const result = mapSubmittedData(mockFormBody);
 
       const expected = {
         [FIXED_SUM_AMOUNT]: Number(mockFormBody[FIXED_SUM_AMOUNT]),
-        [CHARGE_PERCENTAGE]: Number(mockFormBody[CHARGE_PERCENTAGE]),
       };
 
       expect(result).toEqual(expected);
     });
   });
 
-  describe(`when neither ${FIXED_SUM_AMOUNT} or ${CHARGE_PERCENTAGE} are provided`, () => {
+  describe(`when ${CHARGE_PERCENTAGE} and ${FIXED_SUM_AMOUNT} are provided`, () => {
+    it('should return the form body with both fields mapped as a number', () => {
+      const mockFormBody = {
+        [CHARGE_PERCENTAGE]: '0',
+        [FIXED_SUM_AMOUNT]: '1',
+      };
+
+      const result = mapSubmittedData(mockFormBody);
+
+      const expected = {
+        [CHARGE_PERCENTAGE]: Number(mockFormBody[CHARGE_PERCENTAGE]),
+        [FIXED_SUM_AMOUNT]: Number(mockFormBody[FIXED_SUM_AMOUNT]),
+      };
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe(`when an empty ${METHOD} string is provided`, () => {
+    it(`should return the form body with ${METHOD} as null`, () => {
+      const mockFormBody = {
+        [METHOD]: '',
+      };
+
+      const result = mapSubmittedData(mockFormBody);
+
+      const expected = {
+        [METHOD]: null,
+      };
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe(`when neither ${CHARGE_PERCENTAGE}, ${FIXED_SUM_AMOUNT} or ${METHOD} are provided`, () => {
     it('should return the form body as provided', () => {
       const mockFormBody = {
         mockField: true,
