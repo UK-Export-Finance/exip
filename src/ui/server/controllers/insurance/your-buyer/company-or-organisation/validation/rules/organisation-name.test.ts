e2f7ab28400d526c65018bd9b669f35e081d48a7
ttbarnes <tony@tonybarnes.me>
2024-04-27 11:42:38 +0100

    feat(EMS-3125): no pdf - buyer - company or organisation name - maximum length

--- src/ui/server/controllers/insurance/your-buyer/company-or-organisation/validation/rules/organisation-name.test.ts
+++ src/ui/server/controllers/insurance/your-buyer/company-or-organisation/validation/rules/organisation-name.test.ts
@@ -1,7 +1,8 @@
 import companyOrOrganisationNameRules from './organisation-name';
+import { MAXIMUM_CHARACTERS } from '../../../../../../constants/validation';
 import FIELD_IDS from '../../../../../../constants/field-ids/insurance/your-buyer';
 import { ERROR_MESSAGES } from '../../../../../../content-strings';
-import emptyFieldValidation from '../../../../../../shared-validation/empty-field';
+import providedAndMaxLength from '../../../../../../shared-validation/provided-and-max-length';
 import { mockErrors } from '../../../../../../test-mocks';
 
 const {
@@ -11,20 +12,20 @@ const {
 const {
   INSURANCE: {
     YOUR_BUYER: {
-      COMPANY_OR_ORGANISATION: { [FIELD_ID]: ERROR_MESSAGE },
+      COMPANY_OR_ORGANISATION: { [FIELD_ID]: ERROR_MESSAGES_OBJECT },
     },
   },
 } = ERROR_MESSAGES;
 
-describe('controllers/insurance/your-buyer/validation/organisation', () => {
+describe('controllers/insurance/your-buyer/validation/organisation-name', () => {
   const mockBody = {
     [FIELD_ID]: '',
   };
 
-  it('should return the result of emptyFieldValidation', () => {
+  it('should return the result of providedAndMaxLength', () => {
     const result = companyOrOrganisationNameRules(mockBody, mockErrors);
 
-    const expected = emptyFieldValidation(mockBody, FIELD_ID, ERROR_MESSAGE.IS_EMPTY, mockErrors);
+    const expected = providedAndMaxLength(mockBody, FIELD_ID, ERROR_MESSAGES_OBJECT, mockErrors, MAXIMUM_CHARACTERS.BUYER_COMPANY_OR_ORGANISATION);
 
     expect(result).toEqual(expected);
   });
