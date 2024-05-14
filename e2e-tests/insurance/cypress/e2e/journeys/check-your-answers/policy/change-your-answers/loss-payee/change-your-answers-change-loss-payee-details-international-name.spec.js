3596ef78efc23f7019047db73a60938eb2ce0e22
ttbarnes <tony@tonybarnes.me>
2024-05-14 17:13:30 +0100

    feat(EMS-3285-3286): fix/update e2e tests

--- e2e-tests/insurance/cypress/e2e/journeys/check-your-answers/policy/change-your-answers/loss-payee/change-your-answers-change-loss-payee-details-international-name.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/check-your-answers/policy/change-your-answers/loss-payee/change-your-answers-change-loss-payee-details-international-name.spec.js
@@ -10,7 +10,10 @@ const {
 
 const {
   ROOT,
-  POLICY: { LOSS_PAYEE_DETAILS_CHECK_AND_CHANGE },
+  POLICY: {
+    LOSS_PAYEE_DETAILS_CHECK_AND_CHANGE,
+    LOSS_PAYEE_FINANCIAL_DETAILS_INTERNATIONAL_CHECK_AND_CHANGE,
+  },
   CHECK_YOUR_ANSWERS: { TYPE_OF_POLICY },
 } = INSURANCE_ROUTES;
 
@@ -74,12 +77,14 @@ context(`Insurance - Change your answers - Policy - Loss payee details - Interna
       cy.navigateToUrl(checkYourAnswersUrl);
     });
 
-    it(`should redirect to ${TYPE_OF_POLICY}`, () => {
+    it(`should redirect to ${LOSS_PAYEE_FINANCIAL_DETAILS_INTERNATIONAL_CHECK_AND_CHANGE} and then ${TYPE_OF_POLICY}`, () => {
       summaryList.field(fieldId).changeLink().click();
 
       cy.assertUrl(`${lossPayeeDetailsUrl}#${NAME}-label`);
 
       cy.changeAnswerField({ newValueInput }, field(fieldId).input());
+
+      cy.completeAndSubmitLossPayeeFinancialDetailsInternationalForm({});
 
       cy.assertChangeAnswersPageUrl({ referenceNumber, route: TYPE_OF_POLICY, fieldId });
     });
--- e2e-tests/insurance/cypress/e2e/journeys/check-your-answers/policy/change-your-answers/loss-payee/change-your-answers-change-loss-payee-details-uk-name.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/check-your-answers/policy/change-your-answers/loss-payee/change-your-answers-change-loss-payee-details-uk-name.spec.js
@@ -10,7 +10,10 @@ const {
 
 const {
   ROOT,
-  POLICY: { LOSS_PAYEE_DETAILS_CHECK_AND_CHANGE },
+  POLICY: {
+    LOSS_PAYEE_DETAILS_CHECK_AND_CHANGE,
+    LOSS_PAYEE_FINANCIAL_DETAILS_UK_CHECK_AND_CHANGE,
+  },
   CHECK_YOUR_ANSWERS: { TYPE_OF_POLICY },
 } = INSURANCE_ROUTES;
 
@@ -74,12 +77,14 @@ context(`Insurance - Change your answers - Policy - Loss payee details - UK - ${
       cy.navigateToUrl(checkYourAnswersUrl);
     });
 
-    it(`should redirect to ${TYPE_OF_POLICY}`, () => {
+    it(`should redirect to ${LOSS_PAYEE_FINANCIAL_DETAILS_UK_CHECK_AND_CHANGE} and then ${TYPE_OF_POLICY}`, () => {
       summaryList.field(fieldId).changeLink().click();
 
       cy.assertUrl(`${lossPayeeDetailsUrl}#${NAME}-label`);
 
       cy.changeAnswerField({ newValueInput }, field(fieldId).input());
+
+      cy.completeAndSubmitLossPayeeFinancialDetailsUkForm({});
 
       cy.assertChangeAnswersPageUrl({ referenceNumber, route: TYPE_OF_POLICY, fieldId });
     });
