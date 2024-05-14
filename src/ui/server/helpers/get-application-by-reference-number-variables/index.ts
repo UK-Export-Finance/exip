81add93fd4a33f5c96dcf9fe552da1f7fc82bd99
ttbarnes <tony@tonybarnes.me>
2024-05-14 13:59:22 +0100

    feat(EMS-3285-3286): change your answers - policy - update middleware to return decrypted data

--- src/ui/server/helpers/get-application-by-reference-number-variables/index.ts
+++ src/ui/server/helpers/get-application-by-reference-number-variables/index.ts
@@ -1,6 +1,11 @@
-import { POLICY } from '../../constants/routes/insurance/policy';
+import { INSURANCE_ROUTES } from '../../constants/routes/insurance';
 
-const { CHECK_YOUR_ANSWERS, LOSS_PAYEE_FINANCIAL_DETAILS_UK_ROOT, LOSS_PAYEE_FINANCIAL_DETAILS_INTERNATIONAL_ROOT } = POLICY;
+const {
+  CHECK_YOUR_ANSWERS: { TYPE_OF_POLICY },
+  POLICY: {
+    CHECK_YOUR_ANSWERS, LOSS_PAYEE_FINANCIAL_DETAILS_UK_ROOT, LOSS_PAYEE_FINANCIAL_DETAILS_INTERNATIONAL_ROOT
+  },
+} = INSURANCE_ROUTES;
 
 /**
  * getApplicationByReferenceNumberVariables
@@ -30,13 +35,13 @@ const getApplicationByReferenceNumberVariables = (referenceNumber: string, url: 
   }
 
   /**
-   * if url includes CHECK_YOUR_ANSWERS,
+   * if url includes CHECK_YOUR_ANSWERS or TYPE_OF_POLICY
    * set decryptFinancialUk and decryptFinancialInternational to true.
    * Although the data would only have either UK or international data;
    * At this stage, it is not possible to determine which piece of data is available,
    * because we do not have the application data available.
    */
-  if (url.includes(CHECK_YOUR_ANSWERS)) {
+  if (url.includes(CHECK_YOUR_ANSWERS) || url.includes(TYPE_OF_POLICY)) {
     decryptFinancialUk = true;
     decryptFinancialInternational = true;
   }
