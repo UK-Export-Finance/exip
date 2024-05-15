a8f549fc1e3928d711160e76a7c17f0083ef1e99
ttbarnes <tony@tonybarnes.me>
2024-05-15 09:10:20 +0100

    chore(tests): improve buyer e2e tests

--- e2e-tests/commands/insurance/your-buyer/complete-connection-with-the-buyer-form.js
+++ e2e-tests/commands/insurance/your-buyer/complete-connection-with-the-buyer-form.js
@@ -7,12 +7,12 @@ const {
 } = INSURANCE_FIELD_IDS.YOUR_BUYER;
 
 /**
- * completeConnectionToTheBuyerForm
+ * completeConnectionWithTheBuyerForm
  * Complete the "connection with the buyer" form.
  * @param {Boolean} hasConnectionToBuyer: Should submit "yes" to "have connection to buyer" radio. Defaults to "no".
  * @param {String} description: "Connection with buyer" description.
  */
-const completeConnectionToTheBuyerForm = ({
+const completeConnectionWithTheBuyerForm = ({
   hasConnectionToBuyer = false,
   description = application.BUYER[CONNECTION_WITH_BUYER_DESCRIPTION],
 }) => {
@@ -24,4 +24,4 @@ const completeConnectionToTheBuyerForm = ({
   }
 };
 
-export default completeConnectionToTheBuyerForm;
+export default completeConnectionWithTheBuyerForm;
