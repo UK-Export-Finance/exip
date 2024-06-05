b1e4f514bbb81b3f202a5af34a1fee4660e46cdb
ttbarnes <tony@tonybarnes.me>
2024-06-05 17:46:55 +0100

    fix(EMS-3413): address sonarcloud issue

--- src/ui/server/helpers/required-fields/section-review/index.ts
+++ src/ui/server/helpers/required-fields/section-review/index.ts
@@ -5,6 +5,6 @@ import FIELD_IDS from '../../../constants/field-ids/insurance/check-your-answers
  * Required fields for the insurance - check your answers section
  * @returns {Array<string>} Required field IDs
  */
-const requiredFields = Object.values(FIELD_IDS) as Array<string>;
+const requiredFields = Object.values(FIELD_IDS);
 
 export default requiredFields;
