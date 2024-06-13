4352417fe9dd5db242dd124098086b7a289e0f74
ttbarnes <tony@tonybarnes.me>
2024-06-13 16:08:13 +0100

    feat(EMS-3342): fix/update unit tests/mocks

--- src/api/generate-xlsx/map-application-to-XLSX/map-buyer/map-previous-cover-with-buyer/index.test.ts
+++ src/api/generate-xlsx/map-application-to-XLSX/map-buyer/map-previous-cover-with-buyer/index.test.ts
@@ -4,31 +4,36 @@ import FIELD_IDS from '../../../../constants/field-ids/insurance/your-buyer';
 import { XLSX } from '../../../../content-strings';
 import mapYesNoField from '../../helpers/map-yes-no-field';
 import xlsxRow from '../../helpers/xlsx-row';
-import { mockApplication, mockApplicationSinglePolicyTotalContractValueOverThreshold } from '../../../../test-mocks';
+import { mockApplicationSinglePolicyTotalContractValueOverThreshold } from '../../../../test-mocks';
 
 const { HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER, PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER } = FIELD_IDS;
 
 const { FIELDS } = XLSX;
 
+const {
+  buyer: { relationship },
+  eligibility,
+} = mockApplicationSinglePolicyTotalContractValueOverThreshold;
+
 describe('api/generate-xlsx/map-application-to-xlsx/map-buyer/map-previous-cover-with-buyer', () => {
   describe(`when the total contract value is ${TOTAL_CONTRACT_VALUE.MORE_THAN_250K.VALUE}`, () => {
     describe(`${HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER} is true`, () => {
       it(`should return an array of mapped fields with ${HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER}`, () => {
-        const {
-          buyer: { relationship },
-          eligibility,
-        } = mockApplicationSinglePolicyTotalContractValueOverThreshold;
+        const mockRelationship = {
+          ...relationship,
+          [HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER]: true,
+        };
 
-        const result = mapPreviousCoverWithBuyer(eligibility, relationship);
+        const result = mapPreviousCoverWithBuyer(eligibility, mockRelationship);
 
         const expected = [
           xlsxRow(
             String(FIELDS[HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER]),
             mapYesNoField({
-              answer: relationship[HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER],
+              answer: mockRelationship[HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER],
             }),
           ),
-          xlsxRow(String(FIELDS[PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER]), relationship[PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER]),
+          xlsxRow(String(FIELDS[PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER]), mockRelationship[PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER]),
         ];
 
         expect(result).toEqual(expected);
@@ -37,10 +42,8 @@ describe('api/generate-xlsx/map-application-to-xlsx/map-buyer/map-previous-cover
 
     describe(`${HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER} is false`, () => {
       it(`should return an array of mapped fields without ${HAS_PREVIOUS_CREDIT_INSURANCE_COVER_WITH_BUYER}`, () => {
-        const { buyer, eligibility } = mockApplicationSinglePolicyTotalContractValueOverThreshold;
-
         const mockRelationship = {
-          ...buyer.relationship,
+          ...relationship,
           exporterHasPreviousCreditInsuranceWithBuyer: false,
         };
 
@@ -62,11 +65,6 @@ describe('api/generate-xlsx/map-application-to-xlsx/map-buyer/map-previous-cover
 
   describe(`when the total contract value is NOT ${TOTAL_CONTRACT_VALUE.MORE_THAN_250K.VALUE}`, () => {
     it('should return an empty array', () => {
-      const {
-        buyer: { relationship },
-        eligibility,
-      } = mockApplication;
-
       const result = mapPreviousCoverWithBuyer(eligibility, relationship);
 
       expect(result).toEqual([]);
