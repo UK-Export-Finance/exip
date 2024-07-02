e0e8575a0d595c2d622361f4f331d77027ba98d0
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-07-02 14:28:11 +0100

    fix(EMS-3516): lint fix

--- e2e-tests/pages/insurance/eligibility/index.js
+++ e2e-tests/pages/insurance/eligibility/index.js
@@ -6,4 +6,6 @@ import endBuyerPage from './endBuyer';
 import otherPartiesPage from './otherParties';
 import contractTooShort from './contractTooShort';
 
-export { checkIfEligiblePage, companiesHouseUnavailablePage, companyDetailsPage, eligibleToApplyOnlinePage, endBuyerPage, otherPartiesPage, contractTooShort };
+export {
+  checkIfEligiblePage, companiesHouseUnavailablePage, companyDetailsPage, eligibleToApplyOnlinePage, endBuyerPage, otherPartiesPage, contractTooShort,
+};
