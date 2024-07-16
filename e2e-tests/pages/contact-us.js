7232698af6f27a8769f2e62ca96a8bd61a52136e
Tony Barnes <tony@tonybarnes.me>
2024-07-16 14:56:45 +0100

    feat(EMS-3587-3425): contact page - content iterations (#2742)
    * feat(EMS-3587): contact page - content iterations
    
    * chore(EMS-3587): remove commented code
    
    * fix(EMS-3425): password reset - update label text
    
    * fix(EMS-3425): password reset - update label text

--- e2e-tests/pages/contact-us.js
+++ e2e-tests/pages/contact-us.js
@@ -1,16 +1,12 @@
 export const contactUsPage = {
   heading: () => cy.get('[data-cy="heading"]'),
-  whoToContactText: () => cy.get('[data-cy="who-to-contact"]'),
+  quoteReferenceNumber: () => cy.get('[data-cy="general-enquiries-quote-reference-number"]'),
   generalEnquiries: {
     heading: () => cy.get('[data-cy="general-enquiries-heading"]'),
-    emailPrefix: () => cy.get('[data-cy="general-enquiries-email-prefix"]'),
-    emailLink: () => cy.get('[data-cy="general-enquiries-email-link"]'),
-    quoteReferenceNumber: () => cy.get('[data-cy="general-enquiries-quote-reference-number"]'),
   },
   applicationEnquiries: {
     heading: () => cy.get('[data-cy="application-enquiries-heading"]'),
     emailPrefix: () => cy.get('[data-cy="application-enquiries-email-prefix"]'),
     emailLink: () => cy.get('[data-cy="application-enquiries-email-link"]'),
-    quoteReferenceNumber: () => cy.get('[data-cy="application-enquiries-quote-reference-number"]'),
   },
 };
