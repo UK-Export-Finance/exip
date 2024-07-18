521a68ee1cc5e8b1d94551d60d95fa020232bc33
ttbarnes <tony@tonybarnes.me>
2024-07-18 10:57:14 +0100

    chore(EMS-3465): minor e2e test improvement

--- e2e-tests/insurance/cypress/e2e/journeys/account/password-reset/account-password-reset-retry/account-password-reset-retry-maximum-retries.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/account/password-reset/account-password-reset-retry/account-password-reset-retry-maximum-retries.spec.js
@@ -15,6 +15,7 @@ context(
     const accountSuspendedUrl = `${baseUrl}${SUSPENDED_ROOT}`;
 
     let account;
+    let expectedUrl;
 
     before(() => {
       cy.deleteAccount();
@@ -53,7 +54,7 @@ context(
           const [firstAccount] = responseData;
           account = firstAccount;
 
-          const expectedUrl = `${accountSuspendedUrl}?id=${account.id}`;
+          expectedUrl = `${accountSuspendedUrl}?id=${account.id}`;
 
           cy.assertUrl(expectedUrl);
         });
@@ -70,8 +71,6 @@ context(
       });
 
       it(`should redirect to ${SUSPENDED_ROOT} with ID query param`, () => {
-        const expectedUrl = `${accountSuspendedUrl}?id=${account.id}`;
-
         cy.assertUrl(expectedUrl);
       });
     });
