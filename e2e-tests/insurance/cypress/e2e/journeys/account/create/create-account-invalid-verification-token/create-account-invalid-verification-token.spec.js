3cd0ac08dee37b6e320261522d988ba6a2cf0136
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-07-19 11:44:24 +0100

    chore(EMS-3429): lint

--- e2e-tests/insurance/cypress/e2e/journeys/account/create/create-account-invalid-verification-token/create-account-invalid-verification-token.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/account/create/create-account-invalid-verification-token/create-account-invalid-verification-token.spec.js
@@ -8,50 +8,47 @@ const {
   },
 } = INSURANCE_ROUTES;
 
-context(
-  'Insurance - Account - Create - Create account and verify email with invalid token in url',
-  () => {
-    const baseUrl = Cypress.config('baseUrl');
-    let accountId;
-    const verifyEmailLinkInvalidUrl = `${baseUrl}${VERIFY_EMAIL_INVALID_LINK}`;
+context('Insurance - Account - Create - Create account and verify email with invalid token in url', () => {
+  const baseUrl = Cypress.config('baseUrl');
+  let accountId;
+  const verifyEmailLinkInvalidUrl = `${baseUrl}${VERIFY_EMAIL_INVALID_LINK}`;
 
-    before(() => {
-      cy.deleteAccount();
+  before(() => {
+    cy.deleteAccount();
 
-      cy.clearCookies();
-      Cypress.session.clearAllSavedSessions();
+    cy.clearCookies();
+    Cypress.session.clearAllSavedSessions();
 
-      cy.navigateToCheckIfEligibleUrl();
+    cy.navigateToCheckIfEligibleUrl();
 
-      cy.submitEligibilityAndStartAccountCreation();
-      cy.completeAndSubmitCreateAccountForm();
+    cy.submitEligibilityAndStartAccountCreation();
+    cy.completeAndSubmitCreateAccountForm();
 
-      const expected = `${baseUrl}${CONFIRM_EMAIL}`;
+    const expected = `${baseUrl}${CONFIRM_EMAIL}`;
 
-      cy.assertUrl(expected);
+    cy.assertUrl(expected);
 
-      cy.getAccountByEmail(accountEmail).then((responseData) => {
-        const [firstAccount] = responseData;
+    cy.getAccountByEmail(accountEmail).then((responseData) => {
+      const [firstAccount] = responseData;
 
-        const { id } = firstAccount;
+      const { id } = firstAccount;
 
-        accountId = id;
-      });
+      accountId = id;
     });
+  });
 
-    beforeEach(() => {
-      cy.saveSession();
+  beforeEach(() => {
+    cy.saveSession();
+  });
+
+  describe(`when navigating to ${VERIFY_EMAIL} with an invalid token in the url`, () => {
+    it(`should redirect to ${VERIFY_EMAIL_INVALID_LINK}`, () => {
+      const invalidToken = 'invalid';
+      const url = `${baseUrl}${VERIFY_EMAIL}?token=${invalidToken}&id=${accountId}`;
+
+      cy.navigateToUrl(url);
+
+      cy.assertUrl(verifyEmailLinkInvalidUrl);
     });
-
-    describe(`when navigating to ${VERIFY_EMAIL} with an invalid token in the url`, () => {
-      it(`should redirect to ${VERIFY_EMAIL_INVALID_LINK}`, () => {
-        const invalidToken = 'invalid';
-        const url = `${baseUrl}${VERIFY_EMAIL}?token=${invalidToken}&id=${accountId}`;
-
-        cy.navigateToUrl(url);
-
-        cy.assertUrl(verifyEmailLinkInvalidUrl);
-      });
-    });
-  },
-);
+  });
+});
