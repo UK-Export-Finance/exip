7e5c0c79cfca1c8fa578495dbff0034d14bc0263
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-07-23 13:50:24 +0100

    chore(EMS-3631): change e2e content strings

---
+++ e2e-tests/content-strings/pages/insurance/declarations/anti-bribery-exporting-with-code-of-conduct/index.js
@@ -0,0 +1,11 @@
+import { SHARED } from '../shared';
+
+export const ANTI_BRIBERY_EXPORTING_WITH_CODE_OF_CONDUCT = {
+  ...SHARED,
+  PAGE_TITLE: 'Will you use your anti-bribery code of conduct to win or carry out the exports you want to insure?',
+  VERSIONS: [
+    {
+      VERSION: 2,
+    },
+  ],
+};
