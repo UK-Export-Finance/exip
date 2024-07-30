b29a6f94d03233d66c1012b21fab00fff4d01917
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-07-30 15:18:08 +0100

    feat(EMS-3668-69-73-74): fix failing test

--- e2e-tests/partials/insurance/memberOfAGroup.js
+++ e2e-tests/partials/insurance/memberOfAGroup.js
@@ -1,11 +1,11 @@
 import { INSURANCE_FIELD_IDS } from '../../constants/field-ids/insurance';
 
-const { MEMBER_OF_A_GROUP } = INSURANCE_FIELD_IDS.ELIGIBILITY;
+const { IS_MEMBER_OF_A_GROUP } = INSURANCE_FIELD_IDS.ELIGIBILITY;
 
 const memberOfAGroupPartial = {
-  summary: () => cy.get(`[data-cy="${MEMBER_OF_A_GROUP}-description"] summary`),
-  details: () => cy.get(`[data-cy="${MEMBER_OF_A_GROUP}-description"]`),
-  description: () => cy.get(`[data-cy="${MEMBER_OF_A_GROUP}-description-details"]`),
+  summary: () => cy.get(`[data-cy="${IS_MEMBER_OF_A_GROUP}-description"] summary`),
+  details: () => cy.get(`[data-cy="${IS_MEMBER_OF_A_GROUP}-description"]`),
+  description: () => cy.get(`[data-cy="${IS_MEMBER_OF_A_GROUP}-description-details"]`),
 };
 
 export default memberOfAGroupPartial;
