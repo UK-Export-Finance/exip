c786848faa9efb873a43c1c971806bf9333af37f
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-08-14 17:28:49 +0100

    fix(EMS-3755): fix updated link

--- src/ui/server/content-strings/links.ts
+++ src/ui/server/content-strings/links.ts
@@ -13,7 +13,7 @@ export const LINKS = {
   GIVE_FEEDBACK_LOWERCASE: 'give feedback',
   SKIP_TO_MAIN_CONTENT: 'Skip to main content',
   EXTERNAL: {
-    GUIDANCE: 'https://www.gov.uk/guidance/export-insurance-policy#eligibility',
+    GUIDANCE: 'https://www.gov.uk/guidance/credit-insurance-policy#eligibility',
     BEFORE_YOU_START: 'https://www.gov.uk/guidance/get-a-quote-for-ukef-export-insurance',
     PRIVACY: 'https://www.gov.uk/government/publications/ukef-privacy-notice/ukef-privacy-notice--2',
     FEEDBACK: 'https://forms.office.com/r/TacytrRCgJ',
