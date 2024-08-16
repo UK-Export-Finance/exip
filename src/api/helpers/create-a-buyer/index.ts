994e19a3c692a4ba88c5f6b406e29af16ad509e5
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2024-08-16 09:56:41 +0100

    fix(EMS-3755): fix failing e2e tests (#2949)
    * fix(EMS-3755): fixed failing e2e tests
    
    * fix(EMS-3755): added to dockerfile
    
    * fix(EMS-3755): fix updated link
    
    * fix(EMS-3755): fix updated link
    
    * fix(EMS-3755): fix updated link
    
    * fix(EMS-3755): code fixes and added helpers
    
    * fix(EMS-3755): fix typos
    
    * fix(EMS-3755): code improvements
    
    * fix(EMS-3755): revert package.json change
    
    ---------
    
    Co-authored-by: Zain Kassam <zkassam@ukexportfinance.gov.uk>

--- src/api/helpers/create-a-buyer/index.ts
+++ src/api/helpers/create-a-buyer/index.ts
@@ -1,49 +1,27 @@
-import createABuyerTradingHistory from '../create-a-buyer-trading-history';
-import createABuyerContact from '../create-a-buyer-contact';
-import createABuyerRelationship from '../create-a-buyer-relationship';
 import { Context } from '../../types';
 
 /**
  * createABuyer
  * Create a buyer with:
- * 1) A country relationship
- * 2) An application relationship
- * 3) A Buyer trading history and relationship
  * @param {Context} context: KeystoneJS context API
  * @param {String} countryId: Country ID
- * @param {String} applicationId: Application ID
  * @returns {Promise<Object>} Created buyer
  */
-const createABuyer = async (context: Context, countryId: string, applicationId: string) => {
-  console.info('Creating a buyer for %s', applicationId);
+const createABuyer = async (context: Context, countryId: string) => {
+  console.info('Creating a buyer');
 
   try {
     const buyer = await context.db.Buyer.createOne({
       data: {
         country: {
-          connect: { id: countryId },
-        },
-        application: {
-          connect: { id: applicationId },
+          connect: {
+            id: countryId,
+          },
         },
       },
     });
 
-    // Create a buyer trading address with buyer relationships
-    const buyerTradingHistory = await createABuyerTradingHistory(context, buyer.id, applicationId);
-
-    // Create a buyer relationship row with buyer relationships
-    const buyerRelationship = await createABuyerRelationship(context, buyer.id, applicationId);
-
-    // Create a buyer contact with buyer relationship
-    const buyerContact = await createABuyerContact(context, buyer.id, applicationId);
-
-    return {
-      ...buyer,
-      buyerTradingHistory,
-      relationship: buyerRelationship,
-      buyerContact,
-    };
+    return buyer;
   } catch (error) {
     console.error('Error creating a buyer %O', error);
 
---
+++ src/api/helpers/update-applications-data/index.test.ts
@@ -0,0 +1,61 @@
+import updateApplicationsData from '.';
+import createManyApplications from '../create-many-applications-and-reference-numbers';
+import getKeystoneContext from '../../test-helpers/get-keystone-context';
+import INITIAL_APPLICATION_DATA from '../../constants/application/initial-application-data';
+import { Context, Application } from '../../types';
+
+describe('helpers/update-applications-data', () => {
+  let context: Context;
+  let applications: Array<Application>;
+  let referenceNumbers;
+
+  const assertError = (error) => {
+    const errorString = String(error);
+
+    expect(errorString.includes('Updating many applications - helper')).toEqual(true);
+  };
+
+  beforeAll(async () => {
+    context = getKeystoneContext();
+
+    ({ applications, referenceNumbers } = await createManyApplications(context, [INITIAL_APPLICATION_DATA, INITIAL_APPLICATION_DATA]));
+  });
+
+  it('should create multiple applications and reference numbers', async () => {
+    const update = [
+      {
+        where: { id: applications[0].id },
+        data: { referenceNumber: referenceNumbers[0].id },
+      },
+      {
+        where: { id: applications[1].id },
+        data: { referenceNumber: referenceNumbers[1].id },
+      },
+    ];
+
+    const createdApplications = await updateApplicationsData(context, update);
+
+    const expected = [
+      {
+        ...applications[0],
+        referenceNumber: referenceNumbers[0].id,
+      },
+      {
+        ...applications[1],
+        referenceNumber: referenceNumbers[1].id,
+      },
+    ];
+
+    expect(createdApplications).toEqual(expected);
+  });
+
+  describe('when update is not successful', () => {
+    test('it should throw an error', async () => {
+      try {
+        await updateApplicationsData({}, []);
+      } catch (error) {
+        assertError(error);
+      }
+    });
+  });
+});
