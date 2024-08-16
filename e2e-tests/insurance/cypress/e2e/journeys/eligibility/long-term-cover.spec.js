ca1e14925836b9a384bbe7b6b5d785a5cabdac94
Zain Kassam <zkassam@ukexportfinance.gov.uk>
2024-08-16 14:53:49 +0100

    chore(EMS-3759): fix failing test

--- e2e-tests/insurance/cypress/e2e/journeys/eligibility/long-term-cover.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/eligibility/long-term-cover.spec.js
@@ -15,7 +15,7 @@ context(
     let url;
 
     before(() => {
-      cy.completeAndSubmitEligibilityForms({ form: 'coverPeriod', completeAndSubmitEligibilityForms: false });
+      cy.completeAndSubmitEligibilityForms({ form: 'coverPeriod', coverPeriodIsUnderThreshold: false });
 
       url = `${baseUrl}${LONG_TERM_COVER}`;
 
