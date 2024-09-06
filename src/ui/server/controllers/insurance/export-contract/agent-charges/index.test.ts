945fd767e6572f0f07f7445863eef55d0712ce0b
ttbarnes <tony@tonybarnes.me>
2024-09-06 16:23:31 +0100

    feat(EMS-3825): update controller

--- src/ui/server/controllers/insurance/export-contract/agent-charges/index.test.ts
+++ src/ui/server/controllers/insurance/export-contract/agent-charges/index.test.ts
@@ -46,7 +46,7 @@ const {
 } = INSURANCE_ROUTES;
 
 const {
-  AGENT_CHARGES: { METHOD, PAYABLE_COUNTRY_CODE, FIXED_SUM, FIXED_SUM_AMOUNT, FIXED_SUM_CURRENCY_CODE, PERCENTAGE, PERCENTAGE_CHARGE },
+  AGENT_CHARGES: { METHOD, PAYABLE_COUNTRY_CODE, FIXED_SUM, FIXED_SUM_CURRENCY_CODE, PERCENTAGE, PERCENTAGE_CHARGE },
 } = EXPORT_CONTRACT_FIELD_IDS;
 
 const {
@@ -87,7 +87,7 @@ describe('controllers/insurance/export-contract/agent-charges', () => {
 
   describe('FIELD_IDS', () => {
     it('should have the correct FIELD_IDS', () => {
-      const expected = [METHOD, PAYABLE_COUNTRY_CODE, FIXED_SUM_AMOUNT, PERCENTAGE_CHARGE];
+      const expected = [METHOD, PAYABLE_COUNTRY_CODE, PERCENTAGE_CHARGE];
 
       expect(FIELD_IDS).toEqual(expected);
     });
@@ -124,11 +124,6 @@ describe('controllers/insurance/export-contract/agent-charges', () => {
           FIXED_SUM: {
             ID: FIXED_SUM,
             ...FIELDS.AGENT_CHARGES[FIXED_SUM],
-          },
-          FIXED_SUM_AMOUNT: {
-            ID: FIXED_SUM_AMOUNT,
-            ...FIELDS.AGENT_CHARGES[FIXED_SUM_AMOUNT],
-            LABEL: `${FIELDS.AGENT_CHARGES[FIXED_SUM_AMOUNT].LABEL} ${currency.name}?`,
           },
           PERCENTAGE: {
             ID: PERCENTAGE,
@@ -296,7 +291,6 @@ describe('controllers/insurance/export-contract/agent-charges', () => {
   describe('post', () => {
     const validBody = {
       [METHOD]: mockExportContractAgentServiceCharge[METHOD],
-      [FIXED_SUM_AMOUNT]: mockExportContractAgentServiceCharge[FIXED_SUM_AMOUNT],
       [PAYABLE_COUNTRY_CODE]: mockExportContractAgentServiceCharge[PAYABLE_COUNTRY_CODE],
     };
 
