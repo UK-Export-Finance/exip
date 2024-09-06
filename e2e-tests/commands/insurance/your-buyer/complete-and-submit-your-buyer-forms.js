c8c4db44bee54354f1a22713f320db51f55bc09d
ttbarnes <tony@tonybarnes.me>
2024-09-06 12:41:51 +0100

    chore(docs): improve completeAndSubmitYourBuyerForms documentation

--- e2e-tests/commands/insurance/your-buyer/complete-and-submit-your-buyer-forms.js
+++ e2e-tests/commands/insurance/your-buyer/complete-and-submit-your-buyer-forms.js
@@ -2,34 +2,30 @@
  * completeAndSubmitYourBuyerForms
  * completes your buyer forms up to the specified form to stop at
  * eg, when 'connectionWithTheBuyer' is passed, it will complete all forms up to and including 'connectionWithTheBuyer'
- * @param {String} formToStopAt: the form to stop at
- * @param {Boolean} viaTaskList: whether to complete the section via the task list
- * @param {Boolean} hasConnectionToBuyer: whether the exporter has a connection with the buyer
- * @param {Boolean} exporterHasTradedWithBuyer: whether the exporter has traded with the buyer
- * @param {Boolean} outstandingPayments: whether the exporter has outstanding payments with the buyer
- * @param {Boolean} failedToPay: whether the buyer has failed to pay the exporter
- * @param {Boolean} fullyPopulatedBuyerTradingHistory: whether to fully populate the buyer trading history form
- * @param {Boolean} exporterHasBuyerFinancialAccounts: whether the exporter has buyer financial accounts
- * @param {String} isoCode: Policy currency ISO code
  * @param {Boolean} alternativeCurrency: If alternative currency should be entered.
  * @param {Boolean} clickAlternativeCurrencyLink: if the "Alternative currency" link should be clicked.
+ * @param {Boolean} exporterHasTradedWithBuyer: whether the exporter has traded with the buyer
+ * @param {Boolean} failedToPay: whether the buyer has failed to pay the exporter
+ * @param {String} formToStopAt: the form to stop at
+ * @param {Boolean} fullyPopulatedBuyerTradingHistory: whether to fully populate the buyer trading history form
+ * @param {Boolean} hasConnectionToBuyer: whether the exporter has a connection with the buyer
+ * @param {Boolean} exporterHasBuyerFinancialAccounts: whether the exporter has buyer financial accounts
+ * @param {String} isoCode: Policy currency ISO code
+ * @param {Boolean} outstandingPayments: whether the exporter has outstanding payments with the buyer
+ * @param {Boolean} viaTaskList: whether to complete the section via the task list
  */
-
-// TODO
-// TODO - alphabetical ordered documentation
-// TODO - alphabetical ordered documentation
 const completeAndSubmitYourBuyerForms = ({
-  formToStopAt,
-  viaTaskList,
-  hasConnectionToBuyer,
-  exporterHasTradedWithBuyer,
-  outstandingPayments,
-  failedToPay,
-  fullyPopulatedBuyerTradingHistory,
-  exporterHasBuyerFinancialAccounts,
-  isoCode,
   alternativeCurrency,
   clickAlternativeCurrencyLink,
+  exporterHasTradedWithBuyer,
+  failedToPay,
+  formToStopAt,
+  fullyPopulatedBuyerTradingHistory,
+  exporterHasBuyerFinancialAccounts,
+  hasConnectionToBuyer,
+  isoCode,
+  outstandingPayments,
+  viaTaskList,
 }) => {
   cy.startInsuranceYourBuyerSection({ viaTaskList });
 
