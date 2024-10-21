e2e90b15227a59626d0b8c7af8642bf70519c8b9
ttbarnes <tony@tonybarnes.me>
2024-10-21 16:17:53 +0100

    chore(EMS-3901): update ui type

--- src/ui/types/application.ts
+++ src/ui/types/application.ts
@@ -279,13 +279,13 @@ interface Application extends ApplicationCore {
   sectionReview: ApplicationSectionReview;
   declaration: ApplicationDeclaration;
   totalContractValueOverThreshold: boolean;
-  migratedV2toV3: boolean;
+  migratedV2toV3?: boolean;
 }
 
 interface ApplicationFlatCore extends ApplicationCore, InsuranceEligibilityCore, ApplicationOwner {
   buyerCountry: string;
   totalContractValueOverThreshold?: boolean;
-  migratedV2toV3: boolean;
+  migratedV2toV3?: boolean;
 }
 
 type ApplicationFlat = ApplicationFlatCore & ApplicationPolicy & ApplicationBroker & ApplicationCompany & ApplicationDeclaration;
