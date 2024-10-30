53d3b7614958071453fa79ccc2409fc61a07c779
ttbarnes <tony@tonybarnes.me>
2024-10-30 16:12:36 +0000

    chore(EMS-3539): fix/update unit test

--- src/api/generate-xlsx/styled-columns/modify-row-heights/index.test.ts
+++ src/api/generate-xlsx/styled-columns/modify-row-heights/index.test.ts
@@ -1,14 +1,19 @@
 import modifyRowHeights from '.';
 import { XLSX_CONFIG } from '../../../constants';
+import SECTION_NAMES from '../../../constants/XLSX-CONFIG/SECTION_NAMES';
 import APPLICATION_INFORMATION_INDEXES from '../../../constants/XLSX-CONFIG/INDEXES/APPLICATION_INFORMATION';
 import createMockWorksheet from '../../../test-mocks/create-mock-worksheet';
 
 const { LARGE_ADDITIONAL_COLUMN_HEIGHT, ADDITIONAL_TITLE_COLUMN_HEIGHT } = XLSX_CONFIG;
 
+const { APPLICATION_INFORMATION } = SECTION_NAMES;
+
 const { EXPORTER_CONTACT_DETAILS, KEY_INFORMATION } = APPLICATION_INFORMATION_INDEXES;
 
 describe('api/generate-xlsx/styled-columns/modify-row-heights', () => {
-  const { mockWorksheet, mockSheetName } = createMockWorksheet();
+  const { mockWorksheet } = createMockWorksheet();
+
+  const mockSheetName = APPLICATION_INFORMATION;
 
   it('should add column heights to particular columns', async () => {
     const mockIndexes = [5, 6];
