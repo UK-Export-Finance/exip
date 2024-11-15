fa038b9f6cf0c7f048b15e308c7a5aeb55675e45
ttbarnes <tony@tonybarnes.me>
2024-11-15 13:04:15 +0000

    feat(EMS-3978): fix typo

--- src/ui/server/controllers/insurance/policy/broker-manual-address/validation/index.test.ts
+++ src/ui/server/controllers/insurance/policy/broker-manual-address/validation/index.test.ts
@@ -2,7 +2,7 @@ import validation from '.';
 import validationRules from './rules';
 import combineValidationRules from '../../../../../helpers/combine-validation-rules';
 
-describe('controllers/insurance/policy/broker-details/validation', () => {
+describe('controllers/insurance/policy/broker-manual-address/validation', () => {
   it('should return an array of results from rule functions', () => {
     const mockFormBody = {};
 
