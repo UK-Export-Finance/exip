0d702f015545a58c440fddce45797c0407822e64
Tony Barnes <tony@tonybarnes.me>
2024-11-28 14:51:38 +0000

    feat(EMS-3981-3984): broker addresses - ordnance survey integration (#3303)
    * feat(EMS-3981): broker addresses - initial page setup
    
    * feat(EMS-3981): update test.yml
    
    * feat(EMS-3981-3984): broker addresses - ordnance survey integration
    
    * feat(EMS-3981-3984): update ordnance survey addresses mapping
    
    * feat(EMS-3981-3984): add 'zero addresses' route constant
    
    * feat(EMS-3981-3984): broker addresses - update body text
    
    * feat(EMS-3981-3984): update ui ordnance survey mapping
    
    * feat(EMS-3981-3984): broker addresses - improve ordnance survey conditions
    
    * feat(EMS-3981-3984): fix/add broker addresses data-cy selectors
    
    * feat(EMS-3981-3984): initial e2e test coverage
    
    * chore(EMS-3981): remove todo comment
    
    * chore(EMS-3981): remove todo comment
    
    * feat(EMS-3981): remove commented code
    
    * chore(EMS-3981-3984): improve api error handling logging
    
    * chore(EMS-3981): fix typo
    
    * feat(EMS-3981): fix/update unit tests

--- src/ui/server/controllers/insurance/policy/broker-addresses/index.test.ts
+++ src/ui/server/controllers/insurance/policy/broker-addresses/index.test.ts
@@ -1,21 +1,41 @@
-import { PAGE_CONTENT_STRINGS, TEMPLATE, get } from '.';
+import { PAGE_CONTENT_STRINGS, TEMPLATE, get, pageVariables } from '.';
 import { PAGES } from '../../../../content-strings';
 import { TEMPLATES } from '../../../../constants';
 import { INSURANCE_ROUTES } from '../../../../constants/routes/insurance';
+import POLICY_FIELD_IDS from '../../../../constants/field-ids/insurance/policy';
+import { POLICY_FIELDS } from '../../../../content-strings/fields/insurance/policy';
 import insuranceCorePageVariables from '../../../../helpers/page-variables/core/insurance';
 import getUserNameFromSession from '../../../../helpers/get-user-name-from-session';
+import api from '../../../../api';
+import mapOrdnanceSurveyAddresses from '../../../../helpers/mappings/map-ordnance-survey-addresses';
 import { Request, Response } from '../../../../../types';
-import { mockReq, mockRes } from '../../../../test-mocks';
+import { mockReq, mockRes, mockOrdnanceSurveyAddressResponse, mockSpyPromiseRejection, referenceNumber } from '../../../../test-mocks';
 
-const { PROBLEM_WITH_SERVICE } = INSURANCE_ROUTES;
+const { SELECT_THE_ADDRESS } = POLICY_FIELD_IDS.BROKER_ADDRESSES;
+
+const {
+  INSURANCE_ROOT,
+  PROBLEM_WITH_SERVICE,
+  POLICY: { BROKER_DETAILS_ROOT, BROKER_ZERO_ADDRESSES_ROOT },
+} = INSURANCE_ROUTES;
+
+const { BROKER_ADDRESSES } = POLICY_FIELDS;
+
+const tempMockPostcode = 'W1A 1AA';
+const tempMockHouseNameOrNumber = 'WOGAN HOUSE';
+const mockTotalAddresses = mockOrdnanceSurveyAddressResponse.addresses.length;
 
 describe('controllers/insurance/policy/broker-addresses', () => {
   let req: Request;
   let res: Response;
 
+  const getOrdnanceSurveyAddressSpy = jest.fn(() => Promise.resolve(mockOrdnanceSurveyAddressResponse));
+
   beforeEach(() => {
     req = mockReq();
     res = mockRes();
+
+    api.keystone.getOrdnanceSurveyAddress = getOrdnanceSurveyAddressSpy;
   });
 
   afterAll(() => {
@@ -34,16 +54,44 @@ describe('controllers/insurance/policy/broker-addresses', () => {
     });
   });
 
+  describe('pageVariables', () => {
+    it('should have correct properties', () => {
+      const result = pageVariables(referenceNumber, mockTotalAddresses);
+
+      const expected = {
+        FIELD: {
+          ID: SELECT_THE_ADDRESS,
+          ...BROKER_ADDRESSES[SELECT_THE_ADDRESS],
+        },
+        BODY: `${mockTotalAddresses} ${PAGE_CONTENT_STRINGS.BODY}`,
+        SAVE_AND_BACK_URL: `${INSURANCE_ROOT}/${referenceNumber}#`,
+      };
+
+      expect(result).toEqual(expected);
+    });
+  });
+
   describe('get', () => {
-    it('should render template', () => {
-      get(req, res);
+    it('should call api.keystone.getOrdnanceSurveyAddress', async () => {
+      await get(req, res);
+
+      expect(getOrdnanceSurveyAddressSpy).toHaveBeenCalledTimes(1);
+
+      expect(getOrdnanceSurveyAddressSpy).toHaveBeenCalledWith(tempMockPostcode, tempMockHouseNameOrNumber);
+    });
+
+    it('should render template', async () => {
+      await get(req, res);
 
       expect(res.render).toHaveBeenCalledWith(TEMPLATE, {
         ...insuranceCorePageVariables({
           PAGE_CONTENT_STRINGS,
           BACK_LINK: req.headers.referer,
         }),
+        ...pageVariables(referenceNumber, mockOrdnanceSurveyAddressResponse.addresses.length),
         userName: getUserNameFromSession(req.session.user),
+        mappedAddresses: mapOrdnanceSurveyAddresses(mockOrdnanceSurveyAddressResponse.addresses),
+        postcode: tempMockPostcode,
       });
     });
 
@@ -52,10 +100,69 @@ describe('controllers/insurance/policy/broker-addresses', () => {
         delete res.locals.application;
       });
 
-      it(`should redirect to ${PROBLEM_WITH_SERVICE}`, () => {
-        get(req, res);
+      it(`should redirect to ${PROBLEM_WITH_SERVICE}`, async () => {
+        await get(req, res);
 
         expect(res.redirect).toHaveBeenCalledWith(PROBLEM_WITH_SERVICE);
+      });
+    });
+
+    describe('api error handling', () => {
+      describe('when api.keystone.getOrdnanceSurveyAddress returns apiError=true', () => {
+        it(`should redirect to ${PROBLEM_WITH_SERVICE}`, async () => {
+          const mockResponse = {
+            ...mockOrdnanceSurveyAddressResponse,
+            apiError: true,
+          };
+
+          api.keystone.getOrdnanceSurveyAddress = jest.fn(() => Promise.resolve(mockResponse));
+
+          await get(req, res);
+
+          expect(res.redirect).toHaveBeenCalledWith(PROBLEM_WITH_SERVICE);
+        });
+      });
+
+      describe('when api.keystone.getOrdnanceSurveyAddress returns invalidPostcode=true', () => {
+        it(`should redirect to ${BROKER_DETAILS_ROOT}`, async () => {
+          const mockResponse = {
+            ...mockOrdnanceSurveyAddressResponse,
+            invalidPostcode: true,
+          };
+
+          api.keystone.getOrdnanceSurveyAddress = jest.fn(() => Promise.resolve(mockResponse));
+
+          await get(req, res);
+
+          expect(res.redirect).toHaveBeenCalledWith(`${INSURANCE_ROOT}/${referenceNumber}${BROKER_DETAILS_ROOT}`);
+        });
+      });
+
+      describe('when api.keystone.getOrdnanceSurveyAddress returns noAddressesFound=true', () => {
+        it(`should redirect to ${BROKER_ZERO_ADDRESSES_ROOT}`, async () => {
+          const mockResponse = {
+            ...mockOrdnanceSurveyAddressResponse,
+            noAddressesFound: true,
+          };
+
+          api.keystone.getOrdnanceSurveyAddress = jest.fn(() => Promise.resolve(mockResponse));
+
+          await get(req, res);
+
+          expect(res.redirect).toHaveBeenCalledWith(`${INSURANCE_ROOT}/${referenceNumber}${BROKER_ZERO_ADDRESSES_ROOT}`);
+        });
+      });
+
+      describe('when there is an error', () => {
+        beforeEach(() => {
+          api.keystone.getOrdnanceSurveyAddress = mockSpyPromiseRejection;
+        });
+
+        it(`should redirect to ${PROBLEM_WITH_SERVICE}`, async () => {
+          await get(req, res);
+
+          expect(res.redirect).toHaveBeenCalledWith(PROBLEM_WITH_SERVICE);
+        });
       });
     });
   });
