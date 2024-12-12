2b01aa01e2fbe01afb79ebb5ae87c8b590c5da56
ttbarnes <tony@tonybarnes.me>
2024-12-12 13:10:42 +0000

    feat(EMS-4000): fix typo

--- e2e-tests/insurance/cypress/e2e/journeys/application-submission/multiple-policy-type/policy-conditions/submit-an-application-multiple-policy-type-not-eligible-for-small-export-builder.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/application-submission/multiple-policy-type/policy-conditions/submit-an-application-multiple-policy-type-not-eligible-for-small-export-builder.spec.js
@@ -1,7 +1,7 @@
 import { APPLICATION } from '../../../../../../../constants';
 
 context(
-  'Insurance - submit an application - Multiple policy type - not eligible for small export builder - As an Exporter, I want to submit my completed credit insurance application, So that UKEF can process and make a decision on my application',
+  'Insurance - submit an application - Multiple policy type - Not eligible for small export builder - As an Exporter, I want to submit my completed credit insurance application, So that UKEF can process and make a decision on my application',
   () => {
     let referenceNumber;
 
