8421f1de0d839a41cff3c17cb2af437e085417c4
ttbarnes <tony@tonybarnes.me>
2024-12-14 16:15:15 +0000

    feat(EMS-4024): improve e2e test coverage

--- e2e-tests/insurance/cypress/e2e/journeys/declarations/modern-slavery/validation/modern-slavery-validation-empty-form.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/declarations/modern-slavery/validation/modern-slavery-validation-empty-form.spec.js
@@ -14,9 +14,11 @@ const {
 
 const ERROR_STRINGS = ERROR_MESSAGES.INSURANCE.DECLARATIONS.MODERN_SLAVERY;
 
+const expectedErrorsCount = 3;
+
 const baseUrl = Cypress.config('baseUrl');
 
-context('Insurance - Declarations - Modern slavery page - validation - empty form', () => {
+context('Insurance - Declarations - Modern slavery page - validation - submitting an empty form', () => {
   let referenceNumber;
   let url;
 
@@ -35,53 +37,41 @@ context('Insurance - Declarations - Modern slavery page - validation - empty for
     cy.saveSession();
 
     cy.navigateToUrl(url);
+
+    cy.clickSubmitButton();
   });
 
   after(() => {
     cy.deleteApplication(referenceNumber);
   });
 
-  describe('when submitting an empty form', () => {
-    const expectedErrorsCount = 3;
-
-    beforeEach(() => {
-      cy.navigateToUrl(url);
-
-      cy.clickSubmitButton();
-    });
-
-    it(`should render ${WILL_ADHERE_TO_ALL_REQUIREMENTS} validation error`, () => {
-      cy.submitAndAssertRadioErrors({
-        errorIndex: 0,
-        field: yesRadio(WILL_ADHERE_TO_ALL_REQUIREMENTS),
-        expectedErrorsCount,
-        expectedErrorMessage: ERROR_STRINGS[WILL_ADHERE_TO_ALL_REQUIREMENTS].IS_EMPTY,
-        fieldShouldGainFocus: true,
-      });
-    });
-
-    it(`should render ${HAS_NO_OFFENSES_OR_INVESTIGATIONS} validation error`, () => {
-      cy.submitAndAssertRadioErrors({
-        errorIndex: 1,
-        field: yesRadio(HAS_NO_OFFENSES_OR_INVESTIGATIONS),
-        expectedErrorsCount,
-        expectedErrorMessage: ERROR_STRINGS[HAS_NO_OFFENSES_OR_INVESTIGATIONS].IS_EMPTY,
-        fieldShouldGainFocus: true,
-      });
-    });
-
-    it(`should render ${IS_NOT_AWARE_OF_EXISTING_SLAVERY} validation error`, () => {
-      cy.submitAndAssertRadioErrors({
-        errorIndex: 2,
-        field: yesRadio(IS_NOT_AWARE_OF_EXISTING_SLAVERY),
-        expectedErrorsCount,
-        expectedErrorMessage: ERROR_STRINGS[IS_NOT_AWARE_OF_EXISTING_SLAVERY].IS_EMPTY,
-        fieldShouldGainFocus: true,
-      });
+  it(`should render ${WILL_ADHERE_TO_ALL_REQUIREMENTS} validation error`, () => {
+    cy.submitAndAssertRadioErrors({
+      errorIndex: 0,
+      field: yesRadio(WILL_ADHERE_TO_ALL_REQUIREMENTS),
+      expectedErrorsCount,
+      expectedErrorMessage: ERROR_STRINGS[WILL_ADHERE_TO_ALL_REQUIREMENTS].IS_EMPTY,
+      fieldShouldGainFocus: true,
     });
   });
 
-  // TODO
-  // TODO
-  // TODO: submit all as no
+  it(`should render ${HAS_NO_OFFENSES_OR_INVESTIGATIONS} validation error`, () => {
+    cy.submitAndAssertRadioErrors({
+      errorIndex: 1,
+      field: yesRadio(HAS_NO_OFFENSES_OR_INVESTIGATIONS),
+      expectedErrorsCount,
+      expectedErrorMessage: ERROR_STRINGS[HAS_NO_OFFENSES_OR_INVESTIGATIONS].IS_EMPTY,
+      fieldShouldGainFocus: true,
+    });
+  });
+
+  it(`should render ${IS_NOT_AWARE_OF_EXISTING_SLAVERY} validation error`, () => {
+    cy.submitAndAssertRadioErrors({
+      errorIndex: 2,
+      field: yesRadio(IS_NOT_AWARE_OF_EXISTING_SLAVERY),
+      expectedErrorsCount,
+      expectedErrorMessage: ERROR_STRINGS[IS_NOT_AWARE_OF_EXISTING_SLAVERY].IS_EMPTY,
+      fieldShouldGainFocus: true,
+    });
+  });
 });
