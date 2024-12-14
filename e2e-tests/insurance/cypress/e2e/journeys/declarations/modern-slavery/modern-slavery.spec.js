00225ff0af5d4b06f88c277cedbba9d6dfe146b1
ttbarnes <tony@tonybarnes.me>
2024-12-14 13:25:45 +0000

    feat(EMS-4038): declarations - modern slavery - conditional radios - e2e tests

--- e2e-tests/insurance/cypress/e2e/journeys/declarations/modern-slavery/modern-slavery.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/declarations/modern-slavery/modern-slavery.spec.js
@@ -1,9 +1,6 @@
-import { field as fieldSelector, headingCaption, yesRadio, noRadio } from '../../../../../../pages/shared';
+import { headingCaption } from '../../../../../../pages/shared';
 import { modernSlaveryPage } from '../../../../../../pages/insurance/declarations';
 import { PAGES } from '../../../../../../content-strings';
-import { DECLARATIONS_FIELDS as FIELDS } from '../../../../../../content-strings/fields/insurance/declarations';
-import { FIELD_VALUES } from '../../../../../../constants';
-import { DECLARATIONS as DECLARATIONS_FIELD_IDS } from '../../../../../../constants/field-ids/insurance/declarations';
 import { INSURANCE_ROUTES } from '../../../../../../constants/routes/insurance';
 
 const CONTENT_STRINGS = PAGES.INSURANCE.DECLARATIONS.MODERN_SLAVERY;
@@ -12,10 +9,6 @@ const {
   ROOT: INSURANCE_ROOT,
   DECLARATIONS: { MODERN_SLAVERY },
 } = INSURANCE_ROUTES;
-
-const {
-  MODERN_SLAVERY: { WILL_ADHERE_TO_ALL_REQUIREMENTS, HAS_NO_OFFENSES_OR_INVESTIGATIONS, IS_NOT_AWARE_OF_EXISTING_SLAVERY },
-} = DECLARATIONS_FIELD_IDS;
 
 const {
   intro: { answerTheQuestions, guidingPrinciplesLink, ifYouSayNo },
@@ -74,120 +67,6 @@ context('Insurance - Declarations - Modern slavery page - TODO EMS-4023', () => 
 
       it('should render `if you say no` copy', () => {
         cy.checkText(ifYouSayNo(), CONTENT_STRINGS.INTRO.IF_YOU_SAY_NO);
-      });
-    });
-
-    describe(WILL_ADHERE_TO_ALL_REQUIREMENTS, () => {
-      const fieldId = WILL_ADHERE_TO_ALL_REQUIREMENTS;
-
-      const fieldStrings = FIELDS.MODERN_SLAVERY[fieldId].VERSIONS[0];
-
-      it('should render a legend', () => {
-        cy.checkText(fieldSelector(fieldId).legend(), fieldStrings.LABEL);
-      });
-
-      describe('`yes` radio', () => {
-        const selector = yesRadio();
-
-        it('should render a label', () => {
-          cy.checkText(selector.label().first(), FIELD_VALUES.YES);
-
-          cy.checkRadioInputYesAriaLabel(fieldStrings.LABEL);
-        });
-
-        it('should render an input', () => {
-          selector.input().first().should('exist');
-        });
-      });
-
-      describe('`no` radio', () => {
-        const selector = noRadio();
-
-        it('should render a label', () => {
-          cy.checkText(selector.label().first(), FIELD_VALUES.NO);
-
-          cy.checkRadioInputNoAriaLabel(fieldStrings.LABEL);
-        });
-
-        it('should render an input', () => {
-          selector.input().first().should('exist');
-        });
-      });
-    });
-
-    describe(HAS_NO_OFFENSES_OR_INVESTIGATIONS, () => {
-      const fieldId = HAS_NO_OFFENSES_OR_INVESTIGATIONS;
-
-      const fieldStrings = FIELDS.MODERN_SLAVERY[fieldId].VERSIONS[0];
-
-      it('should render a legend', () => {
-        cy.checkText(fieldSelector(fieldId).legend(), fieldStrings.LABEL);
-      });
-
-      describe('`yes` radio', () => {
-        const selector = yesRadio();
-
-        it('should render a label', () => {
-          cy.checkText(selector.label().eq(1), FIELD_VALUES.YES);
-
-          cy.checkRadioInputYesAriaLabel(fieldStrings.LABEL, 1);
-        });
-
-        it('should render an input', () => {
-          selector.input().eq(1).should('exist');
-        });
-      });
-
-      describe('`no` radio', () => {
-        const selector = noRadio();
-
-        it('should render a label', () => {
-          cy.checkText(selector.label().first(), FIELD_VALUES.NO);
-
-          cy.checkRadioInputNoAriaLabel(fieldStrings.LABEL, 1);
-        });
-
-        it('should render an input', () => {
-          selector.input().first().should('exist');
-        });
-      });
-    });
-
-    describe(IS_NOT_AWARE_OF_EXISTING_SLAVERY, () => {
-      const fieldId = IS_NOT_AWARE_OF_EXISTING_SLAVERY;
-
-      const fieldStrings = FIELDS.MODERN_SLAVERY[fieldId].VERSIONS[0];
-
-      it('should render a legend', () => {
-        cy.checkText(fieldSelector(fieldId).legend(), fieldStrings.LABEL);
-      });
-
-      describe('`yes` radio', () => {
-        const selector = yesRadio();
-
-        it('should render a label', () => {
-          cy.checkText(selector.label().eq(1), FIELD_VALUES.YES);
-
-          cy.checkRadioInputYesAriaLabel(fieldStrings.LABEL, 2);
-        });
-
-        it('should render an input', () => {
-          selector.input().eq(1).should('exist');
-        });
-      });
-
-      describe('`no` radio', () => {
-        const selector = noRadio();
-
-        it('should render a label', () => {
-          cy.checkText(selector.label().first(), FIELD_VALUES.NO);
-
-          cy.checkRadioInputNoAriaLabel(fieldStrings.LABEL, 2);
-        });
-
-        it('should render an input', () => {
-          selector.input().first().should('exist');
-        });
       });
     });
   });
