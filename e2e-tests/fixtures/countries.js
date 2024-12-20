00cee7e6a8008c585cd3421b3e1fa65017b70c65
ttbarnes <tony@tonybarnes.me>
2024-12-20 16:51:16 +0000

    chore(EMS-4067-4070): e2e improvements

--- e2e-tests/fixtures/countries.js
+++ e2e-tests/fixtures/countries.js
@@ -1,11 +1,6 @@
-export const XAD = {
-  NAME: 'Abu Dhabi',
-  ISO_CODE: 'XAD',
-};
-
-export const DZA = {
-  NAME: 'Algeria',
-  ISO_CODE: 'DZA',
+export const AFG = {
+  NAME: 'Afghanistan',
+  ISO_CODE: 'AFG',
 };
 
 export const AGO = {
@@ -13,19 +8,9 @@ export const AGO = {
   ISO_CODE: 'AGO',
 };
 
-export const FRA = {
-  NAME: 'France',
-  ISO_CODE: 'FRA',
-};
-
-export const BRA = {
-  NAME: 'Brazil',
-  ISO_CODE: 'BRA',
-};
-
-export const AFG = {
-  NAME: 'Afghanistan',
-  ISO_CODE: 'AFG',
+export const ARG = {
+  NAME: 'Argentina',
+  ISO_CODE: 'AGO',
 };
 
 export const BLR = {
@@ -33,16 +18,45 @@ export const BLR = {
   ISO_CODE: 'BLR',
 };
 
+export const BRA = {
+  NAME: 'Brazil',
+  ISO_CODE: 'BRA',
+};
+
+export const DZA = {
+  NAME: 'Algeria',
+  ISO_CODE: 'DZA',
+};
+
+export const FRA = {
+  NAME: 'France',
+  ISO_CODE: 'FRA',
+};
+
 export const GBR = {
   NAME: 'United Kingdom',
   ISO_CODE: 'GBR',
 };
 
+export const TN = {
+  NAME: 'Tunisia',
+  ISO_CODE: 'TN',
+};
+
+export const XAD = {
+  NAME: 'Abu Dhabi',
+  ISO_CODE: 'XAD',
+};
+
 const mockCountries = [DZA, FRA, AGO, GBR, AFG, BLR, XAD, BRA];
 
 const ONLINE_SUPPORT_1 = DZA;
+
 const NO_ONLINE_SUPPORT_1 = FRA;
 const NO_ONLINE_SUPPORT_2 = AGO;
+const NO_ONLINE_SUPPORT_3 = ARG;
+const NO_ONLINE_SUPPORT_4 = TN;
+
 const NOT_SUPPORTED_1 = GBR;
 const NOT_SUPPORTED_2 = AFG;
 const NOT_SUPPORTED_3 = BLR;
@@ -58,8 +72,6 @@ export const COUNTRY_QUOTE_SUPPORT = {
   NOT_SUPPORTED_1,
 };
 
-// TODO: rename "by email e2e tests" to be "no online support"?
-
 /**
  * COUNTRY_APPLICATION_SUPPORT
  * Different types of country support for an application
@@ -68,6 +80,8 @@ export const COUNTRY_APPLICATION_SUPPORT = {
   ONLINE_SUPPORT: ONLINE_SUPPORT_1,
   NO_ONLINE_SUPPORT_1,
   NO_ONLINE_SUPPORT_2,
+  NO_ONLINE_SUPPORT_3,
+  NO_ONLINE_SUPPORT_4,
   NOT_SUPPORTED_1,
   NOT_SUPPORTED_2,
   NOT_SUPPORTED_3,
--- e2e-tests/insurance/cypress/e2e/journeys/eligibility/buyer-country/buyer-country-no-online-support.spec.js
+++ e2e-tests/insurance/cypress/e2e/journeys/eligibility/buyer-country/buyer-country-no-online-support.spec.js
@@ -7,6 +7,8 @@ const {
 
 const COUNTRY_NAME_1 = COUNTRY_APPLICATION_SUPPORT.NO_ONLINE_SUPPORT_1.NAME;
 const COUNTRY_NAME_2 = COUNTRY_APPLICATION_SUPPORT.NO_ONLINE_SUPPORT_2.NAME;
+const COUNTRY_NAME_3 = COUNTRY_APPLICATION_SUPPORT.NO_ONLINE_SUPPORT_3.NAME;
+const COUNTRY_NAME_4 = COUNTRY_APPLICATION_SUPPORT.NO_ONLINE_SUPPORT_4.NAME;
 
 context(
   'Insurance - Buyer country page - As an exporter, I want to check if UKEF offer credit insurance policy for where my buyer is based - submit countries that have no online insurance support',
@@ -18,14 +20,26 @@ context(
     });
 
     describe(COUNTRY_NAME_1, () => {
-      it(`redirects to ${TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT} exit page`, () => {
+      it(`should redirect to ${TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT} exit page`, () => {
         cy.enterCountryAndAssertExitPageUrlBuyerCountry(COUNTRY_NAME_1, TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT);
       });
     });
 
     describe(COUNTRY_NAME_2, () => {
-      it(`redirects to ${TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT} exit page`, () => {
+      it(`should redirect to ${TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT} exit page`, () => {
         cy.enterCountryAndAssertExitPageUrlBuyerCountry(COUNTRY_NAME_2, TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT);
+      });
+    });
+
+    describe(COUNTRY_NAME_3, () => {
+      it(`should redirect to ${TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT} exit page`, () => {
+        cy.enterCountryAndAssertExitPageUrlBuyerCountry(COUNTRY_NAME_3, TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT);
+      });
+    });
+
+    describe(COUNTRY_NAME_4, () => {
+      it(`should redirect to ${TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT} exit page`, () => {
+        cy.enterCountryAndAssertExitPageUrlBuyerCountry(COUNTRY_NAME_4, TALK_TO_AN_EXPORT_FINANCE_MANAGER_EXIT);
       });
     });
   },
