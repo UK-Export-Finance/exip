d4a43f367ca8b2be6e2f01ef2a06d137225a5d36
ttbarnes <tony@tonybarnes.me>
2025-01-02 13:55:18 +0000

    feat(EMS-4086-4087): add e2e test

---
+++ e2e-tests/insurance/cypress/e2e/journeys/application-submission/multiple-policy-type/policy-conditions/submit-an-application-multiple-policy-type-eligible-for-small-export-builder-alternative-currency.spec.js
@@ -0,0 +1,34 @@
+import { APPLICATION } from '../../../../../../../constants';
+
+context(
+  'Insurance - submit an application - Multiple policy type - Eligible for small export builder - Alternative currency - As an Exporter, I want to submit my completed credit insurance application, So that UKEF can process and make a decision on my application',
+  () => {
+    let referenceNumber;
+
+    before(() => {
+      cy.completeSignInAndSubmitAnApplication({
+        policyType: APPLICATION.POLICY_TYPE.MULTIPLE,
+        maximumBuyerWillOwe: APPLICATION.SMALL_EXPORT_BUILDER.MAXIMUM_BUYER_WILL_OWE - 1,
+        alternativeCurrencyPolicy: true,
+      }).then((refNumber) => {
+        referenceNumber = refNumber;
+      });
+    });
+
+    beforeEach(() => {
+      cy.saveSession();
+    });
+
+    after(() => {
+      cy.deleteApplication(referenceNumber);
+    });
+
+    it('should successfully submit the application and redirect to `application submitted`', () => {
+      cy.assertApplicationSubmittedUrl(referenceNumber);
+    });
+
+    it('should render in a `submitted` state in the dashboard', () => {
+      cy.assertDashboardApplicationSubmitted(referenceNumber);
+    });
+  },
+);
