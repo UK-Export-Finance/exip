d3b23048ca1eb9c65a493bb0fb1bc20256a02595
ttbarnes <tony@tonybarnes.me>
2025-01-28 17:43:01 +0000

    chore(EMS-4155): update unit test

--- src/api/integrations/APIM/index.test.ts
+++ src/api/integrations/APIM/index.test.ts
@@ -3,14 +3,13 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import APIM from '.';
 import { EXTERNAL_API_ENDPOINTS, GBP, USD } from '../../constants';
+import { ApimSendEmailResponse } from '../../types';
 import { mockCisCountries, mockCurrencies } from '../../test-mocks';
 
 dotenv.config();
 
 const { APIM_MDM_URL } = process.env;
 const { APIM_MDM } = EXTERNAL_API_ENDPOINTS;
-
-// TODO: update once main is merged into this branch.
 
 describe('integrations/APIM', () => {
   describe('getCisCountries', () => {
@@ -159,6 +158,71 @@ describe('integrations/APIM', () => {
         mock.onGet(url).reply(500);
 
         await expect(APIM.getCurrenciesExchange(mockSource, mockTarget)).rejects.toThrow('Calling APIM - currencies exchange');
+      });
+    });
+  });
+
+  describe('sendEmail', () => {
+    const url = `${APIM_MDM_URL}${APIM_MDM.EMAIL}`;
+
+    const mockTemplateId = 'mock-template-id';
+    const mockSendToEmailAddress = 'mock@email.com';
+    const mockPersonalisation = { mock: true };
+
+    describe('when a 201 status and data is returned', () => {
+      test('it should return success=true and a emailRecipient field', async () => {
+        const mock = new MockAdapter(axios);
+
+        const mockResponseData: ApimSendEmailResponse = {
+          status: 201,
+          data: {
+            content: {},
+            id: 'mock-id',
+            reference: 'mock-reference',
+            template: {},
+            uri: 'mock-uri',
+          },
+        };
+
+        mock.onPost(url).reply(201, mockResponseData);
+
+        const result = await APIM.sendEmail(mockTemplateId, mockSendToEmailAddress, mockPersonalisation);
+
+        const expected = {
+          success: true,
+          emailRecipient: mockSendToEmailAddress,
+        };
+
+        expect(result).toEqual(expected);
+      });
+    });
+
+    describe('when no data is returned', () => {
+      test('it should return success=false and a emailRecipient field', async () => {
+        const mock = new MockAdapter(axios);
+
+        mock.onPost(url).reply(201);
+
+        const result = await APIM.sendEmail(mockTemplateId, mockSendToEmailAddress, mockPersonalisation);
+
+        const expected = {
+          success: false,
+          emailRecipient: mockSendToEmailAddress,
+        };
+
+        expect(result).toEqual(expected);
+      });
+    });
+
+    describe('when a 201 status is not returned', () => {
+      test('it should throw an error', async () => {
+        const mock = new MockAdapter(axios);
+
+        mock.onPost(url).reply(500);
+
+        const response = APIM.sendEmail(mockTemplateId, mockSendToEmailAddress, mockPersonalisation);
+
+        await expect(response).rejects.toThrow('Calling APIM - send email');
       });
     });
   });
