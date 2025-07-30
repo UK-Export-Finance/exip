1bd55de46c4eec2da2f827a2be1a3e239ea909e1
Abhi Markan <amarkan>
2025-07-30 10:56:38 +0100

    feat(EMS-4227): updated the external link and dependencies updates

--- src/api/content-strings/links.ts
+++ src/api/content-strings/links.ts
@@ -1,7 +1,7 @@
 export const LINKS = {
   EXTERNAL: {
-    GUIDANCE: 'https://www.gov.uk/guidance/credit-insurance-policy#eligibility',
-    BEFORE_YOU_START: 'https://www.gov.uk/guidance/get-a-quote-for-ukef-credit-insurance',
+    GUIDANCE: 'https://www.gov.uk/guidance/export-insurance-policy#eligibility',
+    BEFORE_YOU_START: 'https://www.gov.uk/guidance/get-a-quote-for-ukef-export-insurance',
     PRIVACY: 'https://www.gov.uk/government/publications/ukef-privacy-notice/ukef-privacy-notice--2',
     FEEDBACK: 'https://forms.office.com/r/TacytrRCgJ',
     RESEARCH: 'https://forms.office.com/pages/responsepage.aspx?id=jhOEgACUnkCm2ka1KB4LCkj8OKxLpCpDmTbrMyQ3j2JUOUFHNUc0QUhUOFdLNkJXWkRUS0wyMUZFNiQlQCN0PWcu',
@@ -20,6 +20,6 @@ export const LINKS = {
     COMPANIES_HOUSE: 'https://find-and-update.company-information.service.gov.uk',
     OHCHR_UN_GUIDING_PRINCIPLES_ON_BUSINESS_AND_HUMAN_RIGHTS:
       'https://www.ohchr.org/en/publications/reference-publications/guiding-principles-business-and-human-rights',
-    SMALL_EXPORT_BUILDER: 'https://www.gov.uk/guidance/credit-insurance-policy',
+    SMALL_EXPORT_BUILDER: 'https://www.gov.uk/guidance/export-insurance-policy',
   },
 };
--- src/ui/server/content-strings/links.ts
+++ src/ui/server/content-strings/links.ts
@@ -14,8 +14,8 @@ export const LINKS = {
     HREF: ROUTES.ROOT,
   },
   EXTERNAL: {
-    GUIDANCE: 'https://www.gov.uk/guidance/credit-insurance-policy#eligibility',
-    BEFORE_YOU_START: 'https://www.gov.uk/guidance/get-a-quote-for-ukef-credit-insurance',
+    GUIDANCE: 'https://www.gov.uk/guidance/export-insurance-policy#eligibility',
+    BEFORE_YOU_START: 'https://www.gov.uk/guidance/get-a-quote-for-ukef-export-insurance',
     PRIVACY: 'https://www.gov.uk/government/publications/ukef-privacy-notice/ukef-privacy-notice--2',
     FEEDBACK: 'https://forms.office.com/r/TacytrRCgJ',
     RESEARCH: 'https://forms.office.com/pages/responsepage.aspx?id=jhOEgACUnkCm2ka1KB4LCkj8OKxLpCpDmTbrMyQ3j2JUOUFHNUc0QUhUOFdLNkJXWkRUS0wyMUZFNiQlQCN0PWcu',
@@ -37,6 +37,6 @@ export const LINKS = {
     LEGISLATION_COMPANIES_ACT_2006_SECTION_1159: 'https://www.legislation.gov.uk/ukpga/2006/46/section/1159/enacted',
     LEGISLATION_MODERN_SLAVERY_ACT_2015: 'https://www.legislation.gov.uk/ukpga/2015/30/contents',
     GOV_PUBLISH_AN_ANNUAL_MODERN_SLAVERY_STATEMENT: 'https://www.gov.uk/guidance/publish-an-annual-modern-slavery-statement',
-    SMALL_EXPORT_BUILDER: 'https://www.gov.uk/guidance/credit-insurance-policy',
+    SMALL_EXPORT_BUILDER: 'https://www.gov.uk/guidance/export-insurance-policy',
   },
 };
